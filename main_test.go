a82b04d006772863d5a2b6347617a48de68fc8f1
Efren Garcia Home <efren.gl@gmail.com>
2020-06-25 17:29:43 -0500

    bug

--- main_test.go
+++ main_test.go
@@ -6,15 +6,22 @@ import (
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: sqlite, mysql
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	users := []User{
+		{Name: "subquery_having_1", Age: 10},
+		{Name: "subquery_having_2", Age: 20},
+		{Name: "subquery_having_3", Age: 30},
+		{Name: "subquery_having_4", Age: 40},
+	}
+	DB.Create(&users)
 
-	DB.Create(&user)
+	var results []User
+	DB.Select("AVG(age) as age, name").Table("users").Where("name LIKE ?", "subquery_having%").Group("users.name").Having("AVG(age) > (?)", DB.
+		Select("AVG(age)").Where("name LIKE ?", "subquery_having%").Table("users")).Find(&results)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	if len(results) != 2 {
+		t.Errorf("Two user group should be found, instead found %d", len(results))
 	}
 }
