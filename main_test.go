9a31a34edefffe37462c88997bf86c3686416f51
Efren Garcia Home <efren.gl@gmail.com>
2020-06-25 18:05:59 -0500

    Field with gorm:"-" is not set in query

--- main_test.go
+++ main_test.go
@@ -6,15 +6,21 @@ import (
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: sqlite, mysql
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	user := User{Name: "jinzhu", Company: Company{
+		Name: "xxx",
+	}}
 
 	DB.Create(&user)
 
 	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	if err := DB.Select("users.id, c.name as company_name").Joins("inner join companies as c on users.company_id =  c.id ").
+		Table("users").First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+	if result.CompanyName == "" {
+		t.Errorf("The field CompanyName is not set")
 	}
 }
--- models.go
+++ models.go
@@ -27,6 +27,7 @@ type User struct {
 	Languages []Language `gorm:"many2many:UserSpeak"`
 	Friends   []*User    `gorm:"many2many:user_friends"`
 	Active    bool
+	CompanyName string `gorm:"-"`
 }
 
 type Account struct {
