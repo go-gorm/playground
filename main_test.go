13a993df9bb1fcc8d2eff2e0c8b1d7e11713990d
Efren Garcia Home <efren.gl@gmail.com>
2020-06-30 10:30:12 -0500

    Panic when using anonymous structure in query.

--- main_test.go
+++ main_test.go
@@ -9,12 +9,20 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("Failed, got error: %v", r)
+		}
+	}()
 	user := User{Name: "jinzhu"}
 
 	DB.Create(&user)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	var entity = struct {
+		ID int64
+	}{}
+
+	if err := DB.Model(&User{}).Select("id").First(&entity, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
