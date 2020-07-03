526f999ff5cdbee80448ef5f195e38928b1e35d7
aveyuan <aveyuan@163.com>
2020-07-03 22:48:20 +0800

    insert data not set date query error

--- main_test.go
+++ main_test.go
@@ -17,4 +17,16 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	// insert a test data, not set time.
+	if err := DB.Exec("INSERT INTO `users` (`name`) VALUES ('test')").Error; err != nil {
+		t.Errorf("Failed, User insert error: %v ", err)
+	}
+
+	// try, query all
+	var users []User
+	if err := DB.Find(&User{}).Find(&users).Error; err != nil {
+		t.Errorf("Failed, User query all error: %v ", err)
+	}
+
 }
