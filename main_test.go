e826074e432f42d54c99af57aba3ea66272d88fd
alresvor <alresvor@gmail.com>
2020-07-05 15:55:00 +0800

    test foreign key

--- main_test.go
+++ main_test.go
@@ -8,13 +8,17 @@ import (
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+type Bugs struct {
+	ID     int64
+	Status BugStatus `gorm:"ForeignKey:Status;References:Status;Constraint:OnUpdate:CASCADE"`
+}
+
+type BugStatus struct {
+	Status string `gorm:"PrimaryKey;type:varchar(22)"`
+}
+
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	if err := DB.AutoMigrate(new(BugStatus), new(Bugs)); err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
