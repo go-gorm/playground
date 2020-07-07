78d33012465646a357b9653f9183a28ee90d243c
J Winnie <git@stormdesign.us>
2020-07-07 13:26:03 -0700

    Remove Message declaration from test

--- main_test.go
+++ main_test.go
@@ -11,13 +11,6 @@ import (
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
-type Message struct {
-	gorm.Model
-	Title	string
-	Body	string
-	Slug	string `gorm:"unique;not null"`
-}
-
 func TestGORM(t *testing.T) {
 	message := Message{Title: "", Body: "", Slug: ""}
 
