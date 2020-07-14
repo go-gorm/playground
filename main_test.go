4aff7f003efc182e47f78621dc38272c7a60f91d
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2020-07-15 02:26:20 +0800

    add test code

--- main_test.go
+++ main_test.go
@@ -9,12 +9,40 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	post := Post{
+		Title: "Hello World",
+		Content:
+		"Foo Bar",
+		Threads: []Thread{
+			{
+				Title:   "Foo",
+				Content: "Bar",
+			},
+			{
+				Title:   "Foo21",
+				Content: "Bar1",
+			},
+			{
+				Title:   "Foo2",
+				Content: "Bar2",
+			},
+		},
+	}
 
-	DB.Create(&user)
+	if err := DB.Create(&post).Error; err != nil {
+		t.Errorf("Failed to create post, got error: %v", err)
+	}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	if err := DB.Delete(&post).Error; err != nil {
+		t.Errorf("Failed to delete post, got error: %v", err)
+	}
+
+	var threads []Thread
+	if err := DB.Find(&threads).Error; err != nil {
+		t.Errorf("Failed to fetch threads, got error: %v", err)
+	}
+
+	if len(threads) > 0 {
+		t.Errorf("Failed to delete all threads, given parent post is deleted")
 	}
 }
--- models.go
+++ models.go
@@ -1,60 +1,27 @@
 package main
 
 import (
-	"database/sql"
-	"time"
+	_ "database/sql"
+	_ "time"
 
 	"gorm.io/gorm"
 )
 
-// User has one `Account` (has one), many `Pets` (has many) and `Toys` (has many - polymorphic)
-// He works in a Company (belongs to), he has a Manager (belongs to - single-table), and also managed a Team (has many - single-table)
-// He speaks many languages (many to many) and has many friends (many to many - single-table)
-// His pet also has one Toy (has one - polymorphic)
-type User struct {
+type Post struct {
 	gorm.Model
-	Name      string
-	Age       uint
-	Birthday  *time.Time
-	Account   Account
-	Pets      []*Pet
-	Toys      []Toy `gorm:"polymorphic:Owner"`
-	CompanyID *int
-	Company   Company
-	ManagerID *uint
-	Manager   *User
-	Team      []User     `gorm:"foreignkey:ManagerID"`
-	Languages []Language `gorm:"many2many:UserSpeak"`
-	Friends   []*User    `gorm:"many2many:user_friends"`
-	Active    bool
+	Title   string
+	Content string
+	Threads []Thread `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL"` // This wouldn't automagically work if soft-delete
 }
 
-type Account struct {
+type Thread struct {
 	gorm.Model
-	UserID sql.NullInt64
-	Number string
+	Title   string
+	Content string
+	PostID  uint
 }
 
-type Pet struct {
-	gorm.Model
-	UserID *uint
-	Name   string
-	Toy    Toy `gorm:"polymorphic:Owner;"`
-}
-
-type Toy struct {
-	gorm.Model
-	Name      string
-	OwnerID   string
-	OwnerType string
-}
-
-type Company struct {
-	ID   int
-	Name string
-}
-
-type Language struct {
-	Code string `gorm:"primarykey"`
-	Name string
+func (p *Post) BeforeDelete(db *gorm.DB) (err error) {
+	tx := db.Delete(&p.Threads)
+	return tx.Error
 }
