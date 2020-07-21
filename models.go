55eb28ea0e0eba02d75b3c28dbf5179a7e97ed20
mai1015 <mai1015@users.noreply.github.com>
2020-07-21 13:00:35 -0400

    commit

--- models.go
+++ models.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"github.com/google/uuid"
 	"time"
 
 	"gorm.io/gorm"
@@ -50,11 +52,55 @@ type Toy struct {
 }
 
 type Company struct {
-	ID   int
+	UUIDModel
 	Name string
+	DateTimeModel
 }
 
 type Language struct {
 	Code string `gorm:"primarykey"`
 	Name string
+}
+
+type CreationDateTimeModel struct {
+	CreatedAt time.Time
+}
+
+func (m *CreationDateTimeModel) BeforeCreate(tx *gorm.DB) (err error) {
+	fmt.Println("Test creation")
+	m.CreatedAt = time.Now()
+	return nil
+}
+
+type DateTimeModel struct {
+	CreationDateTimeModel
+	UpdatedAt time.Time
+	DeletedAt *time.Time `sql:"index"`
+}
+
+func (m *DateTimeModel) BeforeCreate(tx *gorm.DB) (err error) {
+	fmt.Println("Test datatime")
+	m.UpdatedAt = time.Now()
+	return nil
+}
+
+type UUIDModel struct {
+	ID string
+}
+
+func isEmpty(id uuid.UUID) bool {
+	for i := range id {
+		if id[i] != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func (u *UUIDModel) BeforeCreate(tx *gorm.DB) (err error) {
+	fmt.Println("Test UUID")
+	if len(u.ID) <= 0 {
+		u.ID = uuid.New().String()
+	}
+	return nil
 }
