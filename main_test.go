8aef493346a0d93226399a4ca20a21cfcb760f42
tiehm <tiehm@users.noreply.github.com>
2020-07-21 18:29:09 +0200

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -6,15 +6,33 @@ import (
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: mysql
+
+type Settings struct {
+	ID uint `gorm:"type:mediumint;unsigned;primary_key;AUTO_INCREMENT;unsigned" json:"ID"`
+	DiscountCode string `gorm:"type:varchar(255);not null" json:"discount_code"`
+}
+
+
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	DB.Debug().AutoMigrate(&Settings{})
 
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	settings := Settings{
+		DiscountCode: "test",
+	}
+	
+	DB.Create(&settings)
+	
+	find := Settings{
+		DiscountCode: "test",
+	}
+	err := DB.Debug().Where(&find).First(&find).Error
+	if err != nil {
+		t.Errorf("Failed, got error: %s", err.Error())
+		return
+	}
+	if find.ID == 0 {
+		t.Errorf("Failed, did not get correct struct %v", find)
 	}
 }
