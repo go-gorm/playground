33a74ea6d6a8329cfcd89bc67694d4286b4352d1
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2020-07-21 15:12:36 +0800

    download instant client from oracle on run stage (well let's not hope for dmca)

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 dialects=("sqlite" "mysql" "postgres" "sqlserver" "oracle")
+ORACLE_INSTANT_CLIENT_URL="https://download.oracle.com/otn_software/linux/instantclient/19600/instantclient-basic-linux.x64-19.6.0.0.0dbru.zip"
+ORACLE_INSTANT_CLIENT_FILE="instant_client.zip"
 
 if [ "$GORM_ENABLE_CACHE" = "" ]
 then
@@ -17,6 +19,15 @@ for dialect in "${dialects[@]}" ; do
   then
     if [[ $(grep TEST_DRIVER main_test.go) =~ "${dialect}" ]]
     then
+      if [[ "$dialect" =~ "oracle" ]]
+      then
+        echo "downloading oracle instant client..."
+        curl "$ORACLE_INSTANT_CLIENT_URL" -o "$ORACLE_INSTANT_CLIENT_FILE"
+        echo "unzipping oracle instant client..."
+        unzip "$ORACLE_INSTANT_CLIENT_FILE"
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/instantclient_19_6
+        echo "exported instant client libraries to LD_LIBRARY_PATH, now it should not complain about missing oracle libraries"
+      fi
       echo "testing ${dialect}..."
       GORM_DIALECT=${dialect} go test -race -count=1 -v ./...
     else
