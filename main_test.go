8ca3bbed0dd25a3d231c25a0382ce3b0e38733b6
Tom Berger <tab1293@gmail.com>
2020-07-27 10:51:33 -0400

    Joins do not work with has many associations

--- main_test.go
+++ main_test.go
@@ -9,12 +9,34 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	user := User{Name: "tom"}
+	err := DB.Create(&user).Error
+	if err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	pets := [...]*Pet{
+		&Pet{
+			Name:   "fluffy",
+			UserID: &user.ID,
+		},
+		&Pet{
+			Name:   "lucky",
+			UserID: &user.ID,
+		},
+	}
+
+	for _, pet := range pets {
+		err := DB.Create(pet).Error
+		if err != nil {
+			t.Errorf("Failed, got error: %v", err)
+		}
+	}
+
+	var u User
+	err = DB.Joins("Pets").Where("users.id = ?", user.ID).Find(&u).Error
+	if err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
 }
