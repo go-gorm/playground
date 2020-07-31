3a3ea97d094f19ab6c0b24ac896b219f3c012bfd
Stephano George <huohuo5230@gmail.com>
2020-07-31 17:29:38 +0800

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+type S struct {
+	ID   int    `gorm:"autoIncrement"`
+	Name string `gorm:""`
+	Fs   []F    `gorm:"many2many:s_f_bind; foreignKey:ID; joinForeignKey:SID; references:ID; joinReferences:FID"`
+}
+type F struct {
+	ID   int    `gorm:"autoIncrement"`
+	Name string `gorm:""`
+}
+
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
 
-	DB.Create(&user)
+	db, err := gorm.Open(sqlite.Open("test.db"), nil)
+	checkError(err)
+	err = db.AutoMigrate(&S{})
+	checkError(err)
+	err = db.AutoMigrate(&F{})
+	checkError(err)
+}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+func checkError(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
