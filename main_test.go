46853af017cdc9549503dc86d6b653aafb8d8113
Mendel Greenberg <mendel@chabad360.me>
2020-08-04 13:07:57 -0700

    added a few more tests

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"testing"
 	"reflect"
+	"encoding/json"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -15,7 +16,18 @@ func TestGORM(t *testing.T) {
 	DB.Create(&user)
 
 	var result User
-	if DB.First(&result, user.ID); !reflect.DeepEqual(result.CreatedAt, user.CreatedAt) {
-		t.Errorf("Failed, result.CreatedAt != user.CreatedAt, result.CreatedAt: %v, user.CreatedAt: %v", result.CreatedAt, user.CreatedAt)
+	DB.First(&result, user.ID)
+	
+	userJson, _ := json.Marshal(user)
+	resultJson, _ := json.Marshal(result)
+	
+	if resultJson != userJson {
+		t.Errorf("jsonEqual Failed, resultJson: %v, userJson: %v", resultJson, userJson)
+	}
+	if !result.CreatedAt.Equal(user.CreatedAt) {
+		t.Errorf("time.Equal Failed, result.CreatedAt: %v, user.CreatedAt: %v", result.CreatedAt, user.CreatedAt)
+	}
+	if !reflect.DeepEqual(result.CreatedAt, user.CreatedAt) {
+		t.Errorf("reflect.DeepEqual Failed, result.CreatedAt: %v, user.CreatedAt: %v", result.CreatedAt, user.CreatedAt)
 	}
 }
