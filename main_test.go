67ef82fea51c6cb8122bd112a0efac201d891de4
Jason Freidman <jason.freidman@sisense.com>
2020-08-14 17:44:27 -0700

    Show panic when DB.WithContext(ctx).Transaction is called on a canceled context.
    I initially thought this happened if the context is canceled during the transaction,
    but that seems to work okay.
    
    I will fix my code by checking ctx.Err() but was surprised to see a panic in the library.

--- main_test.go
+++ main_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -13,8 +17,23 @@ func TestGORM(t *testing.T) {
 
 	DB.Create(&user)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
-	}
+	ctx := context.Background()
+	//	ctx, _ = context.WithTimeout(ctx, time.Nanosecond)
+
+	ctx, cancelFunc := context.WithCancel(ctx)
+	cancelFunc()
+
+	err := DB.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		var result User
+		if err := tx.First(&result, user.ID).Error; err != nil {
+			t.Errorf("Failed, got error: %v", err)
+		}
+		user.Name = "foobar"
+		user.ID = 0
+		tx.Create([]User{user})
+
+		return errors.New("some error")
+	})
+
+	t.Error(err)
 }
