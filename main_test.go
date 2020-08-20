3a8ab34d68f97d18143aa8c2a11508267f11b058
Stephano George <huohuo5230@gmail.com>
2020-08-20 20:50:34 +0800

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,20 +1,58 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
 	"testing"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
-// GORM_REPO: https://github.com/go-gorm/gorm.git
-// GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+type T01 struct {
+	ID   int    `gorm:"autoIncrement"`
+	Name string `gorm:"uniqueIndex"`
+	T2s  []T02  `gorm:"many2many:t01_t02_bind; foreignKey:Name; joinForeignKey:Name1; references:Name; joinReferences:Name2; constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+type T02 struct {
+	ID   int    `gorm:"autoIncrement"`
+	Name string `gorm:"uniqueIndex"`
+}
+func TestTest2(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{
+		Logger: logger.New(
+			log.New(os.Stdout, "", log.LstdFlags),
+			logger.Config{
+				LogLevel: logger.Info,
+			}),
+	})
+	checkError(err)
+	
+	err = db.AutoMigrate(&T01{}, &T02{})
+	checkError(err)
 
-func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	t1 := T01{Name: "1"}
+	err = db.Create(&t1).Error
+	checkError(err)
+	
+	err = db.Model(&t1).Association("T2s").Append([]T02{{Name: "1"}})
+	checkError(err)
+	err = db.Model(&t1).Association("T2s").Append([]T02{{Name: "1-1"}})
+	checkError(err)
+	
+	t2s := make([]T02, 0, 2)
+	err = db.Model(&t1).Association("T2s").Find(&t2s)
+	checkError(err)
+	
+	b, _ := json.MarshalIndent(t2s, "", "    ")
+	fmt.Printf("%s\n", b)
+}
 
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		panic(err)
 	}
 }
