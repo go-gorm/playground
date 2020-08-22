f7618376acd9236ee33d21acc6a2b7ddd0af8df2
ustas <ustasam@gmail.com>
2020-08-22 11:34:54 +0400

    Stack Overflow and Data Corruption

--- main_test.go
+++ main_test.go
@@ -1,20 +1,215 @@
 package main
-25
+
 import (
+	"errors"
+	"fmt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
 	"testing"
 )
 
-// GORM_REPO: https://github.com/go-gorm/gorm.git
-// GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+var models = []interface{}{&BaseModel{}, &Site{}, &User{}, &Company{}, &BusinessModel{}, &Directory{},
+	&File{}, &TimeSlot{}, &Person{}, &Employee{}, &Subdivision{}, &BranchOffice{}, &ProductGroup{}}
 
-func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+//var models = GetModels()
+var testDB *gorm.DB
 
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+func InitDB(migrate bool, customConfig bool) {
+	dbPath := filepath.Join(os.TempDir(), "gorm.test.db")
+	os.Remove(dbPath)
+	if customConfig {
+		DB, _ = gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	} else {
+		DB, _ = gorm.Open(sqlite.Open(dbPath), &gorm.Config{SkipDefaultTransaction: false, PrepareStmt: true, DisableForeignKeyConstraintWhenMigrating: true})
 	}
+	testDB = DB
+	if migrate {
+		testDB.Migrator().DropTable(models...)
+		testDB.AutoMigrate(models...)
+	}
+
+}
+
+func TestGORM_AutoMigrate(t *testing.T) { // OK
+	InitDB(false, false)
+	if err := testDB.AutoMigrate(models...); err != nil {
+		t.Errorf("List AutoMigrate Failed, got error: %v", err)
+	}
+	if err := testDB.Migrator().DropTable(models...); err != nil {
+		t.Errorf("DropTable Failed, got error: %v", err)
+	}
+}
+
+func TestGORM_AutoMigrate2(t *testing.T) { // OK
+	InitDB(false, false)
+	for _, model := range models {
+		fmt.Println("AutoMigrate - " + reflect.ValueOf(model).Elem().Type().Name())
+		if err := testDB.AutoMigrate(model); err != nil {
+			t.Errorf("AutoMigrate Failed, got error: %v", err)
+		}
+	}
+	for _, model := range models {
+		fmt.Println("DropTable " + reflect.ValueOf(model).Elem().Type().Name())
+		if err := testDB.Migrator().DropTable(model); err != nil {
+			t.Errorf("DropTable Failed, got error: %v", err)
+		}
+	}
+}
+
+func TestGORM_StackOverflowInCreate(t *testing.T) {
+	if os.Getenv("SKIP") != "" {
+		t.Skip()
+	}
+	InitDB(true, false)
+
+	admin := &User{PersonName: " Admin", Enabled: true, UserType: "admin", Password: "123"}
+	admin.Name = "Admin"
+	admin.Description = "Администратор сайта"
+	admin.User = admin
+	testDB.Create(admin)
+}
+
+func TestGORM_StackOverflowInSave(t *testing.T) {
+	if os.Getenv("SKIP") != "" {
+		t.Skip()
+	}
+	InitDB(true, false)
+
+	admin := &User{PersonName: " Admin", Enabled: true, UserType: "admin", Password: "123"}
+	admin.Name = "Admin"
+	admin.Description = "Администратор сайта"
+	testDB.Create(admin)
+	admin.User = admin
+	testDB.Save(admin)
+}
+
+func TestGORM_Series(t *testing.T) {
+	Series(false, t) // data corruption occurs
+	Series(true, t) // in my project data corruption occurs in custom config case
+}
+
+func CreateSite(site *Site, admin *User, company *Company) (*Site, *User, *Company) {
+	site.Name = strings.Split(site.Host, ".")[0]
+	site.Number = site.Number
+	if admin == nil {
+		admin = &User{PersonName: site.Name + " Admin", Enabled: true, UserType: "admin", Password: "HashAndSalt(site.Name + time.Now().String())"}
+		admin.Name = site.Number + "Admin"
+		admin.Description = "Администратор сайта - " + site.Host
+		//admin.User = admin // lineA
+		testDB.Create(admin) // not work 1 if lineA uncommented
+		//admin.User = admin // lineB
+		//testDB.Save(admin) // not work 2 if lineB uncommented
+		testDB.Model(admin).Update("user_id", admin.ID) // ok in lineB - commented
+	}
+
+	if company == nil {
+		company = &Company{}
+		company.Name = site.Name
+		company.Number = site.Number
+		company.Description = "Адрес сайта - " + site.Host
+		company.User = admin // not stored (stored as miracle)
+		//company.Company=company // not work
+		testDB.Create(company) // miracle = in this line in table "users" (not companies! as I suppose) field company_id becomes = admin.ID in line "company.User = admin" uncommented
+		testDB.Model(company).Update("company_id", company.ID)
+		testDB.Model(company).Update("user_id", admin.ID)
+	}
+
+	//if admin.Company == nil {
+	//	admin.Company = company
+	//	testDB.Save(admin) // stack overflow
+	//}
+	if admin.CompanyID == 0 {
+		testDB.Model(admin).Update("company_id", company.ID)
+	}
+
+	//site.Company = company // stored as supposed in CompanyID
+	//site.User = admin // stored as supposed in UserID
+	site.CompanyID = company.ID // stored as supposed in CompanyID
+	site.UserID = admin.ID
+	testDB.Create(site) // work well
+	return site, admin, company
+}
+
+func Series(customConfig bool, t *testing.T) {
+	InitDB(true, customConfig)
+
+	sites := []*Site{}
+	users := []*User{}
+	companies := []*Company{}
+
+	GtxBotToken := "12495358:AAGuC3QJg"
+	GtxChat := "70347"
+	CreateSite(&Site{BusinessModel: BusinessModel{Name: "My Chef"}, Host: "mychefeda.ru", Hosts: "edaa.ga;127.1.1.2", Redirects: "edarot.ru;edaa.ga<samon.tk",
+		BotToken: "1383:AAEzuBSV2AeA", Chat: "-100173315", AllowLogin: true, AllowRegister: true,
+		YandexShopID: "7405", YandexPassword: "test_MCcL1wRWFKHOEFx6bC8"}, nil, nil)
+	CreateSite(&Site{BusinessModel: BusinessModel{Name: "asamon"}, Host: "asamon.ru", Hosts: "127.1.1.3",
+		BotToken: GtxBotToken, Chat: GtxChat, AllowLogin: true, AllowRegister: false}, nil, nil)
+	CreateSite(&Site{BusinessModel: BusinessModel{Name: "1cluga"}, Host: "1clu.ga", Hosts: "127.1.1.4",
+		BotToken: GtxBotToken, Chat: GtxChat, AllowLogin: true, AllowRegister: false}, nil, nil)
+	CreateSite(&Site{BusinessModel: BusinessModel{Name: "Тестовый сайт"}, Host: "ustas.ml", Hosts: "127.1.1.5",
+		BotToken: GtxBotToken, Chat: GtxChat, AllowLogin: true, AllowRegister: false,
+		YandexShopID: "7415", YandexPassword: "test_pSFqmi6t0ZgbgJbXkLNoQ"}, nil, nil)
+	CreateSite(&Site{BusinessModel: BusinessModel{Name: "Тестовый домашний 1cka.ml"}, Host: "1cka.ml", BotToken: GtxBotToken, Chat: GtxChat,
+		AllowLogin: true, AllowRegister: false, YandexShopID: "7354", YandexPassword: "test_ApA9lu8wIkB27E9llkTo"}, nil, nil)
+
+	for i := 0; i < 50; i++ {
+		site, user, company := CreateSite(&Site{BusinessModel: BusinessModel{Name: "Метамагазин"}, Host: "metamagazine.ru", Hosts: "127.1.1.1", Redirects: "metamagazin.ru;127.5.5.5",
+			RedirectFromRoot: true, BotToken: GtxBotToken, Chat: GtxChat, AllowLogin: true, AllowRegister: false}, nil, nil)
+
+		sites = append(sites, site)
+		users = append(users, user)
+		companies = append(companies, company)
+
+		CreateSite(&Site{BusinessModel: BusinessModel{Name: "Метамагазин"}, Host: "metamagazine.ru", Hosts: "127.1.1.1", Redirects: "metamagazin.ru;127.5.5.5",
+			RedirectFromRoot: true, BotToken: GtxBotToken, Chat: GtxChat, AllowLogin: true, AllowRegister: false}, nil, nil)
+	}
+
+	for i := 0; i < 50; i++ {
+		site := sites[i]
+		user := users[i]
+		company := companies[i]
+
+		if user.UserID != user.ID {
+			t.Errorf("TestGORM_Series: %v, customConfig: %v", errors.New("user.UserID != user.ID"), customConfig)
+		}
+		if user.CompanyID != company.ID {
+			t.Errorf("TestGORM_Series: %v, customConfig: %v", errors.New("user.CompanyID != company.ID"), customConfig)
+		}
+		if company.UserID != user.ID {
+			t.Errorf("TestGORM_Series: %v, customConfig: %v", errors.New("company.UserID != user.ID"), customConfig)
+		}
+		if company.CompanyID != company.ID {
+			t.Errorf("TestGORM_Series: %v, customConfig: %v", errors.New("company.CompanyID != company.ID"), customConfig)
+		}
+		if site.CompanyID != company.ID {
+			t.Errorf("TestGORM_Series: %v, customConfig: %v", errors.New("site.CompanyID != company.ID"), customConfig)
+		}
+		if site.UserID != user.ID {
+			t.Errorf("TestGORM_Series: %v, customConfig: %v", errors.New("site.UserID != user.ID"), customConfig)
+		}
+	}
+
+	var q_sites []*Site
+	testDB.Find(&q_sites)
+
+	var q_users []*User
+	testDB.Find(&q_users)
+
+	var q_companies []*Company
+	testDB.Find(&q_companies)
+
+	if len(q_companies) != 105 {
+		t.Errorf("TestGORM_Series: %v", errors.New("len(sites)!=105"))
+	}
+	if len(q_users) != 105 {
+		t.Errorf("TestGORM_Series: %v", errors.New("len(users)!=105"))
+	}
+	if len(q_companies) != 105 {
+		t.Errorf("TestGORM_Series: %v", errors.New("len(companies)!=105"))
+	}
+
 }
--- models.go
+++ models.go
@@ -7,11 +7,122 @@ import (
 	"gorm.io/gorm"
 )
 
+type (
+	BaseModel struct {
+		//gorm.Model // `json:"-"` in gorm.Model
+		ID        uint           `gorm:"primarykey"`
+		CreatedAt time.Time      `json:"-"`
+		UpdatedAt time.Time      `json:"-"`
+		DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+
+		Company   *Company `display:"none" visibility:"500" Label:"Компания" json:"-"`
+		CompanyID uint     `display:"none" json:"-"`
+		User      *User    `display:"none" visibility:"500" Label:"Пользователь" json:"-"`
+		UserID    uint     `display:"none" json:"-"`
+	}
+
+	Site struct {
+		BusinessModel    `Model:"Сайт" Plural:"Сайты"`
+		Host             string `Label:"Адрес сайта"`
+		Hosts            string `Label:"Дополнительные адреса сайта"` // ; - separator
+		Redirects        string `Label:"Адреса перенаправления"`      // ; - separator, - - delimiter
+		RedirectFromRoot bool   `Label:"Перенаправлять на свой домен"`
+		AllowLogin       bool   `Label:"Разрешение входа пользователя"`
+		AllowRegister    bool   `Label:"Разрешение регистрации"`
+
+		BotToken string `Label:"ID и токен телеграм канала"`
+		Chat     string `Label:"ID чата телеграм канала"`
+
+		YandexShopID   string `Label:"ID Yandex магазина"`
+		YandexPassword string `Label:"Пароль Yandex магазина"`
+
+		//Renderer *echoview.ViewEngine `gorm:"-"`
+	}
+
+	User struct {
+		Directory
+		UserType   string    `Label:"Тип"`
+		Email      string    `Label:"Почта" validType:"email"`
+		Phone      string    `Label:"Телефон"`
+		Password   string    `Label:"Пароль"`
+		Language   string    `Label:"Язык"`
+		Enabled    bool      `Label:"Включен"` // Блокировка
+		PersonID   uint      `Label:"Персона"`
+		Person     *Person   `Label:"Персона"`
+		PersonName string    `Label:"ФИО"`
+		EmployeeID uint      `Label:"Работник"`
+		Employee   *Employee `Label:"Работник"`
+	}
+	Company struct { //Organization
+		Directory
+		CompanyType string
+		PersonID    uint    `Label:"Владелец"`
+		Person      *Person `Label:"Владелец"`
+		TimeSlotID  uint
+		TimeSlot    *TimeSlot `Label:"Расписание работы"`
+	}
+	BusinessModel struct {
+		BaseModel
+		Number      string `Label:"Номер"`
+		Numb        uint   `Label:"Номер" visibility:"300"`
+		Name        string `Label:"Наименование" required:"true"`
+		Files       []File `gorm:"polymorphic:Owner;" Label:"Файлы" visibility:"900" index:"502" json:",omitempty"`
+		Description string `Label:"Описание" index:"501" json:",omitempty"`
+	}
+	Directory struct {
+		BusinessModel
+		DirectoryID uint       `Label:"Группа"`
+		Directory   *Directory `Label:"Группа" do:"width:100"`
+	}
+	File struct {
+		BaseModel
+		OwnerID   int
+		OwnerType string
+		Path      string
+	}
+	TimeSlot struct {
+		BusinessModel
+		Start time.Time
+		End   time.Time
+	}
+	Person struct {
+		Directory	`Plural:"Физические лица" Model:"Физическое лицо"`
+		Sex      string    `Label:"Пол"`
+		Birthday time.Time `Label:"Дата рождение"`
+		Address  string    `Label:"Адрес"`
+	}
+
+	Employee struct {
+		Directory	`Plural:"Работники" Model:"Работник"`
+		PersonID      uint    `Label:"Персона"`
+		Person        *Person `Label:"Персона"`
+		SubdivisionID uint
+		Subdivision   *Subdivision
+	}
+	Subdivision struct {
+		Directory		`Plural:"Подразделения" Model:"Подразделение"`
+		BranchOfficeID uint
+		BranchOffice   *BranchOffice
+		TimeSlotID     uint
+		TimeSlot       *TimeSlot `Label:"Расписание работы"`
+	}
+	BranchOffice struct {
+		Directory	`Plural:"Филиалы компании" Model:"Филиал компании"`
+		TimeSlotID uint
+		TimeSlot   *TimeSlot `Label:"Расписание работы"`
+	}
+	ProductGroup struct {
+		Directory	`Plural:"Группа номенклатуры" Model:"Группы номенклатуры" Category:"Прочее"`
+	}
+)
+
+//region unused
+
 // User has one `Account` (has one), many `Pets` (has many) and `Toys` (has many - polymorphic)
 // He works in a Company (belongs to), he has a Manager (belongs to - single-table), and also managed a Team (has many - single-table)
 // He speaks many languages (many to many) and has many friends (many to many - single-table)
 // His pet also has one Toy (has one - polymorphic)
-type User struct {
+type User_PG struct {
 	gorm.Model
 	Name      string
 	Age       uint
@@ -49,7 +160,7 @@ type Toy struct {
 	OwnerType string
 }
 
-type Company struct {
+type Company_PG struct {
 	ID   int
 	Name string
 }
@@ -58,3 +169,5 @@ type Language struct {
 	Code string `gorm:"primarykey"`
 	Name string
 }
+
+//endregion
