bcfb44bbd1af6aa5a42970cecb8e0937a60819bb
Michael Senn <michael@morrolan.ch>
2020-08-24 20:14:00 +0200

    Add example of SIGSEGV.

--- main_test.go
+++ main_test.go
@@ -6,15 +6,9 @@ import (
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: sqlite
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
-	}
+	// DB.Migrator().CreateTable(&User{})
+	DB.Migrator().DropTable("users")
 }
