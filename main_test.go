f9abdc17074c8e250939201e36978c248d481de1
soranoba <soranoba@gmail.com>
2020-08-25 18:05:18 +0900

    hasMany deletion error

--- main_test.go
+++ main_test.go
@@ -17,4 +17,8 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	if err := DB.Delete(User{}).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
 }
