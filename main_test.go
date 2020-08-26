a9ebd50b7eb5c669c4519114ec9953f1c755b835
LRH3321 <lrh3321@126.com>
2020-08-26 13:48:59 +0800

    Order and Limit clauses don't work for `Update`

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"testing"
 )
 
@@ -16,5 +17,24 @@ func TestGORM(t *testing.T) {
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+}
+
+func TestUpdateWithLimit(t *testing.T) {
+	defer DB.Delete(User{}, "name LIKE ?", "user%")
+
+	for i := 0; i < 10; i++ {
+		user := User{Name: "user" + strconv.Itoa(i), Age: uint(i)}
+		DB.Create(&user)
+	}
+
+	tx := DB.Model(&User{}).Select("age").Where("name LIKE ?", "user%").Order("name ASC").Limit(5).Updates(&User{Age: 100})
+
+	if tx.Error != nil {
+		t.Errorf("Failed, got error: %v", tx.Error)
+	}
+
+	if tx.RowsAffected > 5 {
+		t.Errorf("Failed, update %v rows", tx.RowsAffected)
 	}
 }
