809da2f077e9fc05c33fa280ebe01765a8014bfc
Pourqavam <39864929+Pourqavam@users.noreply.github.com>
2020-08-28 01:18:44 +0430

    Preload self-referential belongs to
    Preload self-referential belongs to problem

--- main_test.go
+++ main_test.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"testing"
+	"fmt"
 )
+
+type User struct {
+	ID           int32 `gorm:"primaryKey"`
+	Name         string
+	CreatedBy    *int32
+	Creator      *User `gorm:"foreignKey:CreatedBy;references:ID"`
+}
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	u1 := User{ID: 1, CreatedBy: nil}
+	DB.Create(&u1)
+	
+	u2 := User{ID: 2, CreatedBy: &u1.ID}
+	DB.Create(&u2)
 
-	DB.Create(&user)
+	u1Found := &User{}
+	DB.Preload("Creator").Find(u1Found, 1)
+	fmt.Println(u1Found) // CreatedBy is nil, but Creator is u2
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
-	}
+	u2Found := &User{}
+	DB.Preload("Creator").Find(u2Found, 2)
+	fmt.Println(u2Found) // CreatedBy is 1, but Creator is nil
 }
