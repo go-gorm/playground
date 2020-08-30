2ecdbd959441de3e1d82fafe6161e64a44029473
Jorge Javier Araya Navarro <jorge@esavara.cr>
2020-08-30 03:50:09 -0600

    Use correct table names

--- pkg/model/v43/migration/v4/migration.go
+++ pkg/model/v43/migration/v4/migration.go
@@ -5,10 +5,10 @@ import (
 )
 
 func Migrate_17_tipo_decimal_exoneraciones_otros_cargos(tx *gorm.DB, m gorm.Migrator) error {
-	err := tx.Exec("ALTER TABLE exoneracions ALTER COLUMN porcentaje_exoneracion TYPE DECIMAL(4,2);").Error
+	err := tx.Exec("ALTER TABLE t_exoneracion ALTER COLUMN porcentaje_exoneracion TYPE DECIMAL(4,2);").Error
 	if err != nil {
 		return err
 	}
-	err = tx.Exec("ALTER TABLE otros_cargos ALTER COLUMN porcentaje TYPE DECIMAL(4,2);").Error
+	err = tx.Exec("ALTER TABLE t_otros_cargos ALTER COLUMN porcentaje TYPE DECIMAL(4,2);").Error
 	return err
 }
