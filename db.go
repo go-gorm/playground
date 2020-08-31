7350b8b7e81a30261c474f7548c2a32efff26bab
Jorge Javier Araya Navarro <jorge@esavara.cr>
2020-08-30 20:27:04 -0600

    Try (and fails) to reproduce the other bug

--- db.go
+++ db.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/DATA-DOG/go-txdb"
+	_ "github.com/lib/pq"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
--- models.go
+++ models.go
@@ -13,7 +13,7 @@ import (
 // His pet also has one Toy (has one - polymorphic)
 type User struct {
 	gorm.Model
-	Name      string
+	Name      string `gorm:"not null;size:100;unique"`
 	Age       uint
 	Birthday  *time.Time
 	Account   Account
