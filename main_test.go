22907dc3a1f17f64893b5de7173197de6ca468bb
zzjin <zzjin@users.noreply.github.com>
2020-09-03 16:59:29 +0800

    test case for closed Transaction
    test case for closed Transaction

--- main_test.go
+++ main_test.go
@@ -9,12 +9,13 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	rawDB, rawErr := DB.DB()
+	if rawErr != nil {
+		t.Errorf("Failed, got error: %v", rawErr)
 	}
+	rawDB.Close()
+
+	DB.Transaction(func(tx *gorm.DB) error {
+		return nil
+	})
 }
