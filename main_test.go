49a2708a02aa4c4af4f824039d1787645abe7f09
zzjin <zzjin@users.noreply.github.com>
2020-09-03 18:02:58 +0800

    Update main_test.go
    add `PrepareStmt: true`

--- main_test.go
+++ main_test.go
@@ -1,9 +1,17 @@
-package main
+package internal
 
 import (
+	"log"
+	"os"
+	"path/filepath"
 	"testing"
 
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -11,6 +19,11 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
+	DB, err := OpenTestConnection()
+	if err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
 	rawDB, rawErr := DB.DB()
 	if rawErr != nil {
 		t.Errorf("Failed, got error: %v", rawErr)
@@ -20,4 +33,44 @@ func TestGORM(t *testing.T) {
 	DB.Transaction(func(tx *gorm.DB) error {
 		return nil
 	})
+}
+
+func OpenTestConnection() (db *gorm.DB, err error) {
+	dbDSN := os.Getenv("GORM_DSN")
+	switch os.Getenv("GORM_DIALECT") {
+	case "mysql":
+		log.Println("testing mysql...")
+		if dbDSN == "" {
+			dbDSN = "gorm:gorm@tcp(localhost:9910)/gorm?charset=utf8&parseTime=True&loc=Local"
+		}
+		db, err = gorm.Open(mysql.Open(dbDSN), &gorm.Config{PrepareStmt: true})
+	case "postgres":
+		log.Println("testing postgres...")
+		if dbDSN == "" {
+			dbDSN = "user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+		}
+		db, err = gorm.Open(postgres.Open(dbDSN), &gorm.Config{PrepareStmt: true})
+	case "sqlserver":
+		// CREATE LOGIN gorm WITH PASSWORD = 'LoremIpsum86';
+		// CREATE DATABASE gorm;
+		// USE gorm;
+		// CREATE USER gorm FROM LOGIN gorm;
+		// sp_changedbowner 'gorm';
+		log.Println("testing sqlserver...")
+		if dbDSN == "" {
+			dbDSN = "sqlserver://gorm:LoremIpsum86@localhost:9930?database=gorm"
+		}
+		db, err = gorm.Open(sqlserver.Open(dbDSN), &gorm.Config{PrepareStmt: true})
+	default:
+		log.Println("testing sqlite3...")
+		db, err = gorm.Open(sqlite.Open(filepath.Join(os.TempDir(), "gorm.db")), &gorm.Config{PrepareStmt: true})
+	}
+
+	if debug := os.Getenv("DEBUG"); debug == "true" {
+		db.Logger = db.Logger.LogMode(logger.Info)
+	} else if debug == "false" {
+		db.Logger = db.Logger.LogMode(logger.Silent)
+	}
+
+	return
 }
