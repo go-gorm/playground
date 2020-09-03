5d8dced5dcf19ba77964bfa70cecae5c2d3b068b
zzjin <zzjin@users.noreply.github.com>
2020-09-03 21:17:44 +0800

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+type Property struct {
+	ID        int64       `gorm:"column:id;primaryKey"`
+	PropKey   string      `gorm:"column:prop_key"`
+	PropValue interface{} `gorm:"column:prop_value"`
+}
+
+type PropValueDataA struct {
+	VideoURL string `json:"video_url"`
+}
+type PropValueDataB struct {
+	ImageID int64 `json:"image_id"`
+}
+
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	DB.Migrator().AutoMigrate(&Property{})
+	defer DB.Migrator().DropTable(&Property{})
 
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	if err := DB.Create(&Property{}); err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
