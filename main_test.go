601cd2f5daf327dd71d760b2a5b9982820fa6289
Vito Castellano <vito.castellano@facile.it>
2020-09-05 12:45:47 +0200

    Automigrate postgres error defaul

--- main_test.go
+++ main_test.go
@@ -5,16 +5,17 @@ import (
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
-// GORM_BRANCH: master
+// GORM_BRANCH: v1.20.0
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	type Product struct {
+		Value float64 `gorm:"default:1"`
+	}
 
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	for i := 0; i < 2; i++ {
+		if err := DB.AutoMigrate(&Product{}); err != nil {
+			t.Errorf("Failed, got error: %v", err)
+		}
 	}
 }
