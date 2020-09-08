1abe0b92acc152ce8297406b35f30e5d4b092210
Jonathan A. Sternberg <jonathan@sternberg.org>
2020-09-08 14:41:19 -0500

    Set max open connections to illustrate automigrate issue

--- main_test.go
+++ main_test.go
@@ -13,6 +13,21 @@ func TestGORM(t *testing.T) {
 
 	DB.Create(&user)
 
+	pet := Pet{UserID: &user.ID, Name: "fido"}
+
+	DB.Create(&pet)
+
+	sqlDB, err := DB.DB()
+	if err != nil {
+		t.Fatal(err)
+	}
+	sqlDB.SetMaxOpenConns(1)
+
+	// Rerun the migrations now that data is present.
+	if err := DB.AutoMigrate(&User{}, &Pet{}); err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
