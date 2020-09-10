d47c08359ac4ed9f1878bae7fd066fbd3a8439c1
ap-in-git <ashokpoudel023@gmail.com>
2020-09-10 14:57:24 +0545

    A second difference in updated at with composite key create

--- main_test.go
+++ main_test.go
@@ -1,20 +1,53 @@
 package main
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
 func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
 
 	DB.Create(&user)
+	var sameUser User
+	DB.Where("id =?", user.ID).First(&sameUser)
+	println(user.UpdatedAt.Format("2006-01-02 15:04:05"))
+	println(sameUser.UpdatedAt.Format("2006-01-02 15:04:05"))
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	totalItemCreated := 10
+	for i := 0; i < totalItemCreated; i++ {
+		rand.Seed(time.Now().UnixNano())
+
+		var job AdSet
+		job.AdsetName = randSeq(10)
+		job.AdsetId = uint64(i + 1)
+		job.Region = "[]"
+		job.Device = "[]"
+		job.Extra = "[]"
+		DB.Create(&job)
+		var sameJob AdSet
+		DB.Where("adset_id = ? ", i+1).First(&sameJob)
+
+		formatStr := "2006-01-02 15:04:05"
+
+		if job.UpdatedAt.Format(formatStr) != sameJob.UpdatedAt.Format(formatStr) {
+			t.Errorf("Expected %v got %v", sameJob.UpdatedAt.Format(formatStr), job.UpdatedAt.Format(formatStr))
+		}
 	}
+
 }
--- models.go
+++ models.go
@@ -11,6 +11,40 @@ import (
 // He works in a Company (belongs to), he has a Manager (belongs to - single-table), and also managed a Team (has many - single-table)
 // He speaks many languages (many to many) and has many friends (many to many - single-table)
 // His pet also has one Toy (has one - polymorphic)
+
+
+type Model struct {
+	ID        uint       `gorm:"primary_key," json:"id"`
+	CreatedAt *time.Time `json:"created_at,omitempty" gorm:"type:datetime"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty" gorm:"type:datetime"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty"  gorm:"type:datetime"`
+}
+
+type AdSet struct {
+	AdsetId          uint64     `json:"adset_id" gorm:"type:bigint;primary_key;autoIncrement:false"`
+	Source           string     `json:"source" gorm:"type:varchar(20);primary_key;autoIncrement:false"`
+	AccountCurrency  string     `json:"account_currency" gorm:"type:varchar(20)"`
+	AccountId        uint64     `json:"account_id" gorm:"type:bigint"`
+	CampaignId       uint64     `json:"campaign_id" gorm:"type:bigint"`
+	AdsetName        string     `json:"adset_name"`
+	BuyingType       string     `json:"buying_type"`
+	Objective        string     `json:"objective"`
+	CurrentBid       float64    `json:"current_bid"`
+	AdsetStatus      string     `json:"adset_status"`
+	DailyBudget      float64    `json:"daily_budget"`
+	BidStrategy      string     `json:"bid_strategy"`
+	CBO              bool       `json:"cbo"`
+	Region           string     `json:"region" gorm:"type:json"`
+	Country          string     `json:"country" gorm:"type:varchar(20)"`
+	DomainId         *uint16    `json:"domain_id" gorm:"type:smallint(5)"`
+	RedirectDomainId *uint16    `json:"redirect_domain_id" gorm:"type:smallint(5)"`
+	Device           string     `json:"device" gorm:"type:json"`
+	Extra            string     `json:"extra" gorm:"type:json"`
+	StartDate        *time.Time `json:"start_date"`
+	CreatedAt        *time.Time `json:"created_at,omitempty" gorm:"type:datetime"`
+	UpdatedAt        *time.Time `json:"updated_at,omitempty" gorm:"type:datetime"`
+}
+
 type User struct {
 	gorm.Model
 	Name      string
@@ -28,6 +62,7 @@ type User struct {
 	Friends   []*User    `gorm:"many2many:user_friends"`
 	Active    bool
 }
+
 
 type Account struct {
 	gorm.Model
