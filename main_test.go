5e946fdc4c40f1f3a1d7889d71e95b0aece5f59e
unkn0wn <unkn0wn@rocinante.io>
2020-09-11 09:36:19 +0800

    Bug

--- main_test.go
+++ main_test.go
@@ -15,6 +15,10 @@ type Bugs struct {
 	Foo string
 }
 
+// GORM_REPO: https://github.com/go-gorm/gorm.git
+// GORM_BRANCH: master
+// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+
 func TestGORM(t *testing.T) {
 	if err := DB.AutoMigrate(new(Bugs)); err != nil {
 		t.Errorf("Failed, got error: %v", err)
