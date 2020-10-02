d07400720d567a51fdf544b8ebd12cbd3eb1d51b
facundomedica <14063057+facundomedica@users.noreply.github.com>
2020-10-02 11:16:18 -0300

    Save has an unexpected behavior

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +11,13 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
+	// I'll try to update a non existent user WITH PK, which shouldn't insert a new record
+	// (Save update value in database, if the value doesn't have primary key, will insert it)
+	user := User{Model: gorm.Model{ID: 1234567890}, Name: "somename"}
+	DB.Save(&user)
 
 	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	if err := DB.First(&result, user.ID).Error; err == nil {
+		t.Errorf("The user was created, which is not what the docs say.")
 	}
 }
