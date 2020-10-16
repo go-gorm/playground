b800c1a225ce7495e328c8b9e5c838bde6846e9e
Seon-Il Kim <seon1.kim@samsung.com>
2020-10-16 15:08:48 +0900

    Add check preload

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"gorm.io/gorm/clause"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -17,10 +18,24 @@ func TestGORM(t *testing.T) {
 	}
 
 	id := "s-1"
-
+	val := "val1"
 	if err := DB.Create(&SomeModel{
 		SomeId: &id,
+		SomeAtt: &SomeAttModel{
+			Value: &val,
+		}
 	}).Error; err != nil {
 		t.Errorf("failed to create SomeModel: %v", err)
+	}
+
+	result := &SomeModel{}
+	if err := DB.Preload(clause.Associations).Take(result).Error; err!= nil {
+		t.Errorf("failed to get result: %v", err)
+	}
+	if result.SomeAtt == nil {
+		t.Errorf("SomeAtt is nil")
+	}
+	if *result.SomeAtt.Value != val {
+		t.Errorf("val is incorrect")
 	}
 }
