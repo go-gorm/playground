b362be5968843f1a3a9f0d3c2b18bf9d5704d777
frederikhors <41120635+frederikhors@users.noreply.github.com>
2020-10-21 01:20:07 +0200

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"testing"
+	"strconv"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: postgres
 
 func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
 
 	DB.Create(&user)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	var deletedUser User
+	err := dbPG.
+		Where("created_at < NOW() - INTERVAL ?", strconv.Itoa(10)+" hours").
+		Delete(&deletedUser).Error
+	
+	if err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
