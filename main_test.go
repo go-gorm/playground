64577692ac3aedbdb4e3daf10e61824e308fcf7a
Michelle Laurenti <asmeikal@me.com>
2020-10-21 16:52:23 +0200

    update tests

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"gorm.io/gorm"
 	"os"
 	"testing"
 )
@@ -11,30 +13,75 @@ import (
 // GORM_BRANCH: master
 // TEST_DRIVERS: mysql, postgres, sqlserver
 
-func TestColumnTypeLengthInfo(t *testing.T) {
+func getColumn(value interface{}, name string) (gorm.ColumnType, error) {
 	columns, err := DB.Migrator().ColumnTypes(&User{})
 	if err != nil {
-		t.Errorf("Failed, got error: %v", err)
+		return nil, fmt.Errorf("failed to get columns for entity %v: %w", value, err)
 	}
 
 	for _, c := range columns {
-		if c.Name() == "string_len" {
-			if _, ok := c.Length(); !ok {
-				t.Errorf("%s: missing length information on column %s", os.Getenv("GORM_DIALECT"), c.Name())
-			}
+		if c.Name() == name {
+			return c, nil
 		}
+	}
+
+	return nil, fmt.Errorf("column %s not found", name)
+}
+
+func TestHasTable(t *testing.T) {
+	out := DB.Migrator().HasTable(&User{})
+
+	if !out {
+		t.Errorf("table for %#v not found", &User{})
+	}
+}
+
+func TestHasColumn(t *testing.T) {
+	out := DB.Migrator().HasColumn(&User{}, "name")
+
+	if !out {
+		t.Errorf("column name not found")
+	}
+}
+
+func TestColumnTypeLengthInfo(t *testing.T) {
+	columnsWithLength := []string{
+		"string_len",
+	}
+
+	for _, columnName := range columnsWithLength {
+		t.Run(fmt.Sprintf("column %s has length", columnName), func(t *testing.T) {
+			column, err := getColumn(&User{}, columnName)
+
+			if err != nil {
+				t.Errorf("Failed, got error: %v", err)
+			}
+
+			if _, ok := column.Length(); !ok {
+				t.Errorf("%s: missing length information on column %s", os.Getenv("GORM_DIALECT"), column.Name())
+			}
+		})
 	}
 }
 
 func TestColumnTypeNullableInfo(t *testing.T) {
-	columns, err := DB.Migrator().ColumnTypes(&User{})
-	if err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	columnsWithNullable := []string{
+		"name",
+		"age",
+		"string_len",
 	}
 
-	for _, c := range columns {
-		if _, ok := c.Nullable(); !ok {
-			t.Errorf("%s: missing nullable information on column %s", os.Getenv("GORM_DIALECT"), c.Name())
-		}
+	for _, columnName := range columnsWithNullable {
+		t.Run(fmt.Sprintf("column %s has nullable", columnName), func(t *testing.T) {
+			column, err := getColumn(&User{}, columnName)
+
+			if err != nil {
+				t.Errorf("Failed, got error: %v", err)
+			}
+
+			if _, ok := column.Nullable(); !ok {
+				t.Errorf("%s: missing nullable information on column %s", os.Getenv("GORM_DIALECT"), column.Name())
+			}
+		})
 	}
 }
