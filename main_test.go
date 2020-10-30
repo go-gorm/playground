d5852fa79e9a50bd9f98b0689a51cc844f6d064b
David Lawrence <david.lawrence@smithrx.com>
2020-10-30 14:34:37 -0700

    issue has moved to indexes

--- main_test.go
+++ main_test.go
@@ -9,21 +9,20 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 type OtherSchemaTable struct {
-    AColumn string	
+	AColumn string `gorm:"primary_key"`
 }
 
 func (OtherSchemaTable) TableName() string {
-    return "otherschema.other_schema_table"	
+	return "otherschema.other_schema_table"
 }
-
 
 func TestGORM(t *testing.T) {
 	if err := DB.AutoMigrate(&OtherSchemaTable{}); err != nil {
-		t.Errorf("Failed to automigrate: %s", err)	
+		t.Errorf("Failed to automigrate: %s", err)
 	}
-	
+
 	result := make([]OtherSchemaTable, 0)
-	
+
 	if err := DB.Find(&result).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
