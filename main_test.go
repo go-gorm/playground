86b68b06b7a4537aaba79fa2fe3886a9c176d6b6
petchctep <32630922+petchnattkit@users.noreply.github.com>
2020-11-02 03:46:01 +0700

    Reproduces a Table api bug
    I'm doing a 'from subquery' following by this docs https://gorm.io/docs/advanced_query.html#From-SubQuery
    but when I pass subquery arguments into table api, it result an error "too many arguments".

--- main_test.go
+++ main_test.go
@@ -10,10 +10,17 @@ import (
 
 func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
-
+	pet := Pet{Name: "snow"}
 	DB.Create(&user)
-
+	DB.Create(&pet)
+	
 	var result User
+	subQuery1 := DB.Model(&User{}).Select("name")
+	subQuery2 := DB.Model(&Pet{}).Select("name")
+
+	db.Table("(?) as u, (?) as p", subQuery1, subQuery2).Find(&User{})
+
+	// SELECT * FROM (SELECT `name` FROM `users`) as u, (SELECT `name` FROM `pets`) as p
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
