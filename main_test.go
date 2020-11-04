1988a7cdc41cd33163b49fa12284bf03bb37e0d9
Pourqavam <39864929+Pourqavam@users.noreply.github.com>
2020-11-04 12:05:48 +0330

    Primary key of embedded struct returns on insert

--- main_test.go
+++ main_test.go
@@ -4,17 +4,26 @@ import (
 	"testing"
 )
 
+type User struct {
+	ID         int `gorm:"primaryKey"`
+	Name       string
+	CompanyID  int
+
+	Company    *Company `gorm:"embedded;embeddedPrefix:com"`
+}
+
+type Company struct {
+	ID        int `gorm:"primaryKey"`
+	Name      string
+}
+
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	user := User{Name: "jinzhu", CompanyID: 1}
+	if err := DB.Create(&user).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
