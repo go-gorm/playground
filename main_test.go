cdda004a2f4d89c096891ffdd4542ea964742a7b
keleqnma <cyq65536@gmail.com>
2020-11-11 17:02:13 +0800

    fail: text index not-support

--- main_test.go
+++ main_test.go
@@ -2,41 +2,31 @@ package main
 
 import (
 	"testing"
-
-	"gorm.io/gorm"
+	"time"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
-type TestUniqueIdx struct {
-	gorm.Model
-	Foo string `form:"foo" json:"foo" gorm:"unique_index:idx_foo_bar"`
-	Bar string `form:"bar" json:"bar" gorm:"unique_index:idx_foo_bar"`
+type Model struct {
+	ID        uint       `json:"id" gorm:"primary_key"`
+	CreatedAt time.Time  `json:"created_at" gorm:"index"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"-" sql:"index"`
+}
+
+type Test struct {
+	Model
+	Foo string `form:"foo" json:"foo" gorm:"uniqueIndex:idx_tests_foo_bar"`
+	Bar string `form:"bar" json:"bar" gorm:"uniqueIndex:idx_tests_foo_bar"`
 	Baz int    `form:"baz" json:"baz"  gorm:"default:500"`
-}
-
-func addTestUniqueIdx(test TestUniqueIdx) error {
-	return DB.Create(&test).Error
-}
-
-func updateTestUniqueIdxMap(test map[string]interface{}) error {
-	return DB.Model(TestUniqueIdx{}).Where("foo = ?", test["foo"]).Updates(test).Error
-}
-
-func updateTestUniqueIdx(test TestUniqueIdx) error {
-	return DB.Model(&TestUniqueIdx{}).Where("foo = ?", test.Foo).Updates(test).Error
-}
-
-func saveTestUniqueIdx(test TestUniqueIdx) error {
-	return DB.Model(&TestUniqueIdx{}).Where("foo = ?", test.Foo).Save(&test).Error
 }
 
 func TestGORM(t *testing.T) {
 	foo := "jhbcxwc"
 	bar := "wbwkxccw"
-	test := TestUniqueIdx{
+	test := Test{
 		Foo: foo,
 		Bar: bar,
 		Baz: 100,
@@ -46,23 +36,39 @@ func TestGORM(t *testing.T) {
 	testMap["bar"] = bar
 	testMap["baz"] = 100
 
-	if err := DB.AutoMigrate(&TestUniqueIdx{}); err != nil {
+	if err := DB.AutoMigrate(&Test{}); err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 
-	if err := addTestUniqueIdx(test); err != nil {
+	if hasIndex := DB.Migrator().HasIndex(&Test{}, "idx_tests_created_at"); !hasIndex {
+		t.Errorf("Fail to create index idx_tests_created_at")
+	}
+
+	if hasIndex := DB.Migrator().HasIndex(&Test{}, "idx_tests_foo_bar"); !hasIndex {
+		t.Errorf("Fail to create index idx_tests_foo_bar")
+	}
+
+	if err := DB.Create(&test).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 
-	if err := updateTestUniqueIdxMap(testMap); err != nil {
+	if err := DB.Where("foo = ?", test.Foo).Delete(&test).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 
-	if err := updateTestUniqueIdx(test); err != nil {
+	if err := DB.Model(&test).Create(testMap).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 
-	if err := saveTestUniqueIdx(test); err != nil {
+	if err := DB.Model(Test{}).Where("foo = ?", testMap["foo"]).Updates(testMap).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	if err := DB.Model(&Test{}).Where("foo = ?", test.Foo).Updates(test).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	if err := DB.Model(&Test{}).Where("foo = ?", test.Foo).Save(&test).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
