0ebcd92347faf29d48d97ad42c47e5447ceccdac
Jeff P <6480906+JeffPHP@users.noreply.github.com>
2020-11-14 18:34:41 +0100

    Update main_test.go
    A single FirstOrCreate triggers an Error log when User is not found, despite catching errors. 
    It seems there's no way to dismiss this log, except by setting Logger to Silent ...

--- main_test.go
+++ main_test.go
@@ -10,11 +10,8 @@ import (
 
 func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	
+	if err := DB.FirstOrCreate(&user).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
