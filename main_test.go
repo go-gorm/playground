c351b0d8c67eab39e863993a5b5804a871d5a990
AlessandroSechi <alessandro.sechi@yahoo.it>
2020-11-28 19:28:26 +0100

    AfterCreate not firing MySQL

--- main_test.go
+++ main_test.go
@@ -1,20 +1,58 @@
 package main
 
 import (
+	"fmt"
+	"gorm.io/gorm"
 	"testing"
+	"time"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: mysql
+
+type Person struct {
+	ID        int
+	Name      string
+	Addresses []Address `gorm:"many2many:person_addresses;"`
+}
+
+type Address struct {
+	ID   uint
+	Name string
+}
+
+type PersonAddress struct {
+	PersonID  int
+	AddressID int
+	Home      bool
+	CreatedAt time.Time
+	DeletedAt gorm.DeletedAt
+}
+
+func (p *PersonAddress) AfterCreate(db *gorm.DB) error {
+	db.Model(p).Where(p).Update("home", true)
+	return nil
+}
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
 
-	DB.Create(&user)
+	DB.AutoMigrate(&Address{})
+	DB.AutoMigrate(&Person{})
+	DB.AutoMigrate(&PersonAddress{})
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	err := DB.SetupJoinTable(&Person{}, "Addresses", &PersonAddress{})
+
+	if err != nil {
+		fmt.Println(err.Error())
 	}
+
+	DB.Debug().Create(&Person{
+		ID:   1,
+		Name: "Joe",
+		Addresses: []Address{{
+			ID:   1,
+			Name: "First Address",
+		}},
+	})
 }
