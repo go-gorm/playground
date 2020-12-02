2737456344356af77316114ce7ec2795ad28994c
baibaratsky <andrei@baibaratsky.com>
2020-12-02 18:02:05 +0100

    Associations creation with composite primary key

--- main_test.go
+++ main_test.go
@@ -9,12 +9,36 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	user := User{
+		UnitID: 8,
+		ID:     1,
+		Name:   "Andrei",
+		Projects: []Project{
+			{
+				ID:    1,
+				Title: "First project",
+			},
+			{
+				ID:    2,
+				Title: "Second project",
+			},
+		},
+	}
 
-	DB.Create(&user)
+	if err := DB.Create(&user).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
 
 	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	if err := DB.Where(&User{UnitID: user.UnitID, ID: user.ID}).First(&result).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+
+	var projects []Project
+	if err := DB.Where(&Project{UnitID: user.UnitID, UserID: user.ID}).Find(&projects).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	if len(projects) != len(user.Projects) {
+		t.Errorf("Failed, projects length should be %d, got %d", len(user.Projects), len(result.Projects))
 	}
 }
