e5eacae425f869b52ba47996f5735fa61c287ebb
aveyuan <aveyuan@163.com>
2020-12-02 16:56:20 +0800

    count select

--- main_test.go
+++ main_test.go
@@ -9,7 +9,7 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	user := User{Name: "jinzhu", Age: 30}
 
 	DB.Create(&user)
 
@@ -17,4 +17,15 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	var count int64
+	var all []User
+	if err := DB.Model(new(User)).Select("age").Count(&count).Find(&all).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	if all[0].Name == "jinzhu" {
+		t.Error("Failed, Select query is age, name should is null")
+	}
+
 }
