95d89bec46cb70226160096531f69c6ed3b4daf0
tsaikd <tsaikd@gmail.com>
2020-12-04 17:55:53 +0900

    Method Chain Safety/Goroutine Safety not working

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module gorm.io/playground
 go 1.14
 
 require (
+	github.com/stretchr/testify v1.6.1
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/driver/postgres v1.0.5
-	gorm.io/driver/sqlite v1.1.3
+	gorm.io/driver/sqlite v1.1.4
 	gorm.io/driver/sqlserver v1.0.5
-	gorm.io/gorm v1.20.7
+	gorm.io/gorm v1.20.8
 )
 
 replace gorm.io/gorm => ./gorm
--- main_test.go
+++ main_test.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"context"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+type Table struct{ Value int }
+
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	ctx := context.Background()
+	tx1 := DB.Begin()
+	defer tx1.Rollback()
 
-	DB.Create(&user)
+	require.NoError(t, tx1.AutoMigrate(&Table{}))
+	require.NoError(t, tx1.Create(&Table{Value: 1}).Error)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	t.Run("query once", func(t *testing.T) {
+		require.NoError(t, query(ctx, tx1, 1))
+	})
+
+	t.Run("query 5 times", func(t *testing.T) {
+		require.NoError(t, query(ctx, tx1, 5))
+	})
+}
+
+func query(ctx context.Context, tx *gorm.DB, times int) error {
+	eg, ctx := errgroup.WithContext(ctx)
+
+	for i := 0; i < times; i++ {
+		eg.Go(func() error {
+			return tx.WithContext(ctx).Transaction(func(tx2 *gorm.DB) error {
+				return tx2.Take(&Table{}).Error
+			})
+		})
 	}
+
+	return eg.Wait()
 }
