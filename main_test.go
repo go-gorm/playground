81c9bb6e26c3bab2ab7717c366b18bcad3450cc6
Mario Hros <git@reversity.org>
2020-12-04 20:18:27 +0100

    embedded with same field name bug

--- main_test.go
+++ main_test.go
@@ -8,13 +8,51 @@ import (
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+// XUserSpec is user-supplied "specification"
+type XUserSpec struct {
+	ID   uint64
+	Name string
+
+	Languages []string `gorm:"-"`
+}
+
+type XLanguage struct {
+	Code string `gorm:"primarykey"`
+}
+
+// XUser is runtime object
+type XUser struct {
+	XUserSpec `godm:"embedded"`
+
+	Languages []XLanguage `gorm:"many2many:XUserSpeak"`
+}
+
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	if err := DB.AutoMigrate(&XLanguage{}, &XUser{}); err != nil {
+		//t.Fatal(err)
+	}
 
-	DB.Create(&user)
+	spec := XUserSpec{
+		Name:      "jinzhu",
+		Languages: []string{"English", "Chinese"},
+	}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	user := XUser{XUserSpec: spec}
+
+	for _, lang := range spec.Languages {
+		user.Languages = append(user.Languages, XLanguage{Code: lang})
+	}
+
+	if err := DB.Create(&user).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	var result XUser
+	if err := DB.Preload("Languages").First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+
+	if len(result.Languages) == 0 {
+		t.Errorf("No languages assigned")
 	}
 }
