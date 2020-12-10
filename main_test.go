987c50d9651fb7e4734412509fe2b12c12812f43
xXlokerXx <carlos.lares@4mas1.com>
2020-12-10 17:51:00 -0600

    Error on find function

--- main_test.go
+++ main_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -16,5 +19,15 @@ func TestGORM(t *testing.T) {
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+
+	// error is here
+	// when use find the error is nill
+	if err := DB.Find(&result, "id = ?", 0).Error; errors.Is(err, gorm.ErrRecordNotFound) {
+		t.Error("Record not found")
+	}
+
+	if err := DB.First(&result, "id = ?", 0).Error; errors.Is(err, gorm.ErrRecordNotFound) {
+		t.Error("Record not found")
 	}
 }
