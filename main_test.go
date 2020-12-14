fb773200be0e460e7b042279fb01e9ece157eb4a
shangyin <silverg@163.com>
2020-12-14 11:45:46 +0800

    关于使用GROUP_CONCAT之后SCAN字段不渲染的问题

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -9,12 +10,76 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	orders := []Order{
+		{
+			CoinType:             "ADA",
+			Price:                "2",
+			Total:                "5",
+			AccountID:            "1",
+			TransactionAccountID: "1",
+			Exchange:             "a",
+			ContractType:         "aa",
+			State:                "-1",
+		},
+		{
+			CoinType:             "ADA",
+			Price:                "4",
+			Total:                "10",
+			AccountID:            "1",
+			TransactionAccountID: "1",
+			Exchange:             "a",
+			ContractType:         "aa",
+			State:                "-1",
+		},
+		{
+			CoinType:             "bbc",
+			Price:                "3",
+			Total:                "6",
+			AccountID:            "2",
+			TransactionAccountID: "2",
+			Exchange:             "a",
+			ContractType:         "aa",
+			State:                "-1",
+		},
+	}
+	
+	DB.Create(&orders)
+	
+	account := []TransactionAccount{
+		{
+			AccountID:    "1",
+			APIKey:       "11",
+			APISecret:    "11",
+			Exchange:     "a",
+			Passphrase:   "11",
+			ContractType: "aa",
+		},
+		{
+			AccountID:    "2",
+			APIKey:       "22",
+			APISecret:    "22",
+			Exchange:     "a",
+			Passphrase:   "22",
+			ContractType: "aa",
+		},
+	}
+	DB.Create(&account)
+	var (
+		exchange     = "a"
+		contractType = "aa"
+		state        = "-1"
+	)
+	
+	var result StatisticsDetail
+	if err := DB.Model(&Order{}).Debug().
+		Select("SUM( total ) as total_price,`orders`.coin_type,any_value(transaction_accounts.api_key) as api_key,"+
+			"any_value(transaction_accounts.api_secret) as api_secret,any_value(transaction_accounts.passphrase) as passphrase,"+
+			"GROUP_CONCAT(`orders`.transaction_account_id) as ids").
+		Joins("left JOIN transaction_accounts on transaction_accounts.id = `orders`.transaction_account_id").
+		Where("`orders`.exchange =? AND `orders`.contract_type=? AND `orders`.state=?", exchange, contractType, state).
+		Group("`orders`.transaction_account_id,`orders`.coin_type").Scan(&result).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	fmt.Printf("%#v\n", result)
 }
--- models.go
+++ models.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"database/sql"
-	"time"
 
 	"gorm.io/gorm"
 )
@@ -11,50 +9,36 @@ import (
 // He works in a Company (belongs to), he has a Manager (belongs to - single-table), and also managed a Team (has many - single-table)
 // He speaks many languages (many to many) and has many friends (many to many - single-table)
 // His pet also has one Toy (has one - polymorphic)
-type User struct {
+
+// Order .
+type Order struct {
 	gorm.Model
-	Name      string
-	Age       uint
-	Birthday  *time.Time
-	Account   Account
-	Pets      []*Pet
-	Toys      []Toy `gorm:"polymorphic:Owner"`
-	CompanyID *int
-	Company   Company
-	ManagerID *uint
-	Manager   *User
-	Team      []User     `gorm:"foreignkey:ManagerID"`
-	Languages []Language `gorm:"many2many:UserSpeak"`
-	Friends   []*User    `gorm:"many2many:user_friends"`
-	Active    bool
+	CoinType             string `json:"coin_type" gorm:"type:varchar(32);Index;not null;default:'';comment:''"`
+	Price                string `json:"price" gorm:"type:varchar(10);not null;default:'';comment:''"`
+	Total                string `json:"total" gorm:"type:varchar(32);not null;default:0;comment:''"`
+	Exchange             string `json:"exchange" gorm:"type:varchar(32);default:0;Index;comment:''"`
+	ContractType         string `json:"contract_type" gorm:"type:varchar(32);Index;not null;default:'';comment:''"`
+	AccountID            string `json:"account_id" gorm:"type:char(32);not null;default:'';comment:''"`
+	TransactionAccountID string `json:"transaction_account_id" gorm:"type:char(32);not null;default:'';comment:''"`
+	State                string `json:"state" gorm:"type:varchar(255);default:0;comment:''"`
 }
 
-type Account struct {
+type TransactionAccount struct {
 	gorm.Model
-	UserID sql.NullInt64
-	Number string
+	AccountID    string `json:"account_id" gorm:"type:char(32);Index;comment:''"`
+	APIKey       string `json:"api_key" gorm:"type:varchar(255);comment:'';"`
+	APISecret    string `json:"api_secret" gorm:"type:varchar(255);comment:''"`
+	Exchange     string `json:"exchange" gorm:"type:varchar(32);default:0;Index;comment:''"`
+	Passphrase   string `json:"passphrase"  gorm:"type:varchar(255);comment:''"`
+	ContractType string `json:"contract_type" gorm:"type:varchar(32);Index;not null;default:'';comment:''"`
 }
 
-type Pet struct {
-	gorm.Model
-	UserID *uint
-	Name   string
-	Toy    Toy `gorm:"polymorphic:Owner;"`
-}
-
-type Toy struct {
-	gorm.Model
-	Name      string
-	OwnerID   string
-	OwnerType string
-}
-
-type Company struct {
-	ID   int
-	Name string
-}
-
-type Language struct {
-	Code string `gorm:"primarykey"`
-	Name string
+// StatisticsDetail .
+type StatisticsDetail struct {
+	TotalPrice float64 `json:"total_price"`
+	CoinType   string  `json:"coin_type"`
+	APIKey     string  `json:"api_key"`
+	APISecret  string  `json:"api_secret"`
+	Passphrase string  `json:"passphrase"`
+	IDS        string  `json:"ids"`
 }
