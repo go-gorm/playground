75151d756ce7cf87e015d597a06ce15b48f20acd
Kevin S Kirkup <kevin.kirkup@pureport.com>
2020-12-14 16:29:21 -0500

    Add Test for checking DeletedAt when softdeleted

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	gorm "gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -11,10 +13,24 @@ import (
 func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
 
+	zero := gorm.DeletedAt{}
+
 	DB.Create(&user)
 
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+
+	if result.Model.DeletedAt != zero {
+		t.Errorf("Failed, DeletedAt should be empty")
+	}
+
+	if err := DB.Delete(&result).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	if result.Model.DeletedAt == zero {
+		t.Errorf("Failed, DeletedAt is empty")
 	}
 }
