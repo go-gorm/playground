e6a98890017b26d9087051854b5bc8ee711ffab5
Kevin S Kirkup <kevin.kirkup@pureport.com>
2020-12-14 19:30:50 -0500

    Update with Select doesn't update UpdateAt

--- main_test.go
+++ main_test.go
@@ -9,6 +9,13 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
+
+	NormalUpdate(t)
+	FailingUpdate(t)
+
+}
+
+func NormalUpdate(t *testing.T) {
 	user := User{Name: "jinzhu"}
 
 	DB.Create(&user)
@@ -17,4 +24,47 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	updates := map[string]interface{}{}
+	updates["Name"] = "Blah"
+
+	if err := DB.Model(&result).Updates(updates).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	if result.Name != "Blah" {
+		t.Errorf("Failed, DeletedAt is empty")
+	}
+
+	if result.Model.UpdatedAt == user.Model.UpdatedAt {
+		t.Errorf("Failed, UpdatedAt not updated")
+	}
+}
+
+func FailingUpdate(t *testing.T) {
+	user := User{Name: "jinzhu"}
+
+	DB.Create(&user)
+
+	var result User
+	if err := DB.First(&result, user.ID).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	updates := map[string]interface{}{}
+	updates["Name"] = "Blah"
+	fields := []string{"Name"}
+
+	if err := DB.Model(&result).Select(fields).Updates(updates).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	if result.Name != "Blah" {
+		t.Errorf("Failed, DeletedAt is empty")
+	}
+
+	if result.Model.UpdatedAt == user.Model.UpdatedAt {
+		t.Errorf("Failed, UpdatedAt not updated")
+	}
+
 }
