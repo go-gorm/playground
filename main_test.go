b7c269eb4a32b6aeab0ca31a5b1df28a06acbdd2
si9ma <si9ma@si9ma.com>
2020-12-27 17:04:54 +0000

    ? in Select not be replaced to argument in v1.20.9

--- main_test.go
+++ main_test.go
@@ -17,4 +17,7 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+	if _, err := DB.Table("users").Select("COALESCE(age,?)", "42").Rows(); err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
 }
