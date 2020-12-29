1e6e26110e44a584fefc6df4df7dbdec9c3623c2
Omar A. Hachach <contact@omarh.net>
2020-12-29 22:49:01 +0100

    Add test case

--- main_test.go
+++ main_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"database/sql"
+	"gorm.io/gorm/clause"
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -16,5 +20,16 @@ func TestGORM(t *testing.T) {
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+
+	var result2 User
+	stmt := DB.Session(&gorm.Session{DryRun: true}).Where(User{Nickname: sql.NullString{String: "Value", Valid: true}}).First(&result2).Statement
+	if len(stmt.Clauses["WHERE"].Expression.(clause.Where).Exprs) < 2 {
+		t.Errorf("Failed, WHERE \"users\".\"nickname\" = \"Value\" clause not added.")
+	}
+
+	stmt = DB.Session(&gorm.Session{DryRun: true}).Where(User{Nickname: sql.NullString{String: "", Valid: false}}).First(&result2).Statement
+	if len(stmt.Clauses["WHERE"].Expression.(clause.Where).Exprs) < 2 { // 2 because it will check WHERE "users"."deleted_at".
+		t.Errorf("Failed, WHERE \"users\".\"nickname\" IS NULL clause not added.")
 	}
 }
--- models.go
+++ models.go
@@ -27,6 +27,7 @@ type User struct {
 	Languages []Language `gorm:"many2many:UserSpeak"`
 	Friends   []*User    `gorm:"many2many:user_friends"`
 	Active    bool
+	Nickname  sql.NullString
 }
 
 type Account struct {
