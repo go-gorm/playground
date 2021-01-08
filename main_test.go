a5d6160e4dd9bd0bb40a394e8ee758cddc0a8168
zzjin <zzjin@users.noreply.github.com>
2021-01-08 17:49:01 +0800

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -14,13 +14,16 @@ func TestGORM(t *testing.T) {
 	DB.Create(&user)
 	
 	//disable default transction due to mysql already done it inside.
-	DB.SkipDefaultTransaction = true
-
-	if err := DB.Where("id=?", user.ID).Delete(&User{}).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
-	}
-	//again, panic
-	if err := DB.Where("id=?", user.ID).Delete(&User{}).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	if os.Getenv("GORM_DIALECT") == "mysql" {
+		db, _ := gorm.Open(mysql.Open("gorm:gorm@tcp(localhost:9910)/gorm?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{
+			SkipDefaultTransaction: true,
+		})
+		if err := db.Where("id=?", user.ID).Delete(&User{}).Error; err != nil {
+			t.Errorf("Failed, got error: %v", err)
+		}
+		//again, panic
+		if err := db.Where("id=?", user.ID).Delete(&User{}).Error; err != nil {
+			t.Errorf("Failed, got error: %v", err)
+		}
 	}
 }
