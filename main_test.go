e4e72581a34b2f9e38304767cfdfeab1e6d6ac9f
zzjin <zzjin@users.noreply.github.com>
2021-01-08 18:15:41 +0800

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -13,21 +13,31 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-	
 	//disable default transction due to mysql already done it inside.
 	if os.Getenv("GORM_DIALECT") == "mysql" {
 		db, _ := gorm.Open(mysql.Open("gorm:gorm@tcp(localhost:9910)/gorm?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{
 			PrepareStmt:            true,
 			SkipDefaultTransaction: true,
 		})
-		if err := db.Where("id=?", user.ID).Delete(&User{}).Error; err != nil {
+		
+		type Test struct {
+			ID   int64 `gorm:"primaryKey"`
+			Name string
+		}
+
+		if err = DB.AutoMigrate(&Test{}); err != nil {
+			log.Printf("Failed to auto migrate, but got error %v\n", err)
+			os.Exit(1)
+		}
+		
+		user := Test{Name: "zzjin"}
+		DB.Create(&user)
+		
+		if err := db.Where("id=?", user.ID).Delete(&Test{}).Error; err != nil {
 			t.Errorf("Failed, got error: %v", err)
 		}
 		//again, panic
-		if err := db.Where("id=?", user.ID).Delete(&User{}).Error; err != nil {
+		if err := db.Where("id=?", user.ID).Delete(&Test{}).Error; err != nil {
 			t.Errorf("Failed, got error: %v", err)
 		}
 	}
