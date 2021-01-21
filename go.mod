f1084cc19399d3b9dfd468ca4021c1a700b69dfc
Dirk Nilius <dirk.nilius@cgi.com>
2021-01-21 16:25:41 +0100

    Reproduction on bug report

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gorm.io/playground
 go 1.14
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/driver/postgres v1.0.5
 	gorm.io/driver/sqlite v1.1.4
--- main_test.go
+++ main_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"errors"
+	"github.com/DATA-DOG/go-sqlmock"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"testing"
 )
 
@@ -17,4 +21,42 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+}
+
+type Model struct {
+	gorm.Model
+}
+
+func TestAutomigrate(t *testing.T) {
+	dbMock, sqlMock, err := sqlmock.New()
+	if err != nil {
+		t.Errorf("sqlmock could not be created")
+	}
+	defer func() {
+		if err := sqlMock.ExpectationsWereMet(); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	db, err := gorm.Open(postgres.New(postgres.Config{Conn: dbMock}), &gorm.Config{})
+	if err != nil {
+		t.Errorf("db could not be created")
+	}
+
+	// This test passing would be the expected behaviour
+	t.Run("fail on create table", func(t *testing.T) {
+		errMsg := "sql error"
+		sqlMock.ExpectExec("CREATE TABLE").WillReturnError(errors.New(errMsg))
+		// uncomment this will make the test pass
+		//sqlMock.ExpectExec("CREATE INDEX").WillReturnError(errors.New(errMsg))
+
+		err = db.AutoMigrate(&Model{})
+		if err == nil {
+			t.Errorf("expected migration to return error")
+		}
+
+		if err.Error() != errMsg {
+			t.Errorf("expected migration failed with %s, but got %s", errMsg, err.Error())
+		}
+	})
 }
