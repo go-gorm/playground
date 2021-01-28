cb8f3cc17d21a7f09ba8e72808468bd68e94254b
Erik Müller <e.mueller@detelling.com>
2021-01-28 12:06:16 +0100

    add test preloading all associations together with nested associations

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gorm.io/gorm/clause"
 	"testing"
 )
 
@@ -9,12 +10,29 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	c1 := C{Name: "c1"}
+	c2 := C{Name: "c2"}
 
-	DB.Create(&user)
+	b1 := B{C: c1, Name: "b1"}
+	b2 := B{C: c2, Name: "b2"}
+	b3 := B{C: c1, Name: "b3"}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	a1 := A{BB: []B{b1,b2}, Name: "a1"}
+	a2 := A{BB: []B{b3}, Name: "a2"}
+
+	aa := []A{a1,a2}
+
+	if err := DB.Create(&aa).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	// BUG: preloading all associations together with nested associations fails.
+	tx := DB.Where(A{Name: a1.Name}).
+		Preload(clause.Associations).
+		Preload("BB.C")
+
+	var res A
+	if err := tx.Find(&res).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
