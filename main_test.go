754089147885e389edb050e1f654e93021c688b3
Iman Namvar <iman.namvar@gmail.com>
2021-01-31 14:21:56 +0330

    test: float number set scale  tag

--- main_test.go
+++ main_test.go
@@ -9,12 +9,16 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	points := 16.37
+	user := User{Name: "jinzhu", Points:points}
 
 	DB.Create(&user)
 
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+    if result.Points != points {
+		t.Errorf("Failed, points should be %f, but is %f ", points, result.Points)
 	}
 }
--- models.go
+++ models.go
@@ -27,6 +27,7 @@ type User struct {
 	Languages []Language `gorm:"many2many:UserSpeak"`
 	Friends   []*User    `gorm:"many2many:user_friends"`
 	Active    bool
+	Points    float64    `gorm:"precision:16;scale:2"`
 }
 
 type Account struct {
