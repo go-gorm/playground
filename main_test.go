d7273506cc4e986c5a098f03318eba638856fd01
abelseniorly <abel@seniorly.com>
2021-02-05 22:21:37 +0530

    field update error test

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm/clause"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -17,4 +19,48 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+}
+
+func TestUserCompany(t *testing.T) {
+	user := User{
+		Name: "jinzhu",
+		Company: Company{
+			Name: "TestCompany",
+		},
+	}
+
+	if err := DB.Create(&user).Error; err != nil {
+		t.Errorf("Failed to create user. %v", err.Error())
+		return
+	}
+
+	if user.CompanyID == nil || *user.CompanyID == 0 {
+		t.Errorf("CompanyID field not updated")
+		return
+	}
+	var result User
+	if err := DB.Preload(clause.Associations).First(&result, user.ID).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+		return
+	}
+	if result.Company.Name == "" {
+		t.Errorf("company not saved")
+		return
+	}
+
+	user.Company.Name = "NewTestCompany"
+	if err := DB.Save(&user).Error; err != nil {
+		t.Errorf("Failed to update user. %v", err.Error())
+		return
+	}
+
+	if err := DB.Preload(clause.Associations).First(&result, user.ID).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+		return
+	}
+	if result.Company.Name == "NewTestCompany" {
+		t.Errorf("company name got updated eventhough it is create only permission")
+		return
+	}
+
 }
--- models.go
+++ models.go
@@ -20,7 +20,7 @@ type User struct {
 	Pets      []*Pet
 	Toys      []Toy `gorm:"polymorphic:Owner"`
 	CompanyID *int
-	Company   Company
+	Company   Company `gorm:"<-:create"`
 	ManagerID *uint
 	Manager   *User
 	Team      []User     `gorm:"foreignkey:ManagerID"`
