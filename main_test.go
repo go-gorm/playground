fdee75b980b131de074eed39707549d1dae38ffc
Xiao Renhao <renhao.xiao@silkrode.com.tw>
2021-02-08 17:26:28 +0800

    clause where not work

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm/clause"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +11,31 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
+	// creat first line
+	user := User{Name: "alice", Age: 18}
+	user.ID = 1
 	DB.Create(&user)
 
+	// create duplicate line
+	dupUser := User{Name: "alice", Age: 18}
+	dupUser.ID = 1
+	DB.Clauses(clause.OnConflict{
+		Columns: []clause.Column{{Name: "id"}},
+		DoUpdates: clause.Assignments(map[string]interface{}{
+			"age": 20,
+		}),
+		Where: clause.Where{Exprs: []clause.Expression{
+			clause.Eq{
+				Column: "name",
+				Value:  "bob",
+			},
+		}},
+	}).Create(&dupUser)
+
+	// get result (except user.age == 18)
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+	t.Logf("\n%+v", result)
 }
