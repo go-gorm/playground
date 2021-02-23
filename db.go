58e3a4896cacfd27a9e3c4c7ab28ab890b8d0545
fabio <fabio.rossetto@betha.com>
2021-02-22 22:14:00 -0300

    not update UpdatedAt

--- db.go
+++ db.go
@@ -49,13 +49,13 @@ func OpenTestConnection() (db *gorm.DB, err error) {
 		if dbDSN == "" {
 			dbDSN = "gorm:gorm@tcp(localhost:9910)/gorm?charset=utf8&parseTime=True&loc=Local"
 		}
-		db, err = gorm.Open(mysql.Open(dbDSN), &gorm.Config{})
+		db, err = gorm.Open(mysql.Open(dbDSN), &gorm.Config{FullSaveAssociations: true})
 	case "postgres":
 		log.Println("testing postgres...")
 		if dbDSN == "" {
 			dbDSN = "user=gorm password=gorm host=localhost dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
 		}
-		db, err = gorm.Open(postgres.Open(dbDSN), &gorm.Config{})
+		db, err = gorm.Open(postgres.Open(dbDSN), &gorm.Config{FullSaveAssociations: true})
 	case "sqlserver":
 		// CREATE LOGIN gorm WITH PASSWORD = 'LoremIpsum86';
 		// CREATE DATABASE gorm;
@@ -66,10 +66,10 @@ func OpenTestConnection() (db *gorm.DB, err error) {
 		if dbDSN == "" {
 			dbDSN = "sqlserver://gorm:LoremIpsum86@localhost:9930?database=gorm"
 		}
-		db, err = gorm.Open(sqlserver.Open(dbDSN), &gorm.Config{})
+		db, err = gorm.Open(sqlserver.Open(dbDSN), &gorm.Config{FullSaveAssociations: true})
 	default:
 		log.Println("testing sqlite3...")
-		db, err = gorm.Open(sqlite.Open(filepath.Join(os.TempDir(), "gorm.db")), &gorm.Config{})
+		db, err = gorm.Open(sqlite.Open(filepath.Join(os.TempDir(), "gorm.db")), &gorm.Config{FullSaveAssociations: true})
 	}
 
 	if debug := os.Getenv("DEBUG"); debug == "true" {
--- main_test.go
+++ main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"testing"
+	"time"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +11,34 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	user := User{
+		Name: "jinzhu",
+		Account: Account{
+			UserID: sql.NullInt64{Int64: 123, Valid: true},
+			Number: "123",
+		},
+	}
 
 	DB.Create(&user)
 
 	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	if err := DB.Preload("Account").First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	oldUpdateAt := result.Account.UpdatedAt
+	time.Sleep(1 * time.Second)
+
+	result.Account.Number = "456"
+	DB.Save(&result)
+
+	var result2 User
+	if err := DB.Preload("Account").First(&result2, user.ID).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	if result2.Account.UpdatedAt.Equal(oldUpdateAt) {
+		t.Errorf("Failed, UpdatedAt on account not updated, old: %v new: %v", oldUpdateAt, result2.Account.UpdatedAt)
+	}
+
 }
