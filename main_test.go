4cb736172fd7ff6414be5facfbbb78b0459474ec
Yanchen <745539141@qq.com>
2021-03-04 10:36:25 +0800

    nested preloading not work at v1.20.12, works on v1.20.9

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm/clause"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +11,31 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	u1 := User{Name: "A"}
+	if err := DB.Create(&u1).Error; err != nil {
+		panic(err)
 	}
+
+	// set A as B's manager.
+	if err := DB.Create(&User{Name: "B", ManagerID: &u1.ID}).Error; err != nil {
+		panic(err)
+	}
+
+	var user = User{}
+	if err := DB.Where("id", u1.ID).
+		Preload("Team.Manager").
+		Preload(clause.Associations).
+		Find(&user).
+		Error; err != nil {
+		panic(err)
+	}
+
+	if (user.Team[0].Manager) == nil {
+		// gorm.io/gorm v1.20.12
+		t.Fatal("expect Manager A")
+	}
+
+	// Result:
+	// gorm.io/gorm v1.20.12: FAIL: TestGORM (0.02s). ❌
+	// gorm.io/gorm v1.20.9: PASS: TestGORM (0.02s). ✅
 }
