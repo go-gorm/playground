e16662bae165074d4c995e5a097f5a053e199450
Cameron Dunn <cameron@usesilo.com>
2021-03-08 13:18:12 -0800

    Demonstrate interaction with constraints

--- main_test.go
+++ main_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"testing"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -19,8 +21,10 @@ func TestGORM(t *testing.T) {
 	// A record is inserted, but the name is NULL
 	var createResult User
 	// A double pointer triggers this behavior
-	if err := DB.FirstOrCreate(&createResult, &user).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	if result := DB.FirstOrCreate(&createResult, &user); result.Error != nil {
+		t.Errorf("Failed, got error: %v", result.Error)
+	} else {
+		fmt.Printf("Rows affected %d\n", result.RowsAffected)
 	}
 
 	fmt.Printf("Condition param user ID (expect 0/unchaged) %d\n", user.ID)
@@ -36,6 +40,29 @@ func TestGORM(t *testing.T) {
 		if findResult.ID != createResult.ID {
 			t.Fatalf("User from create and user from find to not match. Create ID %d, find ID %d", findResult.ID, createResult.ID)
 		}
+	}
+}
+
+func TestRecordViolatesConstraint(t *testing.T) {
+	type Constraint struct {
+		gorm.Model
+		Count int
+	}
+	DB.AutoMigrate(&Constraint{})
+	DB.Exec(`ALTER TABLE constraints ADD CONSTRAINT count_check CHECK (count > 0);`)
+
+	user := &Constraint{}
+	var createResult Constraint
+	// A double pointer triggers this behavior
+	if result := DB.FirstOrCreate(&createResult, &user); result.Error != nil {
+		// This should error but does not.
+		// If plain `Create` is used there's an error. If a single pointer with FirstOrCreate is used a panic occurs.
+		t.Errorf("Failed, got error: %v", result.Error)
+	} else if result.RowsAffected != 1 {
+		// No error so presumably created?
+		t.Fatalf("Expected a row to have been created")
+	} else {
+		fmt.Printf("Rows %d\n", result.RowsAffected)
 	}
 }
 
