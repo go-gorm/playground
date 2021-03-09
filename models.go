c4d6906a56010658c61eb08c35f62aa3fb5d0ab4
lang <lang@qq.com>
2021-03-09 14:48:38 +0800

    commit

--- models.go
+++ models.go
@@ -1,5 +1,9 @@
 package main
 
+// User has one `Account` (has one), many `Pets` (has many) and `Toys` (has many - polymorphic)
+// He works in a Company (belongs to), he has a Manager (belongs to - single-table), and also managed a Team (has many - single-table)
+// He speaks many languages (many to many) and has many friends (many to many - single-table)
+// His pet also has one Toy (has one - polymorphic)
 type Order struct {
 	Id         uint64
 	OrderNo    string
