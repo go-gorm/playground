f2b56887ea84708a42d5d7f951a9a5019cf935e8
hp <ruozhixian@sina.com>
2021-03-12 11:22:06 +0800

    the ignored model property's influence in gorm v1.21.x

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gorm.io/gorm/clause"
 	"testing"
 )
 
@@ -8,13 +9,66 @@ import (
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
-func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+/*
+ PASS
+*/
+func TestModelWithoutIgnoredPro(t *testing.T) {
+	tom := PersonOne{
+		ID: 1,
+		Friends: []PersonOne{
+			{
+				ID: 2,
+				Friends: []PersonOne{
+					{
+						ID: 3,
+						Friends: []PersonOne{
+							{
+								ID: 4,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	DB.Create(&tom)
 
-	DB.Create(&user)
+	var result PersonOne
+	DB.Preload("Friends.Friends.Friends."+clause.Associations).Find(&result, "id = ?", 1)
+	if !PersonOneEqual(tom, result) {
+		t.Errorf("Failed, \"tom\" is not created correctly in sql")
+	}
+}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+/*
+ FAIL
+ You can debug to see the details about "tom" and "result"
+ Compare with previous TestModelWithoutIgnoredPro
+*/
+func TestModelWithIgnoredPro(t *testing.T) {
+	tom := PersonTwo{
+		ID: 1,
+		Friends: []PersonTwo{
+			{
+				ID: 2,
+				Friends: []PersonTwo{
+					{
+						ID: 3,
+						Friends: []PersonTwo{
+							{
+								ID: 4,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	DB.Create(&tom)
+
+	var result PersonTwo
+	DB.Preload("Friends.Friends.Friends."+clause.Associations).Find(&result, "id = ?", 1)
+	if !PersonTwoEqual(tom, result) {
+		t.Errorf("Failed, \"tom\" is not created correctly in sql")
 	}
 }
--- models.go
+++ models.go
@@ -7,6 +7,72 @@ import (
 	"gorm.io/gorm"
 )
 
+// PersonOne * one model without an ignored property
+type PersonOne struct {
+	ID             int `gorm:"primaryKey"`
+	SourcePersonID *int
+	Friends        []PersonOne `gorm:"foreignKey:SourcePersonID;references:ID"`
+	//AnotherPro    interface{} `gorm:"-"`
+}
+
+// PersonTwo * one model with an ignored property
+type PersonTwo struct {
+	ID             int `gorm:"primaryKey"`
+	SourcePersonID *int
+	Friends        []PersonTwo `gorm:"foreignKey:SourcePersonID;references:ID"`
+	AnotherPro     interface{} `gorm:"-"`
+}
+
+func PersonOneEqual(p1, p2 PersonOne) bool {
+	if p1.ID != p2.ID {
+		return false
+	}
+
+	if p1.SourcePersonID != nil && p2.SourcePersonID != nil {
+		if *p1.SourcePersonID != *p2.SourcePersonID {
+			return false
+		}
+	} else if p1.SourcePersonID != nil || p2.SourcePersonID != nil {
+		return false
+	}
+
+	if len(p1.Friends) != len(p2.Friends) {
+		return false
+	}
+	for i := 0; i < len(p1.Friends); i++ { // TODO
+		if !PersonOneEqual(p1.Friends[i], p2.Friends[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func PersonTwoEqual(p1, p2 PersonTwo) bool {
+	if p1.ID != p2.ID {
+		return false
+	}
+
+	if p1.SourcePersonID != nil && p2.SourcePersonID != nil {
+		if *p1.SourcePersonID != *p2.SourcePersonID {
+			return false
+		}
+	} else if p1.SourcePersonID != nil || p2.SourcePersonID != nil {
+		return false
+	}
+
+	if len(p1.Friends) != len(p2.Friends) {
+		return false
+	}
+	for i := 0; i < len(p1.Friends); i++ { // TODO
+		if !PersonTwoEqual(p1.Friends[i], p2.Friends[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
 // User has one `Account` (has one), many `Pets` (has many) and `Toys` (has many - polymorphic)
 // He works in a Company (belongs to), he has a Manager (belongs to - single-table), and also managed a Team (has many - single-table)
 // He speaks many languages (many to many) and has many friends (many to many - single-table)
