d0f5a1b38bf0ef6171718696b31e12037c9b3c6e
huhaifan <huhaifan96@gmail.com>
2021-03-18 15:03:50 +0800

    Nested scopes support

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module gorm.io/playground
 go 1.14
 
 require (
-	gorm.io/driver/mysql v1.0.5
-	gorm.io/driver/postgres v1.0.8
-	gorm.io/driver/sqlite v1.1.4
-	gorm.io/driver/sqlserver v1.0.6
+	github.com/jinzhu/now v1.1.2 // indirect
+	gorm.io/driver/mysql v1.0.1
+	gorm.io/driver/postgres v1.0.0
+	gorm.io/driver/sqlite v1.1.1
+	gorm.io/driver/sqlserver v1.0.3
 	gorm.io/gorm v1.21.3
 )
 
--- main_test.go
+++ main_test.go
@@ -2,19 +2,29 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+func LimitScope(db *gorm.DB) *gorm.DB {
+	return db.Limit(1)
+}
+
+func UserScope(db *gorm.DB) *gorm.DB {
+	return db.Scopes(LimitScope).Preload("Toys")
+}
+
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	users := []User{{Name: "jinzhu"}, {Name: "logan"}}
 
-	DB.Create(&user)
+	DB.Create(&users)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	var result []User
+	if err := DB.Scopes(UserScope).Find(&result).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
