d82569a8ad607d8c0c60dd5152f274e3ba5a1d10
nrnrk <noriki6t@gmail.com>
2021-04-20 21:06:59 +0900

    More minimum

--- main_test.go
+++ main_test.go
@@ -9,10 +9,7 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	tx := DB.Begin()
-	defer tx.Rollback()
-
-	if err := tx.Exec(
+	if err := DB.Exec(
 		`INSERT INTO "users" ( "id" ) VALUES ( $1 )`,
 		`1`,
 	).Error; err != nil {
