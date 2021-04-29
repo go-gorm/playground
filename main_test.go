c5d66c275f29f876849b610589cf26ccb14caf00
a.k <a@b.com>
2021-04-29 23:13:35 +0800

    leak bug

--- main_test.go
+++ main_test.go
@@ -1,20 +1,38 @@
 package main
 
 import (
+	"net/http"
+	_ "net/http/pprof"
 	"testing"
+	"time"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
-func TestGORM(t *testing.T) {
+func leak() {
 	user := User{Name: "jinzhu"}
 
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	for i := 0; i < 10000; i++ {
+		DB.FirstOrCreate(&user)
 	}
+}
+func loop_forever() {
+	for true {
+		time.Sleep(time.Second)
+	}
+}
+func TestGORM(t *testing.T) {
+	go func() {
+		t.Log("check the url: http://localhost:3344/debug/pprof")
+		e := http.ListenAndServe("0.0.0.0:3344", nil)
+		if e != nil {
+			panic(e)
+		}
+	}()
+
+	leak()
+
+	loop_forever()
 }
