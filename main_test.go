ed10ec279debda919f2907b389f2ebfcd2a71fa7
shenmengyue <shenmengyue@bytedance.com>
2021-05-17 15:22:56 +0800

    test: add failed test case

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 	"testing"
 )
 
@@ -16,5 +19,29 @@ func TestGORM(t *testing.T) {
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+}
+
+func TestScope(t *testing.T) {
+	const name = "TestScope"
+
+	// prepare an User
+	user := User{Name: name}
+	if err := DB.Create(&user).Error; err != nil {
+		t.Errorf("DB create failed, err=%s", err.Error())
+	}
+
+	ctx := context.TODO() // some context
+	// whereScope can be reused in a Find & a Count, a common usage in paginate APIs.
+	whereScope := func(db *gorm.DB) *gorm.DB {
+		return db.WithContext(ctx).Table("users").Where(clause.Eq{Column: "name", Value: name})
+	}
+
+	var total int64
+	if err := DB.Scopes(whereScope).Count(&total).Error; err != nil {
+		t.Errorf("DB count failed, err=%s", err.Error())
+	}
+	if total != 1 { // should find a record, but count always return 0
+		t.Errorf("DB count failed, want=%d, got=%d", 1, total)
 	}
 }
