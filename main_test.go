2566e0a45ffb5e9f8ea14558f7a50cff4c1a5025
Eric Wollesen <ericw@xmtp.net>
2021-05-18 15:56:53 -0600

    Why does gorm attempt to update an artist instead of insert it?

--- main_test.go
+++ main_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -17,4 +21,41 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+}
+
+func TestShouldInsert(t *testing.T) {
+	if err := DB.AutoMigrate(Artist{}); err != nil {
+		t.Fatalf("error migrating releases table %s", err)
+	}
+
+	tx := DB.Begin().Debug()
+	defer tx.Rollback()
+
+	// The expected result is that an artist should be inserted, but for some
+	// reason gorm tries to update, which fails due to a lack of where-clause.
+	if err := doArtist(tx, "new artist name"); err != nil {
+		t.Fatalf("error %s", err)
+	}
+}
+
+func doArtist(tx *gorm.DB, name string) (err error) {
+	artist := &Artist{}
+	err = tx.Where(Artist{Name: name}).
+		Attrs(Artist{Name: name}).
+		FirstOrInit(&artist).Error
+	if err != nil {
+		return fmt.Errorf("initializing artist %s", err)
+	}
+	log.Printf("<<<<>>>> the new artist's id is %d <<<<>>>>", artist.ID)
+	err = tx.Save(&artist).Error
+	if err != nil {
+		return fmt.Errorf("saving artist %s", err)
+	}
+
+	return nil
+}
+
+type Artist struct {
+	gorm.Model
+	Name string
 }
