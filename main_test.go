2a3826f2c4958afcf082dba5f977518bfc7d94ba
Cloud User <liucl@yunphant.com>
2021-05-19 06:37:16 +0000

    failed test

--- main_test.go
+++ main_test.go
@@ -13,6 +13,16 @@ func TestGORM(t *testing.T) {
 
 	DB.Create(&user)
 
+	user.Name = "test"
+
+	if err := DB.Save(user).Error; err != nil {
+		t.Errorf("Failed to save 1, got error: %v", err)
+	}
+
+	if err := DB.Model(&User{}).Save(user).Error; err != nil {
+		t.Errorf("Failed to save 3, got error: %v", err)
+	}
+
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
