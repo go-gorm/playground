4a099e962f865ed310370dbd3b1e3fd713471134
Jordan <Jordan.Brockopp@target.com>
2021-05-26 15:20:05 -0500

    show record not found bug with Raw()

--- go.mod
+++ go.mod
@@ -7,10 +7,10 @@ require (
 	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
 	github.com/jackc/pgx/v4 v4.11.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.7 // indirect
-	golang.org/x/crypto v0.0.0-20210505212654-3497b51f5e64 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gorm.io/driver/mysql v1.0.6
+	gorm.io/driver/mysql v1.1.0
 	gorm.io/driver/postgres v1.1.0
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/driver/sqlserver v1.0.7
--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -14,7 +16,21 @@ func TestGORM(t *testing.T) {
 	DB.Create(&user)
 
 	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+
+	// create the SQL query to display a bug
+	query := "SELECT * FROM users WHERE name = ?"
+
+	// run the good raw SQL query to display the query is valid
+	goodResult := DB.Raw(query, "jinzhu").Scan(&result)
+	// should return no error since user 'jinzhu' exists
+	if err := goodResult.Error; err != nil {
+		t.Errorf("Gorm Raw() Failed, got: %v, want: %v", err, nil)
+	}
+
+	// run the bad raw SQL query to display the bug
+	badResult := DB.Raw(query, "foobar").Scan(&result)
+	// should return gorm.ErrRecordNotFound since user 'foobar' does not exist
+	if err := badResult.Error; err == nil {
+		t.Errorf("Gorm Raw() Failed, got: %v, want: %v", err, gorm.ErrRecordNotFound)
 	}
 }
