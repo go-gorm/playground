c7a20a0cc479b0cdf168d2ea1a62a4a5b76292f1
David Lawrence <david.lawrence@smithrx.com>
2021-06-24 11:09:33 -0700

    error on count when order is included on postgres

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -13,8 +15,15 @@ func TestGORM(t *testing.T) {
 
 	DB.Create(&user)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	var (
+		count int64
+		query = DB.Model(&User{}).Order("users.id ASC")
+		session = query.Session(&gorm.Session{})
+	)
+	if err := query.Count(&count).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	if err := session.Count(&count).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
