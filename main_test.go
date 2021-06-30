729c86306b22b8ddc82899c33a5aab8959d44194
Hlynur Örn Haraldsson <hlynur@netapp.com>
2021-06-30 15:50:37 +0000

    Fails due to db.Omit

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gorm.io/gorm/clause"
 	"testing"
 )
 
@@ -13,8 +14,21 @@ func TestGORM(t *testing.T) {
 
 	DB.Create(&user)
 
+	DB = DB.Omit(clause.Associations)
+
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	newUser := User{Name: "Testy McBoatFace", Age: 23, Account: Account{Number: "3"}}
+	if err := DB.Create(&newUser).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+
+	newCompany := Company{Name: "ErrorCo"}
+	if err := DB.Create(&newCompany).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
