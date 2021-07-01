15b73bc3d99380c35876ee8455dc3dde13cf4896
Endre Kovacs <endre@alpaca.markets>
2021-07-01 11:46:22 +0200

    incorrect where clause generated from model query of the joined table
    gorm will incorrectly generate the where clause to be
    against the current table (~~~ct~~~) instead of taking it from the model's type.
    
    in such cases currently, it can only be worked around by specifying the where clause explicitly as string.

--- main_test.go
+++ main_test.go
@@ -9,12 +9,46 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	// create 2 users, each with an one-2-many relationship
+	user1 := User{Name: "Alice", Age: 34, Active: true,
+		Pets: []*Pet{
+			{Name: "Doggo"},
+			{Name: "Pepe the frog"},
+		}}
+	user2 := User{Name: "Bob", Age: 35, Active: false, Pets: []*Pet{
+		{Name: "Nyan cat"},
+	}}
 
-	DB.Create(&user)
+	if err := DB.Create(&user1).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	if err := DB.Create(&user2).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	// looking for a pet name 'Doggo'
+	tx := DB.Where(Pet{Name: "Doggo"})
+
+	//
+	// SQL generated:
+	// SELECT `pets`.`id`,`pets`.`created_at`,`pets`.`updated_at`,`pets`.`deleted_at`,`pets`.`user_id`,`pets`.`name` FROM `pets` JOIN users u on u.id = pets.user_id WHERE `pets`.`name` = "Doggo" AND u.name = "Alice" AND `pets`.`deleted_at` IS NULL ORDER BY `pets`.`id` LIMIT 1
+	//
+	// uncomment this and the test will pass:
+	//
+	// tx = tx.Joins("JOIN users u on u.id = pets.user_id").Where("u.name = ?", "Alice")
+	//
+
+	//
+	// SQL generated:
+	// SELECT `pets`.`id`,`pets`.`created_at`,`pets`.`updated_at`,`pets`.`deleted_at`,`pets`.`user_id`,`pets`.`name` FROM `pets` JOIN users u on u.id = pets.user_id WHERE `pets`.`name` = "Doggo" AND `pets`.`name` = "Alice" AND `pets`.`deleted_at` IS NULL ORDER BY `pets`.`id` LIMIT 1
+	//
+	// uncomment this and the test will fail:
+	//
+	tx = tx.Joins("JOIN users u on u.id = pets.user_id").Where(&User{Name: "Alice"})
+	//
+
+	pet := Pet{}
+	if err := tx.First(&pet).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
