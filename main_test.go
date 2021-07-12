6a0fb7903d92babe74c2f8477a595a2bcb5bbcb1
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2021-07-12 10:47:43 +0300

    add TestRawWithAtSign

--- main_test.go
+++ main_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"os"
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -16,5 +19,18 @@ func TestGORM(t *testing.T) {
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+}
+
+func TestRawWithAtSign(t *testing.T) {
+	if os.Getenv("GORM_DIALECT") != "mysql" {
+		t.Skip()
+	}
+	var result map[string]interface{}
+	db := DB.Session(&gorm.Session{DryRun: true}).
+		Raw("SELECT * FROM users WHERE name = ? OR name = @@version", "dz").Take(&result)
+	sql := db.Statement.SQL.String()
+	if sql != "SELECT * FROM users WHERE name = 'dz' OR name = @@version" {
+		t.Errorf("wrong SQL")
 	}
 }
