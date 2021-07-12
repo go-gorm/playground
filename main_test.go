28305bc47943abf40762c844fd1fe93878d420eb
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2021-07-12 10:35:45 +0300

    add TestWhere1

--- main_test.go
+++ main_test.go
@@ -18,3 +18,10 @@ func TestGORM(t *testing.T) {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
+
+func TestWhere1(t *testing.T) {
+	var result []map[string]interface{}
+	if err := DB.Table("users").Where("1").Scan(&result).Error; err != nil {
+		t.Errorf("error on selecting * from users where 1: %v", err)
+	}
+}
