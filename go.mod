aa3595523d8541af2b0794111aaf2689d9a3457a
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2021-07-14 13:06:09 +0300

    add TestErrors

--- go.mod
+++ go.mod
@@ -3,18 +3,16 @@ module gorm.io/playground
 go 1.16
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/denisenkom/go-mssqldb v0.10.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
-	github.com/jackc/pgx/v4 v4.11.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.7 // indirect
-	golang.org/x/crypto v0.0.0-20210505212654-3497b51f5e64 // indirect
-	golang.org/x/text v0.3.6 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gorm.io/driver/mysql v1.0.6
+	github.com/jackc/pgx/v4 v4.12.0 // indirect
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	gorm.io/driver/mysql v1.1.1
 	gorm.io/driver/postgres v1.1.0
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/driver/sqlserver v1.0.7
-	gorm.io/gorm v1.21.9
+	gorm.io/gorm v1.21.12
 )
 
 replace gorm.io/gorm => ./gorm
--- main_test.go
+++ main_test.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"errors"
 	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -16,5 +21,39 @@ func TestGORM(t *testing.T) {
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+}
+
+func TestErrors(t *testing.T) {
+	dbMock, mock, err := sqlmock.New()
+	if err != nil {
+		t.Errorf("Unable to create the mock db: %v", err)
+		return
+	}
+	defer func() { _ = dbMock.Close() }()
+
+	db, err := gorm.Open(mysql.Dialector{
+		Config: &mysql.Config{
+			Conn:                      dbMock,
+			SkipInitializeWithVersion: true,
+		},
+	}, &gorm.Config{
+		Logger: DB.Logger,
+	})
+	if err != nil {
+		t.Errorf("Unable to create the gorm connection to the mock: %v", err)
+		return
+	}
+
+	expectedError := errors.New("some error")
+	mock.ExpectQuery("^SELECT ").WillReturnError(expectedError)
+
+	var result []map[string]interface{}
+	err = db.Table("users").Scan(&result).Error
+	if err != expectedError {
+		t.Errorf("Got wrong error: %v", err)
+	}
+	if err = mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Expectations were not met: %v", err)
 	}
 }
