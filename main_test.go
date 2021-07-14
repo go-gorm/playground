1705331107d9c14dc5c842711a32fe6651ae09d3
Dmitry Zenovich <dmitry.zenovich@toptal.com>
2021-07-14 18:46:48 +0300

    add TestPluckWithNullableColumns

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -16,5 +18,19 @@ func TestGORM(t *testing.T) {
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+}
+
+func TestPluckWithNullableColumns(t *testing.T) {
+	var ids []*int64
+	if err := DB.Model(&User{}).Where("name = 'dz'").Pluck("MAX(id)", &ids).Error; err != nil {
+		t.Errorf("got error on calling Pluck(): %v", err)
+	}
+	// ids should be []*int64{nil}
+	if len(ids) != 1 || ids[0] != nil {
+		t.Errorf("wrong result: %#v", ids)
+		if len(ids) > 0 && ids[0] != nil {
+			t.Errorf("first element's value: %#v", *ids[0])
+		}
 	}
 }
