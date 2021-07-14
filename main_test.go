01ae28ef1b1bfb5df3037e6e8bebe59439890c7e
Teh Nian Fei <tehnianfei@gmail.com>
2021-07-14 13:32:29 +0800

    Bug in COUNT(DISTINCT(table.*))

--- main_test.go
+++ main_test.go
@@ -10,11 +10,24 @@ import (
 
 func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
+	pet := Pet{UserID: &user.ID, Name: "jinzhu"}
 
 	DB.Create(&user)
+	DB.Create(&pet)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	// This is fine
+	if err := DB.Model(&User{}).Distinct("users.*").Joins("JOIN pets ON users.id = pets.user_id").Find(&user).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	// This is also fine
+	var count int64
+	if err := DB.Model(&User{}).Distinct("users.id").Joins("JOIN pets ON users.id = pets.user_id").Count(&count).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	// This is not
+	if err := DB.Model(&User{}).Distinct("users.*").Joins("JOIN pets ON users.id = pets.user_id").Count(&count).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
