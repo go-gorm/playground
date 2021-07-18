fc1146042e0335dc7de35ac9739f5f23f189130d
Tobias Kaupat <tk@lobaro.de>
2021-07-18 23:44:36 +0200

    Executing "SHOW search_path" does not work.

--- main_test.go
+++ main_test.go
@@ -1,20 +1,49 @@
 package main
 
 import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 	"testing"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: postgres
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
 
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	entity := &Organisation{
+		Name: "Admin Organisation",
 	}
+	db := DB
+
+	db = db.Begin()
+	if db.Error != nil {
+		panic("Failed to begin test TX " + db.Error.Error())
+	}
+
+	ShowSearchPath(t, db)
+
+	users := make([]User, 0)
+	err := db.Find(&users).Error
+	require.NoError(t, err)
+
+	assert.Equal(t, "", db.Statement.Table)
+	err = db.Omit(clause.Associations).Create(entity).Error
+	require.NoError(t, err)
+}
+
+func ShowSearchPath(t *testing.T, db *gorm.DB) {
+	type SearchPath struct {
+		SearchPath string `store:"search_path"`
+	}
+	sp := &SearchPath{}
+	err := db.Debug().Raw("SHOW search_path").Scan(&sp).Error
+	if err != nil {
+		require.NoError(t, err)
+	}
+	db.Logger.Info(context.Background(), "search_path: %s", sp.SearchPath)
 }
