8b2a35909ae7729230fe47e11013797ed22e18e7
Devin Gunay <devingunay@gmail.com>
2021-09-02 15:46:56 -0700

    minimum example of UpdatedAt not working as expected

--- main_test.go
+++ main_test.go
@@ -18,3 +18,68 @@ func TestGORM(t *testing.T) {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
+
+func TestUpdatedAt(t *testing.T) {
+	DB.AutoMigrate(&BrokenUpdate{})
+
+	model := &BrokenUpdate{}
+	err := DB.Create(model).Error
+	if err != nil {
+		t.Errorf("Failed to create model: %v", err)
+
+	}
+
+	// Save the created state
+	before := model
+
+	// We update a single field of the model
+	tx := DB.Model(&model).Updates(BrokenUpdate{StringMember: "updated"})
+	if tx.Error != nil {
+		t.Errorf("Failed to update model: %v", tx.Error)
+	}
+
+	// We fetch the model again
+	var retrieved BrokenUpdate
+	result := DB.First(&retrieved, model.ID)
+	if result.Error != nil {
+		t.Errorf("Failed to find model: %v", result.Error)
+	}
+
+	if result.RowsAffected == 0 {
+		t.Errorf("Failed to find model")
+	}
+
+	// And then assert that the fields did/didn't change
+	if !before.CreatedAt.Equal(retrieved.CreatedAt) {
+		t.Errorf("CreatedAt should not change")
+	}
+	if !before.LastCheckinTime.Equal(retrieved.LastCheckinTime) {
+		t.Errorf("LastCheckinTime should not change")
+	}
+	if before.SecretToken != retrieved.SecretToken {
+		t.Errorf("SecretToken should not change")
+	}
+	if !before.DeletedAt.Time.Equal(retrieved.DeletedAt.Time) {
+		t.Errorf("DeletedAt should not change")
+	}
+	if before.EmbeddedStruct != retrieved.EmbeddedStruct {
+		t.Errorf("EmbeddedStruct should not change")
+	}
+
+	if before.ID != retrieved.ID {
+		t.Errorf("ID should not change")
+	}
+	if before.Code != retrieved.Code {
+		t.Errorf("Code should not change")
+	}
+	if before.JobName != retrieved.JobName {
+		t.Errorf("JobName should not change")
+	}
+	// FIXME: UpdatedAt doesn't seem to actually change when .Updates() is used
+	if before.UpdatedAt.Equal(retrieved.UpdatedAt) {
+		t.Errorf("UpdatedAt should change")
+	}
+	if retrieved.StringMember != "updated" {
+		t.Errorf("StringMember should change to 'updated'")
+	}
+}
--- models.go
+++ models.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"time"
 
 	"gorm.io/gorm"
@@ -57,4 +58,46 @@ type Company struct {
 type Language struct {
 	Code string `gorm:"primarykey"`
 	Name string
+}
+
+type IDType uint
+type Code string
+
+type BrokenUpdate struct {
+	ID        IDType         `gorm:"primaryKey;<-:create" json:"id"`
+	CreatedAt time.Time      `gorm:"<-:create" json:"-"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+
+	JobName         string    `gorm:"uniqueIndex;->;type:varchar GENERATED ALWAYS AS ('thing-session-' || CAST(id as TEXT)) STORED;" json:"jobName"`
+	SecretToken     string    `gorm:"unique" json:"-"`
+	Code            Code      `gorm:"uniqueIndex" json:"code"`
+	LastCheckinTime time.Time `json:"-"`
+	EmbeddedStruct  `gorm:"embeddedPrefix:embed_"`
+	StringMember    string `json:"stringMember"`
+}
+
+type EmbeddedStruct struct {
+	Foo  string `json:"foo"`
+	Bar  string `json:"bar"`
+	Quux string `json:"quux"`
+}
+
+func (s *BrokenUpdate) BeforeCreate(db *gorm.DB) (err error) {
+	s.SecretToken = "generated"
+	s.Code = "generated"
+	s.LastCheckinTime = time.Now()
+	return nil
+}
+
+func (s *BrokenUpdate) AfterCreate(db *gorm.DB) (err error) {
+	s.JobName = fmt.Sprintf("thing-session-%d", s.ID)
+
+	return nil
+}
+
+func (s *BrokenUpdate) BeforeDelete(tx *gorm.DB) (err error) {
+	s.EmbeddedStruct = EmbeddedStruct{}
+	tx = tx.Model(s).Select("*").Updates(*s)
+	return tx.Error
 }
