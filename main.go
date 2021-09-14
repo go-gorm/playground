ef7b2b7eb73e46798b96d6d3770ade26eecc2079
Chaitanya Bapat <36437651+mrbeepot@users.noreply.github.com>
2021-09-15 02:25:40 +0530

    Trying async insert in an in-memory sqlite database.

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Sentence struct {
+	gorm.Model
+	ActualSentence string
+}
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Print("Hello\n")
+}
+
+func SetupConnection() *gorm.DB {
+	connection, connectionError := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+	if connectionError != nil {
+		log.Panic("Cannot create a database connection", connectionError)
+	}
+	connection.AutoMigrate(&Sentence{})
+	return connection
+}
+
+func InsertSentence(connection *gorm.DB, errChannel chan error, sentence Sentence) {
+	res := connection.Create(&sentence)
+	if res.Error != nil {
+		errChannel <- res.Error
+	} else {
+		fmt.Printf("Successfully inserted %s\n", sentence.ActualSentence)
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"sync"
 	"testing"
+
+	"gorm.io/gorm"
 )
+
+var wg sync.WaitGroup
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
@@ -17,4 +22,38 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+}
+
+var sentences = []string{
+	"Can",
+	"You",
+	"Please",
+	"Tell",
+	"What",
+	"Am",
+	"Doing",
+	"Wrong",
+}
+
+func TestInsertInSqliteAsync(t *testing.T) {
+	conn := SetupConnection()
+	db, _ := conn.DB()
+	defer db.Close()
+	errChannel := make(chan error)
+	for _, s := range sentences {
+		wg.Add(1)
+		go insert(conn, errChannel, Sentence{ActualSentence: s})
+	}
+	go func() {
+		wg.Wait()
+		close(errChannel)
+	}()
+	for e := range errChannel {
+		t.Errorf("Error inserting in DB %+v", e)
+	}
+}
+
+func insert(connection *gorm.DB, errChannel chan error, sentence Sentence) {
+	defer wg.Done()
+	InsertSentence(connection, errChannel, sentence)
 }
