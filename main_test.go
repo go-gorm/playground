d96a587e5f1110869df1cdaff9f13e854f7243ec
João Victor Santos <profissional.joaovss@gmail.com>
2021-09-16 21:09:16 -0300

    test: callbacks

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +11,52 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	type Context struct {
+		CompanyID *int
+		ManagerID *uint
+	}
 
-	DB.Create(&user)
+	var (
+		contextKey string = "APP-CONTEXT"
+		companyID  int    = 1
+		managerID  uint   = 1
+	)
+
+	assignContext := func(db *gorm.DB) {
+		if ICtx, ok := db.Get(contextKey); ok {
+			ctx := ICtx.(*Context)
+
+			if db.Statement.Schema != nil {
+
+				if field := db.Statement.Schema.LookUpField("CompanyID"); field != nil {
+					if v, isZero := field.ValueOf(db.Statement.ReflectValue); v == nil || isZero {
+						field.Set(db.Statement.ReflectValue, ctx.CompanyID)
+					}
+				}
+
+				if field := db.Statement.Schema.LookUpField("ManagerID"); field != nil {
+					if v, isZero := field.ValueOf(db.Statement.ReflectValue); v == nil || isZero {
+						field.Set(db.Statement.ReflectValue, ctx.CompanyID)
+					}
+				}
+
+			}
+		}
+	}
+
+	callback := DB.Callback()
+	callback.Create().After("gorm:before_create").Register("gobp:assign_tenant", assignContext)
+
+	user := User{
+		Name: "User",
+	}
+
+	ctx := Context{CompanyID: &companyID, ManagerID: &managerID}
+
+	DB.Set(contextKey, &ctx).Create(&user)
 
 	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	if err := DB.Where(&User{CompanyID: &companyID, ManagerID: &managerID}).First(&result).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
