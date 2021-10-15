2c2c004e77bf347a44f4cf8d39408476ee11640f
lhzzz <985854294@qq.com>
2021-10-15 13:08:45 +0800

    add create nil case

--- go.mod
+++ go.mod
@@ -1,20 +1,30 @@
-module gorm.io/playground
+module gorm/playground
 
-go 1.16
+go 1.17
 
 require (
-	github.com/denisenkom/go-mssqldb v0.10.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
-	github.com/jackc/pgx/v4 v4.11.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.7 // indirect
-	golang.org/x/crypto v0.0.0-20210505212654-3497b51f5e64 // indirect
-	golang.org/x/text v0.3.6 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gorm.io/driver/mysql v1.0.6
-	gorm.io/driver/postgres v1.1.0
-	gorm.io/driver/sqlite v1.1.4
-	gorm.io/driver/sqlserver v1.0.7
-	gorm.io/gorm v1.21.9
+	gorm.io/driver/mysql v1.1.2
+	gorm.io/driver/postgres v1.1.2
+	gorm.io/driver/sqlite v1.1.6
+	gorm.io/driver/sqlserver v1.1.0
+	gorm.io/gorm v1.21.16
 )
 
-replace gorm.io/gorm => ./gorm
+require (
+	github.com/denisenkom/go-mssqldb v0.11.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.10.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.1.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.8.1 // indirect
+	github.com/jackc/pgx/v4 v4.13.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.8 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,97 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+)
+
+type Record struct {
+	gorm.Model
+	TaskID       uint64 `gorm:"Index"`
+	BatchId      uint32 `gorm:"Index"`
+	DstObjectId  uint64 `gorm:"Index"`
+	SrcObjectIds string `gorm:"type:blob"` //用,分割objectid
+}
+
+func openDB(debug, singularTable bool) (*gorm.DB, error) {
+	var err error
+	var db *gorm.DB
+
+	logLevel := logger.Warn
+	if debug {
+		fmt.Print("openDB with debug mode")
+		logLevel = logger.Info
+	}
+
+	logger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logLevel,
+			IgnoreRecordNotFoundError: true,
+			Colorful:                  true,
+		},
+	)
+
+	sqlconnstr := "root:root@tcp(localhost:3306)/testDB2?charset=utf8mb4&parseTime=true&loc=Local"
+	db, err = gorm.Open(mysql.Open(sqlconnstr), &gorm.Config{
+		Logger: logger,
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: singularTable,
+		},
+		SkipDefaultTransaction: true,
+		PrepareStmt:            true,
+		AllowGlobalUpdate:      true,
+	})
+	return db, err
+}
 
 func main() {
-	fmt.Println("vim-go")
+
+	db, err := openDB(true, false)
+	if err != nil {
+		fmt.Print("OpenDB failed, err=", err)
+		return
+	}
+
+	sqlDB, e := db.DB()
+	if e != nil {
+		fmt.Print("Get Sql DB failed, err=", e)
+		err = e
+		return
+	}
+
+	if err = db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;").AutoMigrate(&Record{}); err != nil {
+		fmt.Print("create gatherstore database error ", err)
+		sqlDB.Close()
+		return
+	}
+
+	db = db.Model(&Record{})
+
+	var results []Record
+	query := db.Where("task_id=1 AND dst_object_id=1448200665468768285").Select("dst_object_id, src_object_ids").Order("id DESC").Find(&results)
+	if query.Error != nil {
+		return
+	}
+
+	// do something
+
+	record := Record{
+		TaskID:       1,
+		BatchId:      28,
+		DstObjectId:  1448200665468768285,
+		SrcObjectIds: "1448482124259659796,1448200665468768285",
+	}
+	if err := db.Create(&record).Error; err != nil {
+		fmt.Print("create err,err=", err)
+		return
+	}
 }
