7edaf135153206c6c3438a0e351be0bfd9991888
foucaud_e <efoucaud@ifotec.com>
2021-10-21 11:24:43 +0200

    New: Add error pgx to gorm

--- main_test.go
+++ main_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"testing"
+
+	"github.com/jackc/pgx/v4"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +12,40 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	//db connect with pgx
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, "user=gorm password=gorm host=localhost dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai")
+	if err != nil {
+		t.Errorf("Failed can't open db")
+	}
+	//create table events with pgx
+	sql := `
+	DROP TABLE IF EXISTS "events";
+	DROP SEQUENCE IF EXISTS events_id_seq;
+	CREATE SEQUENCE events_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+	CREATE TABLE "public"."events" (
+		"id" integer DEFAULT nextval('events_id_seq') NOT NULL,
+		"town" text NOT NULL,
+		CONSTRAINT "events_pkey" PRIMARY KEY ("id")
+	) WITH (oids = false);
+	`
+	_, err = conn.Exec(context.Background(), sql)
+	if err != nil {
+		t.Errorf("Failed can't exec table events")
+	}
+	//insert event
+	_, err = conn.Exec(context.Background(), "insert into events(town) values('Paris')")
+	if err != nil {
+		t.Errorf("Failed can't insert in table")
+	}
+	//close pgx db
+	err = conn.Close(ctx)
+	if err != nil {
+		t.Errorf("Failed can't close pgx db")
+	}
+	//try to migrate
+	err = DB.AutoMigrate(&Event{})
+	if err != nil {
+		t.Errorf("Failed can't migrate pgx to gorm")
 	}
 }
