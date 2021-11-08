949b1cad2b97430564aabff1314a853f998853f4
Ori Amizur <oamizur@redhat.com>
2021-11-08 15:12:24 +0200

    When reusing where condition, previous where clauses are appended to the current query - This causes the query to become incorrect

--- main_test.go
+++ main_test.go
@@ -2,19 +2,50 @@ package main
 
 import (
 	"testing"
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+func populate(db *gorm.DB) error {
+	parents := []*Parent{
+		{
+			ID: 5,
+		},
+		{
+			ID: 6,
+		},
+		{
+			ID: 7,
+		},
+		{
+			ID: 8,
+		},
+	}
+	return db.Create(&parents).Error
+
+}
+
+
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	err := populate(DB)
+	if err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+	preparedDB := DB.Where("id <> 0")
+	var parents []*Parent
+	if err = preparedDB.Where("id in (?)", []int{5, 6}).Find(&parents).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	if len(parents) != 2 {
+		t.Errorf("Failed, Expected 2 parents.  Got %d", len(parents))
+	}
+	if err = preparedDB.Where("id in (?)", []int{7, 8}).Find(&parents).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	if len(parents) != 2 {
+		t.Errorf("Failed, Expected 2 parents.  Got %d", len(parents))
 	}
 }
