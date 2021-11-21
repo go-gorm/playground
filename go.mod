7be49a56c782e9a8cb97004f045e8880a67359b5
thanhpp <thanhphanphu18@gmail.com>
2021-11-21 22:10:28 +0700

    make changes

--- go.mod
+++ go.mod
@@ -1,20 +1,19 @@
-module gorm.io/playground
+module github.com/thanhpp/playground
 
 go 1.16
 
 require (
-	github.com/denisenkom/go-mssqldb v0.10.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
-	github.com/jackc/pgx/v4 v4.11.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.7 // indirect
-	golang.org/x/crypto v0.0.0-20210505212654-3497b51f5e64 // indirect
-	golang.org/x/text v0.3.6 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gorm.io/driver/mysql v1.0.6
-	gorm.io/driver/postgres v1.1.0
-	gorm.io/driver/sqlite v1.1.4
-	gorm.io/driver/sqlserver v1.0.7
-	gorm.io/gorm v1.21.9
+	github.com/jackc/pgconn v1.10.1
+	github.com/jackc/pgerrcode v0.0.0-20201024163028-a0d42d470451
+	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
+	github.com/jackc/pgtype v1.9.0 // indirect
+	github.com/jinzhu/now v1.1.3 // indirect
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	gorm.io/driver/mysql v1.2.0
+	gorm.io/driver/postgres v1.2.2
+	gorm.io/driver/sqlite v1.2.4
+	gorm.io/driver/sqlserver v1.2.1
+	gorm.io/gorm v1.22.3
 )
 
 replace gorm.io/gorm => ./gorm
--- main_test.go
+++ main_test.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"errors"
 	"testing"
+
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgerrcode"
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +14,47 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	user := User{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		Name: "jinzhu",
+	}
 
 	DB.Create(&user)
 
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+		return
 	}
+
+	if err := DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(&user).Error; err != nil {
+			if isDuplicate(err) {
+				t.Log("--------- DUPLICATE ---------")
+				if err := tx.Where("id = ?", user.ID).Delete(&user).Error; err != nil {
+					return err
+				}
+				return nil
+			}
+
+			return err
+		}
+
+		return nil
+	}); err != nil {
+		t.Errorf("Failed, got error: %v", err)
+		return
+	}
+}
+
+func isDuplicate(err error) bool {
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		if pgErr.Code == pgerrcode.UniqueViolation {
+			return true
+		}
+	}
+	return false
 }
