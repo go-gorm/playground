b604d45c490bbfe7bd4404dc1533b4e4e4a789b9
0xJacky <me@jackyu.cn>
2021-11-23 11:04:22 +0800

    auto migrate test

--- go.mod
+++ go.mod
@@ -5,11 +5,9 @@ go 1.16
 require (
 	github.com/denisenkom/go-mssqldb v0.10.0 // indirect
 	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
-	github.com/jackc/pgx/v4 v4.11.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.7 // indirect
 	golang.org/x/crypto v0.0.0-20210505212654-3497b51f5e64 // indirect
 	golang.org/x/text v0.3.6 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gorm.io/driver/mysql v1.0.6
 	gorm.io/driver/postgres v1.1.0
 	gorm.io/driver/sqlite v1.1.4
@@ -17,4 +15,4 @@ require (
 	gorm.io/gorm v1.21.9
 )
 
-replace gorm.io/gorm => ./gorm
+//replace gorm.io/gorm => ./gorm
--- main_test.go
+++ main_test.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"path"
 	"testing"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+type User struct {
+	gorm.Model
+	Name      string
+	Age       uint
+	Active    bool
+}
 
 func TestGORM(t *testing.T) {
+	dbPath := path.Join("database.db")
+
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{
+		Logger:      logger.Default.LogMode(logger.Info),
+		PrepareStmt: true,
+	})
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	user := User{Name: "jinzhu"}
 
-	DB.Create(&user)
+	db.Create(&user)
+
 
 	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	if err := db.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
