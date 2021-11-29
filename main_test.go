fb672ba6b24bf600bbcb2938b8bc565db3b4d3db
Stepan Morozov <stepan.morozov@atnr.pro>
2021-11-30 02:39:18 +0700

    reproduce panics flow

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"testing"
 )
 
@@ -12,9 +13,35 @@ func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
 
 	DB.Create(&user)
+	DB.Create(&Account{
+		UserID: sql.NullInt64{Int64: int64(user.ID), Valid: true},
+		Number: "123",
+	})
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	var result struct {
+		Something string
+		User
+	}
+
+	// works
+	err := DB.
+		Model(User{}).
+		//Preload("Account").
+		Select("users.*, 'yo' as something").
+		First(&result, user.ID).
+		Error
+	if err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	// panics
+	err = DB.
+		Model(User{}).
+		Preload("Account").
+		Select("users.*, 'yo' as something").
+		First(&result, user.ID).
+		Error
+	if err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
