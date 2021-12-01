033ff642d930df78d0e5ddce8b07bfe305e34a8f
Andreas Kluth <andreas.kluth@snabble.io>
2021-12-01 14:08:41 +0100

    Reproducer for gorm regression.

--- go.mod
+++ go.mod
@@ -10,11 +10,11 @@ require (
 	golang.org/x/crypto v0.0.0-20210505212654-3497b51f5e64 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gorm.io/driver/mysql v1.0.6
-	gorm.io/driver/postgres v1.1.0
-	gorm.io/driver/sqlite v1.1.4
-	gorm.io/driver/sqlserver v1.0.7
-	gorm.io/gorm v1.21.9
+	gorm.io/driver/mysql v1.2.1
+	gorm.io/driver/postgres v1.2.3
+	gorm.io/driver/sqlite v1.2.6
+	gorm.io/driver/sqlserver v1.2.1
+	gorm.io/gorm v1.22.4
 )
 
 replace gorm.io/gorm => ./gorm
--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +11,28 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	tests := []struct {
+		name string
+		id   int
+	}{
+		{name: "ok", id: 1},
+		{name: "broken", id: 0},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			coupon := Coupon{
+				AppliesToProduct: []*CouponProduct{{CouponId: 1, ProductId: test.id}},
+			}
+			DB.Session(&gorm.Session{FullSaveAssociations: true}).Create(&coupon)
 
-	DB.Create(&user)
+			var result Coupon
+			if err := DB.Preload("AppliesToProduct").First(&result, "id = ?", coupon.ID).Error; err != nil {
+				t.Errorf("Failed, got error: %v", err)
+			}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+			if len(result.AppliesToProduct) != 1 {
+				t.FailNow()
+			}
+		})
 	}
 }
