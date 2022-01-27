c1b2eb48cecf3a0fc7d11a064e3f03fab0635d48
Dawinia Lo <liaowandi@bytedance.com>
2022-01-27 16:48:50 +0800

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -9,12 +9,24 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	ids := make([]int, 0, 1000)
+	users := make([]User, 0, 1000)
+	for i := 1; i <= 1000; i++ {
+		ids = append(ids, i)
+		users = append(users, User{CompanyID: &i, Age: 10})
+	}
+	DB.Create(&users)
+	idsLen := len(ids)
+	start := 0
+	for start < idsLen {
+		end := start + 100
+		if end > idsLen {
+			end = idsLen
+		}
+		err := DB.Model(&User{}).Where("company_id in (?)", ids[start:end]).Update("age", 18).Error
+		if err != nil {
+			t.Errorf("Failed, got error: %v", err)
+		}
+		start = end
 	}
 }
