21ef42c674b9a8b0fed17eb03dbf821100ccbfa8
Anne Markis <anne.markis@splice.com>
2022-02-07 11:47:52 -0800

    Create test to show Omit doesn't work right for has one

--- main_test.go
+++ main_test.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
-// GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// GORM_BRANCH: v1.22.5
+// TEST_DRIVERS: mysql
 
 func TestGORM(t *testing.T) {
+	account := Account{Number: "1"}
+	DB.Create(&account)
+
 	user := User{Name: "jinzhu"}
+	user.Account = account
 
-	DB.Create(&user)
+	// Causes this: INSERT INTO `accounts` VALUES() ON DUPLICATE KEY UPDATE `user_id`=VALUES(`user_id`)
+	// This will create an empty account
+	DB.Omit("Account.*").Create(&user)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	var accounts []Account
+	DB.Find(&accounts)
+
+	if len(accounts) != 1 {
+		fmt.Print("Num Accounts")
+		fmt.Println(len(accounts))
+
+		for _, a := range accounts {
+			fmt.Print("Account number: ") // one of these will be an empty string
+			fmt.Println(a.Number)
+		}
+		t.Error("unexpected accounts")
+		t.Fail()
 	}
 }
