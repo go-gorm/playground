866fa92f741bde6abfca646fb5ffef627f1e89ba
Robert Brennan <accounts@rbren.io>
2022-02-14 23:10:41 -0500

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -9,12 +9,17 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	user1 := User{Name: "jinzhu", Age: 42}
+	user2 := User{Name: "jinzhu", Age: 99}
 
-	DB.Create(&user)
+	DB.Create(&user1)
+	DB.Create(&user2)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	results := []User{}
+	if err := DB.Distinct("name").Select("name", "age").Scan(&results).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+	if len(results) != 1 {
+		t.Errorf("Expected 1 result, got %d. %#v", len(results), results)
 	}
 }
