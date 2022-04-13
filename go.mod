a49ca8a36f426c23df4c8655c110722990819d86
lixu.ramen <lixu.ramen@bytedance.com>
2022-04-13 18:58:33 +0800

    feat(find_in_batches): add `FindInBatches` tests

--- go.mod
+++ go.mod
@@ -3,15 +3,16 @@ module gorm.io/playground
 go 1.16
 
 require (
+	github.com/bxcodec/faker/v3 v3.8.0
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
-	github.com/jackc/pgx/v4 v4.15.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.12 // indirect
-	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
-	gorm.io/driver/mysql v1.3.2
-	gorm.io/driver/postgres v1.3.1
+	github.com/stretchr/testify v1.7.1
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	gorm.io/driver/mysql v1.3.3
+	gorm.io/driver/postgres v1.3.4
 	gorm.io/driver/sqlite v1.3.1
-	gorm.io/driver/sqlserver v1.3.1
+	gorm.io/driver/sqlserver v1.3.2
 	gorm.io/gorm v1.23.3
 )
 
--- main_test.go
+++ main_test.go
@@ -1,20 +1,102 @@
 package main
 
 import (
+	"sync"
 	"testing"
+
+	"github.com/bxcodec/faker/v3"
+	"github.com/stretchr/testify/require"
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+var (
+	once = &sync.Once{}
+)
+
+const USERS = 100
+const SINGLE_BATCH = 5
+const LIMIT = 50
+
+func generateDatas(count int) (users []User) {
+	for i := 0; i < count; i++ {
+		users = append(users, User{Name: faker.Username()})
+	}
+
+	return
+}
+
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	once.Do(func() {
+		users := generateDatas(USERS)
+		DB.Create(&users)
+	})
+	var result []User
+	DB.Find(&result)
 
-	DB.Create(&user)
+	require.Equal(t, USERS, len(result), "fetched users are less than created")
+}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+func TestLimitClausedFindInBatches(t *testing.T) {
+	once.Do(func() {
+		users := generateDatas(USERS)
+		DB.Create(&users)
+	})
+
+	var records, singleBatch []User
+	ret := DB.Limit(LIMIT).FindInBatches(&singleBatch, SINGLE_BATCH, func(tx *gorm.DB, batch int) error {
+		records = append(records, singleBatch...)
+		return nil
+	})
+
+	affectedRows := int(ret.RowsAffected)
+
+	require.Equal(t, affectedRows, len(records), "the processed rows count should equal")
+
+	// FIXME: BELOW SHOULD NOT PASS!
+	// Below shows it will overwrite original limit clause
+	require.NotEqual(t, LIMIT, affectedRows, "the size with the affected rows should not equal")
+	require.Equal(t, USERS, affectedRows, "the size with the total users should equal")
+}
+
+func TestOffsetClausedFindInBatches(t *testing.T) {
+	once.Do(func() {
+		users := generateDatas(USERS)
+		DB.Create(&users)
+	})
+
+	for _, offset := range []int{2, 3, 5, 7} {
+		// it will make a below query
+		// || offset | single || offset | single || ... |
+		// ended up with a partial or full query in below forms:
+		// - || ... || offs||et
+		// - || ... || offset ||
+		// - || ... || offset | sing||le
+		// - || ... || offset | single ||
+		realBatches := int(USERS / (offset + SINGLE_BATCH))
+		totalRetrieved := realBatches * SINGLE_BATCH
+		remainEntries := USERS - (offset+SINGLE_BATCH)*realBatches
+		if remainEntries > offset {
+			totalRetrieved += remainEntries - offset
+		}
+
+		var records, singleBatch []User
+		ret := DB.Offset(offset).FindInBatches(&singleBatch, SINGLE_BATCH, func(tx *gorm.DB, batch int) error {
+			records = append(records, singleBatch...)
+			return nil
+		})
+
+		affectedRows := int(ret.RowsAffected)
+
+		require.Equal(t, affectedRows, len(records), "the processed rows count should equal")
+
+		// FIXME: BELOW SHOULD NOT PASS!
+		// It will not retrieve all rows
+		require.NotEqual(t, USERS, affectedRows, "the size with the affected rows should not equal")
+		// It will retrieve only totalRetrieved rows
+		require.Equal(t, totalRetrieved, affectedRows, "the size with the total users should equal")
 	}
 }
