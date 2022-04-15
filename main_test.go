58cae7caccedadb4a6e05f17583b6cc47e905157
100milsats <100milsats@protonmail.com>
2022-04-14 21:58:08 -0400

    break tests

--- main_test.go
+++ main_test.go
@@ -17,4 +17,7 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	vehicle := Vehicle{VehicleID: "id-1"}
+	DB.Create(&vehicle)
 }
