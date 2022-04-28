09195ea133fceb1a289a12382c73e23ec8d6f8a4
Tom <tom@raftt.io>
2022-04-28 11:43:15 +0300

    Reproduce migration bug
    Reproduce failure to migrate the same model more than twice with the
    postgres driver.
    `ERROR: cached plan must not change result type (SQLSTATE 0A000)`

--- main_test.go
+++ main_test.go
@@ -8,13 +8,39 @@ import (
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+func CreateObject() error {
+	type Object struct{}
+	return DB.AutoMigrate(&Object{})
+}
+
+func AddField1() error {
+	type Object struct {
+		Field1 string
+	}
+	return DB.AutoMigrate(&Object{})
+}
+
+func AddField2() error {
+	type Object struct {
+		Field2 string
+	}
+	return DB.AutoMigrate(&Object{})
+}
+
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	if DB.Dialector.Name() != "postgres" {
+		return
+	}
+	if err := CreateObject(); err != nil {
+		t.Errorf("FAILED CREATING OBJECT")
+		return
+	}
+	if err := AddField1(); err != nil {
+		t.Errorf("FAILED ADDING FIELD1")
+		return
+	}
+	if err := AddField2(); err != nil {
+		t.Errorf("I am sad :(")
+		return
 	}
 }
