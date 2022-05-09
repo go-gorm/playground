e5bb6e7205a096b426f2af5c279ecb5cb31e13cc
Jinzhu <wosmvp@gmail.com>
2022-05-09 09:50:20 +0800

    test to sql

--- main_test.go
+++ main_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -17,4 +20,8 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	fmt.Println(DB.ToSQL(func(tx *gorm.DB) *gorm.DB {
+		return tx.Create(user)
+	}))
 }
