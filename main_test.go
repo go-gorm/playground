0c2a1a29cdd3bd55c074ed7d3a6197958fe61968
mnussbaum <michaelnussbaum08@gmail.com>
2022-06-02 18:22:51 -0700

    Triggers failure trying to use nested transactions
    It appears that nested transactions aren't support using the manual
    `db.Begin`/`db.Commit` transaction methods

--- main_test.go
+++ main_test.go
@@ -11,10 +11,16 @@ import (
 func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
 
-	DB.Create(&user)
+	tx := DB.Begin()
+	tx2 := tx.Begin()
+	tx2.Create(&user)
 
 	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	if err := tx2.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+	if err := tx2.Commit().Error; err != nil {
+		t.Errorf("Failed to commit: %v", err)
+	}
+	tx.Commit()
 }
