eff208700c9086d69e1fab648aba329269955333
Jan Vrska <jan.vrska@pilulka.cz>
2022-07-23 14:55:55 +0200

    reproduce bug

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Table struct {
+	Id   uint
+	Test bool `gorm:"default:1"`
+}
 
 func main() {
-	fmt.Println("vim-go")
+	dsn := "gorm:gorm@tcp(mysql)/gorm?charset=utf8mb4&parseTime=True&loc=Local"
+	db, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db.AutoMigrate(&Table{})
+
+	createTestRecord := Table{Test: false}
+	db.Create(&createTestRecord)
 }
