4e78857ac3c41e0e07db3876083b6fb8b8539b69
zuofengwei <zuofengwei@qianxin.com>
2022-08-01 18:42:53 +0800

    add a create function bug of mysql gorm

--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ import (
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS:  mysql
 
 const (
 	_CreatedTableSql = `CREATE TABLE location (
