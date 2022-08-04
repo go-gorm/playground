0ff96ec217ae0091174d7c5d2fa06669119f6136
Tom Berger <tom@getplayback.com>
2022-08-04 10:00:33 -0400

    Show scan regression where same object is returned

--- main_test.go
+++ main_test.go
@@ -2,19 +2,52 @@ package main
 
 import (
 	"testing"
+
+	"github.com/google/uuid"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: postgres
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	tomID := uuid.New().String()
+	DB.Create(&User{
+		ID:   tomID,
+		Name: "tom",
+	})
 
-	DB.Create(&user)
+	bobID := uuid.New().String()
+	DB.Create(&User{
+		ID:   bobID,
+		Name: "bob",
+	})
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	DB.Create(&UserAvatar{
+		ID:     uuid.New().String(),
+		UserID: tomID,
+		Size:   1337,
+	})
+
+	DB.Create(&UserAvatar{
+		ID:     uuid.New().String(),
+		UserID: bobID,
+		Size:   2674,
+	})
+
+	var usersRoomAvatars usersRoomAvatars
+	d := DB.Table("users").
+		Select(
+			"users.*",
+			"user_avatars.id as avatar_id",
+			"user_avatars.user_id as avatar_user_id",
+			"user_avatars.size as avatar_size").
+		Joins("left join user_avatars on user_avatars.user_id = users.id").
+		Group("users.id, user_avatars.id")
+
+	d.Scan(&usersRoomAvatars)
+
+	for _, ua := range usersRoomAvatars {
+		t.Logf("avatar ID: %s", ua.Avatar.ID)
 	}
 }
