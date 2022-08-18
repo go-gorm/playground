fe6f338cb4e037c4c31b5d2142140924cdad1261
YeCheng <jacky.ye@fastonetech.com>
2022-08-18 10:16:28 +0800

    add test playground

--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module gorm.io/playground
 go 1.16
 
 require (
+	github.com/denisenkom/go-mssqldb v0.12.2 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
-	github.com/jackc/pgx/v4 v4.16.1 // indirect
-	golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122 // indirect
-	gorm.io/driver/mysql v1.3.3
-	gorm.io/driver/postgres v1.3.5
-	gorm.io/driver/sqlite v1.3.2
+	github.com/jackc/pgx/v4 v4.17.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.15 // indirect
+	gorm.io/driver/mysql v1.3.6
+	gorm.io/driver/postgres v1.3.9
+	gorm.io/driver/sqlite v1.3.6
 	gorm.io/driver/sqlserver v1.3.2
-	gorm.io/gorm v1.23.4
+	gorm.io/gorm v1.23.8
 )
 
 replace gorm.io/gorm => ./gorm
--- main_test.go
+++ main_test.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
+	"time"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -17,4 +23,50 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+}
+
+// import csv data.
+func importData() {
+	dbFile := filepath.Join(os.TempDir(), "gorm.db")
+	os.Remove(dbFile)
+
+	file, err := os.OpenFile(".init.sql", os.O_WRONLY|os.O_APPEND|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	modeCmd := fmt.Sprintln(".mode csv")
+	if _, err := file.Write([]byte(modeCmd)); err != nil {
+		log.Fatalf("error while writing the file. %v", err)
+	}
+
+	importCmd := fmt.Sprintf(".import %v %v \n", "data.csv", "data")
+	file.WriteString(importCmd)
+
+	// import to database.
+	exec.Command("sqlite3", "--init", file.Name(), filepath.Join(os.TempDir(), "gorm.db")).Run()
+
+	if err := os.Remove(file.Name()); err != nil {
+		log.Fatalf("error while removing the file. %v", err)
+	}
+
+}
+
+func TestGORMTime(t *testing.T) {
+
+	importData()
+
+	var result []Data
+
+	// correct way query.
+	// DB.Table("data").Where("start_time >= ?", "2022-07-01 00:00:00").Find(&result)
+
+	// error way query.
+	time, _ := time.Parse("2006-01-02 15:04:05", "2022-07-01 00:00:00")
+	DB.Table("data").Where("start_time >= ?", time).Find(&result)
+
+	if len(result) != 2 {
+		t.Errorf("Failed, got %v", len(result))
+	}
+
 }
