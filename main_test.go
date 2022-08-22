e7a07b84d223ae0a20014e720389636956a1a981
feiyang <i520gfy@gmail.com>
2022-08-22 12:08:31 +0800

    feat: add test case for gorm update

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -9,12 +10,25 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	user := User{Name: "test1", Age: 20}
 
 	DB.Create(&user)
 
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+	updateName, updateAge := "test2", 22
+	err := DB.Exec("update users set name = ? and age = ? where id = ?", updateName, updateAge, user.ID).Error
+	if err == nil {
+		t.Error("Except err: Truncated incorrect DOUBLE value, but got nil")
+	}
+
+	var updatedUser User
+	if err := DB.First(&updatedUser, user.ID).Error; err != nil {
+		t.Errorf("Failed, got new user error: %v", err)
+	}
+	if reflect.DeepEqual(result, updatedUser) {
+		t.Errorf("Failed, update new user failed! old: %v, new: %v", result, updatedUser)
 	}
 }
