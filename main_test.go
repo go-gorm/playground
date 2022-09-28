1dc93bbc179964dbb1a89ad581cc4b65fbe422d8
Jakob Rockenbauch <jakob.rockenbauch@snabble.io>
2022-09-28 16:51:49 +0200

    add reproducer for invalid loading of embedded fields

--- main_test.go
+++ main_test.go
@@ -6,15 +6,33 @@ import (
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS:  postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	user := User{
+		Name: "jinzhu",
+		Age:  1,
+	}
+	anotherUser := User{Name: "with-company", EmbeddedCompany: &Company{
+		ID:   2,
+		Name: "company",
+	}}
 
 	DB.Create(&user)
+	DB.Create(&anotherUser)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	var result []User
+	err := DB.
+		Order("age DESC"). // make sure user with company is always loaded first
+		Find(&result).
+		Error
+	if err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+
+	foundUser := result[0]
+
+	if foundUser.EmbeddedCompany != nil && foundUser.EmbeddedCompany.Name != "" {
+		t.Errorf("user jinzhu should not have embedded company: %+v", foundUser.EmbeddedCompany)
 	}
 }
--- models.go
+++ models.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"time"
 
 	"gorm.io/gorm"
 )
@@ -12,21 +11,10 @@ import (
 // He speaks many languages (many to many) and has many friends (many to many - single-table)
 // His pet also has one Toy (has one - polymorphic)
 type User struct {
-	gorm.Model
-	Name      string
-	Age       uint
-	Birthday  *time.Time
-	Account   Account
-	Pets      []*Pet
-	Toys      []Toy `gorm:"polymorphic:Owner"`
-	CompanyID *int
-	Company   Company
-	ManagerID *uint
-	Manager   *User
-	Team      []User     `gorm:"foreignkey:ManagerID"`
-	Languages []Language `gorm:"many2many:UserSpeak"`
-	Friends   []*User    `gorm:"many2many:user_friends"`
-	Active    bool
+	ID              uint `gorm:"primarykey"`
+	Age             uint
+	Name            string
+	EmbeddedCompany *Company `gorm:"embedded;embeddedPrefix:company"`
 }
 
 type Account struct {
