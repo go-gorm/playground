2ca0fad62b99569fe252cf15eaa4d82c457fa8de
Robert Hafner <robert.hafner@sas.com>
2022-10-04 09:26:34 -0400

    illustrate limit=0 bug

--- main_test.go
+++ main_test.go
@@ -18,3 +18,29 @@ func TestGORM(t *testing.T) {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
+
+func TestLimitZero(t *testing.T) {
+	type LimitZero struct {
+		Name string
+	}
+
+	DB.Migrator().DropTable(&LimitZero{})
+
+	if err := DB.AutoMigrate(&LimitZero{}); err != nil {
+		t.Fatalf("Failed to migrate for uuid default value, got error: %v", err)
+	}
+
+	lz := LimitZero{Name: "jinzhu"}
+	if err := DB.Create(&lz).Error; err != nil {
+		t.Fatalf("should be able to create data, but got %v", err)
+	}
+
+	result := make([]LimitZero, 0)
+	if err := DB.Model(LimitZero{}).Limit(0).Find(&result).Error; err != nil {
+		t.Errorf("No error should happen, but got %v", err)
+	}
+
+	if len(result) > 0 {
+		t.Fatal("should have zero length")
+	}
+}
