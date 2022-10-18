5e28b23aea0d9b3d449ab2123bab7671a60b6f3f
SystemGlitch <jeremy.la@outlook.fr>
2022-10-18 17:02:54 +0200

    Demo joins preloading ignores select clause

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +11,25 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	user := User{
+		Model: gorm.Model{ID: 1},
+		Name:  "jinzhu",
+		Manager: &User{
+			Model: gorm.Model{ID: 2},
+			Name:  "manager name",
+			Age:   42,
+		},
+	}
 
 	DB.Create(&user)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	var result *User
+	if err := DB.Joins("Manager", DB.Session(&gorm.Session{NewDB: true}).Select("id", "name")).First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+		return
+	}
+
+	if result.Manager.Age != 0 {
+		t.Errorf("Failed, only expected name to be selected, but got age %v", result.Manager.Age)
 	}
 }
