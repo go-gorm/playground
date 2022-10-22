bddce4b3accf545f93c579cc8ad182553045801f
Mevaser <mevaser.rotner@gmail.com>
2022-10-22 05:32:38 +0300

    added failed test

--- main_test.go
+++ main_test.go
@@ -1,7 +1,17 @@
 package main
 
 import (
+	"log"
+	"strconv"
+	"sync"
 	"testing"
+	"time"
+
+	"gorm.io/gorm/clause"
+)
+
+const (
+	count = 30
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +19,45 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	languages := []Language{}
+	for i := 0; i < count; i++ {
+		language := Language{Code: strconv.Itoa(i), Name: strconv.Itoa(i)}
+		DB.Create(&language)
+		languages = append(languages, language)
+	}
 
-	DB.Create(&user)
+	////////////////////////////////////////////////////////////////
+	// When this is deleted, the test passes also when preloading.
+	whenThisIsDeletedTheTestPasses := User{}
+	DB.Create(&whenThisIsDeletedTheTestPasses)
+	////////////////////////////////////////////////////////////////
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	user1 := User{}
+	if true { // If not preloading, the test passes.
+		DB.Preload(clause.Associations).First(&user1)
+	} else {
+		DB.First(&user1)
+	}
+
+	var wg sync.WaitGroup
+	for _, language := range languages {
+		wg.Add(1)
+		go func(userCopy User, languageCopy Language) {
+			defer wg.Done()
+			err := DB.Model(&userCopy).Association("Languages").Append(&languageCopy)
+			if err != nil {
+				log.Printf("[!] Error white appending post to user, %s", err)
+				return
+			}
+			time.Sleep(time.Second) // Simulating task.
+		}(user1, language)
+	}
+	wg.Wait()
+
+	user2 := User{}
+	DB.Preload(clause.Associations).First(&user2)
+	log.Println(len(user2.Languages) == count, len(user2.Languages))
+	if len(user2.Languages) != count {
+		t.Errorf("Failed, %d should be %d", len(user2.Languages), count)
 	}
 }
