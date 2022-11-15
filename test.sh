7cc1a3e9b36e2f165a811313c7fdcd1860687420
airplayx <airplayx@gmail.com>
2022-11-15 10:40:54 +0800

    get `HaveCountList` function return no all fields

--- test.sh
+++ test.sh
@@ -9,8 +9,22 @@ fi
 
 [ -d gorm ] || (echo "git clone --depth 1 -b $(cat main_test.go | grep GORM_BRANCH | awk '{print $3}') $(cat main_test.go | grep GORM_REPO | awk '{print $3}')"; git clone --depth 1 -b $(cat main_test.go | grep GORM_BRANCH | awk '{print $3}') $(cat main_test.go | grep GORM_REPO | awk '{print $3}'))
 
-cp go.mod go.mod.bak
-sed '/gorm.io\/driver/d' go.mod.bak > go.mod
+go get -u -t ./...
+
+# SqlServer for Mac M1
+if [[ -z $GITHUB_ACTION ]]; then
+  if [[ $(uname -a) == *" arm64" ]]; then
+    MSSQL_IMAGE=mcr.microsoft.com/azure-sql-edge docker-compose up --detach --quiet-pull || true
+    echo "starting"
+    go install github.com/microsoft/go-sqlcmd/cmd/sqlcmd@latest || true
+    SQLCMDPASSWORD=LoremIpsum86 sqlcmd -U sa -S localhost:9930 -Q "IF DB_ID('gorm') IS NULL CREATE DATABASE gorm" > /dev/null || true
+    SQLCMDPASSWORD=LoremIpsum86 sqlcmd -U sa -S localhost:9930 -Q "IF SUSER_ID (N'gorm') IS NULL CREATE LOGIN gorm WITH PASSWORD = 'LoremIpsum86';" > /dev/null || true
+    SQLCMDPASSWORD=LoremIpsum86 sqlcmd -U sa -S localhost:9930 -Q "IF USER_ID (N'gorm') IS NULL CREATE USER gorm FROM LOGIN gorm; ALTER SERVER ROLE sysadmin ADD MEMBER [gorm];" > /dev/null || true
+  else
+    docker-compose up --detach --quiet-pull
+    echo "starting..."
+  fi
+fi
 
 for dialect in "${dialects[@]}" ; do
   if [ "$GORM_DIALECT" = "" ] || [ "$GORM_DIALECT" = "${dialect}" ]
@@ -24,5 +38,3 @@ for dialect in "${dialects[@]}" ; do
     fi
   fi
 done
-
-mv go.mod.bak go.mod
