df4f28aebf85fd2542fa7e281ccb103acaf05ef1
曾万忠 <zwjzxh520@gmail.com>
2022-11-25 18:21:00 +0800

    阿里云 PolarDB 代码示例

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"time"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	dsn := "xxx:xxxx@tcp(xxxx)/xxx?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("connect db get error: %v", err)
+		return
+	}
+	srcDb, err := db.DB()
+	srcDb.SetMaxIdleConns(1)
+	srcDb.SetMaxOpenConns(1)
+	srcDb.SetConnMaxLifetime(time.Hour)
+
+	db.Connection(func(dbs *gorm.DB) error {
+		if result := dbs.Exec("create temporary table temp_order_weixin as select * from `order` where bank = 'weixin' AND id > 1"); result.Error != nil {
+			log.Fatalf("create temp table get error: %v", result.Error)
+			return result.Error
+		}
+
+		var count int64
+		if result := dbs.Table("temp_order_weixin as o").
+			Select("count(o.id)").
+			Where("o.id > ?", 1).
+			Count(&count); result.Error != nil {
+			log.Fatalf("count get error: %v", result.Error)
+			return result.Error
+		}
+		return nil
+	})
 }
