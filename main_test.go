441a7caa6f80fbbd30750a7cd999b2247ec34e2e
Tim Cassidy <cassidy@dropfake.com>
2022-12-11 11:16:22 -0800

    remove fmt logs + split the two tests into individual unit tests

--- main_test.go
+++ main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	db_sql "database/sql"
-	"fmt"
 	"testing"
 
 	"gorm.io/gorm"
@@ -16,25 +15,28 @@ func TestBindingValuesUsingRaw(t *testing.T) {
 	dryRunDB := DB.Session(&gorm.Session{DryRun: true})
 
 	query := &User{Name: "asdf", Age: 10}
-	sql2 := dryRunDB.Where(query).Find(&User{}).Statement.SQL.String()
-	fmt.Println("fmt2: ", sql2)
 
 	sqlStatement := dryRunDB.Where(query).Find(&User{}).Statement
 	sqlStr := sqlStatement.SQL.String()
-	fmt.Println("sqlStr: ", sqlStr)
 
 	rawRes := dryRunDB.Raw(sqlStr, "asdf", 10)
 	boundSqlStr := rawRes.Statement.SQL.String()
-	fmt.Println("boundSqlStr: ", boundSqlStr)
 	if boundSqlStr != "SELECT * FROM `users` WHERE `users`.`name` = \"asdf\" AND `users`.`age` = 10 AND `users`.`deleted_at` IS NULL" {
 		t.Fatalf("invalid sql generated, got %v", boundSqlStr)
 	}
+}
+func TestBindingValuesUsingRawWithNamedArg(t *testing.T) {
+	dryRunDB := DB.Session(&gorm.Session{DryRun: true})
+
+	query := &User{Name: "asdf", Age: 10}
+
+	sqlStatement := dryRunDB.Where(query).Find(&User{}).Statement
+	sqlStr := sqlStatement.SQL.String()
 
 	nameArg := db_sql.Named("Name", "asdf")
 	ageArg := db_sql.Named("Age", 10)
-	rawRes = dryRunDB.Raw(sqlStr, nameArg, ageArg)
-	boundSqlStr = rawRes.Statement.SQL.String()
-	fmt.Println("boundSqlStr: ", boundSqlStr)
+	rawRes := dryRunDB.Raw(sqlStr, nameArg, ageArg)
+	boundSqlStr := rawRes.Statement.SQL.String()
 	if boundSqlStr != "SELECT * FROM `users` WHERE `users`.`name` = \"asdf\" AND `users`.`age` = 10 AND `users`.`deleted_at` IS NULL" {
 		t.Fatalf("invalid sql generated, got %v", boundSqlStr)
 	}
