324fd002b50fd0715d44877aeb053eb28d975af2
Vinicius Rangel <me@viniciusrangel.dev>
2022-12-21 12:14:49 -0300

    test

--- models.go
+++ models.go
@@ -1,60 +1,49 @@
 package main
 
 import (
-	"database/sql"
-	"time"
-
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"gorm.io/gorm/schema"
 )
 
-// User has one `Account` (has one), many `Pets` (has many) and `Toys` (has many - polymorphic)
-// He works in a Company (belongs to), he has a Manager (belongs to - single-table), and also managed a Team (has many - single-table)
-// He speaks many languages (many to many) and has many friends (many to many - single-table)
-// His pet also has one Toy (has one - polymorphic)
+type UpdateCount uint
+
 type User struct {
-	gorm.Model
-	Name      string
-	Age       uint
-	Birthday  *time.Time
-	Account   Account
-	Pets      []*Pet
-	Toys      []Toy `gorm:"polymorphic:Owner"`
-	CompanyID *int
-	Company   Company
-	ManagerID *uint
-	Manager   *User
-	Team      []User     `gorm:"foreignkey:ManagerID"`
-	Languages []Language `gorm:"many2many:UserSpeak"`
-	Friends   []*User    `gorm:"many2many:user_friends"`
-	Active    bool
+	ID          uint `gorm:"primarykey"`
+	Name        string
+	UpdateCount UpdateCount `gorm:"default:0"`
 }
 
-type Account struct {
-	gorm.Model
-	UserID sql.NullInt64
-	Number string
+func (u UpdateCount) UpdateClauses(field *schema.Field) []clause.Interface {
+	return []clause.Interface{updateCountClause{Field: field}}
 }
 
-type Pet struct {
-	gorm.Model
-	UserID *uint
-	Name   string
-	Toy    Toy `gorm:"polymorphic:Owner;"`
+func (u UpdateCount) CreateClauses(field *schema.Field) []clause.Interface {
+	return []clause.Interface{updateCountClause{Field: field}}
 }
 
-type Toy struct {
-	gorm.Model
-	Name      string
-	OwnerID   string
-	OwnerType string
+type updateCountClause struct {
+	Field *schema.Field
 }
 
-type Company struct {
-	ID   int
-	Name string
+func (u updateCountClause) Name() string {
+	return ""
 }
 
-type Language struct {
-	Code string `gorm:"primarykey"`
-	Name string
+func (u updateCountClause) Build(builder clause.Builder) {
+}
+
+func (u updateCountClause) MergeClause(c *clause.Clause) {
+}
+
+func (u updateCountClause) ModifyStatement(stmt *gorm.Statement) {
+	if stmt.SQL.Len() == 0 && !stmt.Statement.Unscoped {
+		var count = 0
+		v, zero := u.Field.ValueOf(stmt.Context, stmt.ReflectValue)
+		if !zero {
+			count = int(v.(UpdateCount))
+		}
+		stmt.AddClause(clause.Set{{Column: clause.Column{Name: u.Field.DBName}, Value: count}})
+		stmt.SetColumn(u.Field.DBName, count, true)
+	}
 }
