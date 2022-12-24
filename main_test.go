bc85766b92e35cdf74bb0c0313fa17f625c2495c
Defool <defool@foxmail.com>
2022-12-24 21:57:59 +0800

    Unexpected alter table

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -8,13 +11,32 @@ import (
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+type MyTable struct {
+	Def string `gorm:"size:512;index:idx_def,unique"`
+	Abc string `gorm:"size:65000000"`
+}
+
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	sql := "CREATE TABLE `my_tables` (`def` varchar(512),`abc` longtext,UNIQUE INDEX `idx_def` (`def`))"
+	if err := DB.Exec(sql).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+	// stdout to pipe
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	DB.DryRun = true
+	if err := DB.AutoMigrate(&MyTable{}); err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	// get stdout from pipe
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	if strings.Contains(string(out), "ALTER TABLE") {
+		t.Errorf("Unexpected alter table: %v", string(out))
 	}
 }
