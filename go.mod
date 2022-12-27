ceab144174332e428cbc33da5896d3ea540554d7
Nomango <nomango@qq.com>
2022-12-27 10:27:06 +0800

    test -:migration

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/jackc/pgtype v1.13.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16 // indirect
-	github.com/microsoft/go-mssqldb v0.18.0 // indirect
+	github.com/microsoft/go-mssqldb v0.19.0 // indirect
 	golang.org/x/crypto v0.4.0 // indirect
 	gorm.io/driver/mysql v1.4.4
 	gorm.io/driver/postgres v1.4.5
--- main_test.go
+++ main_test.go
@@ -11,21 +11,32 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 type T1 struct {
-	gorm.Model
-	Content string `gorm:"type:varchar(30)"`
+	ID      uint
+	Content string
 }
 
 type T2 struct {
+	ID      uint
+	Content string
+}
+
+type T3 struct {
 	gorm.Model
+
 	T1ID uint
 	T1   *T1
+
+	T2ID uint
+	T2   *T2 `gorm:"-:migration"`
 }
 
 func TestGORM(t *testing.T) {
-	// Table t1 exists before
+	_ = DB.Migrator().DropTable(&T1{}, &T2{}, &T3{})
+
+	// Table t1 & t2 exists before
 	err := DB.Exec(`CREATE TABLE t1 (
 		id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-		content text NOT NULL,
+		content varchar(30) NOT NULL,
 		PRIMARY KEY (id)
 	);`).Error
 	if err != nil {
@@ -33,8 +44,18 @@ func TestGORM(t *testing.T) {
 		t.FailNow()
 	}
 
-	// Migrate t2
-	err = DB.AutoMigrate(&T2{})
+	err = DB.Exec(`CREATE TABLE t2 (
+		id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+		content varchar(30) NOT NULL,
+		PRIMARY KEY (id)
+	);`).Error
+	if err != nil {
+		t.Errorf("Failed, got error: %v", err)
+		t.FailNow()
+	}
+
+	// Migrate t3
+	err = DB.AutoMigrate(&T3{})
 	if err != nil {
 		t.Errorf("Failed, got error: %v", err)
 		t.FailNow()
@@ -50,9 +71,24 @@ func TestGORM(t *testing.T) {
 	for _, c := range columns {
 		if c.Name() == "content" {
 			columnType, _ := c.ColumnType()
-			if columnType != "text" {
-				t.Errorf("Expected type '%v', but got '%v'", "text", columnType)
-				t.FailNow()
+			if columnType != "varchar(30)" {
+				t.Errorf("T1: Expected type '%v', but got '%v'", "varchar(30)", columnType)
+			}
+		}
+	}
+
+	// t2 should not be modified
+	columns, err = DB.Migrator().ColumnTypes(&T2{})
+	if err != nil {
+		t.Errorf("Failed, got error: %v", err)
+		t.FailNow()
+	}
+
+	for _, c := range columns {
+		if c.Name() == "content" {
+			columnType, _ := c.ColumnType()
+			if columnType != "varchar(30)" {
+				t.Errorf("T2: Expected type '%v', but got '%v'", "varchar(30)", columnType)
 			}
 		}
 	}
