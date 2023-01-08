d89f566bbe385a05b6e48a97c8d117790ea7eea1
Morre <morre@mor.re>
2023-01-08 16:07:46 +0100

    test: failing test

--- go.mod
+++ go.mod
@@ -3,13 +3,17 @@ module gorm.io/playground
 go 1.16
 
 require (
-	github.com/denisenkom/go-mssqldb v0.12.2 // indirect
-	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
-	gorm.io/driver/mysql v1.4.1
-	gorm.io/driver/postgres v1.4.4
-	gorm.io/driver/sqlite v1.4.2
+	github.com/jackc/pgtype v1.13.0 // indirect
+	github.com/jackc/pgx/v4 v4.17.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/microsoft/go-mssqldb v0.19.0 // indirect
+	github.com/stretchr/testify v1.8.1
+	golang.org/x/crypto v0.5.0 // indirect
+	gorm.io/driver/mysql v1.4.5
+	gorm.io/driver/postgres v1.4.6
+	gorm.io/driver/sqlite v1.4.4
 	gorm.io/driver/sqlserver v1.4.1
-	gorm.io/gorm v1.24.0
+	gorm.io/gorm v1.24.3
 )
 
 replace gorm.io/gorm => ./gorm
--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -13,8 +15,15 @@ func TestGORM(t *testing.T) {
 
 	DB.Create(&user)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	var results []User
+	if err := DB.Where(&User{
+		Active: false,
+	}, "Active").Find(&results, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	// This must have 2 results since the first resource was created when the column did not exist yet
+	// I therefore assume that it should have the default value when using AutoMigrate.
+	// The second record is created when the column exits and therefore has the default value set.
+	assert.Len(t, results, 2)
 }
