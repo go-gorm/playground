6c16a03a83aae48f6554b45add9c0be5d27fa750
ro006714 <Robert.Perry@mda.space>
2023-01-18 19:10:37 -0800

    add test

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -9,12 +11,38 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
 
-	DB.Create(&user)
+	require.NoError(t, DB.Exec(`DROP VIEW IF EXISTS first_foo`).Error)
+	require.NoError(t, DB.Exec(`DROP TABLE IF EXISTS foo`).Error)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
-	}
+	err := DB.AutoMigrate(&Foo1{})
+	require.NoError(t, err)
+
+	err = DB.Exec(`CREATE VIEW first_foo AS SELECT name FROM foo LIMIT 1`).Error
+	assert.NoError(t, err)
+
+	// Now migrate table by changing type of column
+	err = DB.AutoMigrate(&Foo2{})
+	assert.NoError(t, err)
+
+}
+
+type Foo1 struct {
+	ID   string
+	Name string
+	Age  int
+}
+
+type Foo2 struct {
+	ID   string
+	Name string `gorm:"type:varchar(255)"`
+	Age  int
+}
+
+func (Foo1) TableName() string {
+	return "foo"
+}
+
+func (Foo2) TableName() string {
+	return "foo"
 }
