b119a546aabb348a54481e68b917003aa69254fe
zchenyu <zchenyu@fireworks.ai>
2023-02-16 13:13:53 -0800

    Chained clauses reproducible example

--- main_test.go
+++ main_test.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm/clause"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
-func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+func TestClauseChaining(t *testing.T) {
+	DB.Create(&Company{
+		ID: 1,
+		Name: "foo",
+	})
 
-	DB.Create(&user)
+	DB.Create(&Company{
+		ID: 2,
+		Name: "bar",
+	})
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	var results []Company
+	DB.Clauses(&clause.Expr{
+		SQL: `id = 1 OR id = 2`,
+	}, &clause.Expr{
+		SQL: `name = "bar"`,
+	}).Find(&results)
+
+	if l := len(results); l != 1 {
+		t.Fatalf("expected 1, got %d", l)
 	}
 }
