542e35f4e46b2985ab9b0363121b50280a2bacc7
liangjunmo <liangjmcn@qq.com>
2023-02-19 13:01:18 +0800

    go-gorm/sharding DATA RACE

--- main_test.go
+++ main_test.go
@@ -1,20 +1,85 @@
 package main
 
 import (
+	"context"
+	"gorm.io/sharding"
 	"testing"
+	"time"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+type ShardingModel struct {
+	ID  uint `gorm:"column:id;type:int unsigned;not null;auto_increment;primary key;'" json:"id"`
+	UID uint `gorm:"column:uid;type:int unsigned;not null;" json:"uid"`
+}
+
+func (*ShardingModel) TableName() string {
+	return "sharding_model"
+}
+
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	err := DB.Use(sharding.Register(sharding.Config{
+		ShardingKey:         "uid",
+		NumberOfShards:      2,
+		PrimaryKeyGenerator: sharding.PKCustom,
+		PrimaryKeyGeneratorFn: func(tableIdx int64) int64 {
+			return 0
+		},
+	}, &ShardingModel{}))
+	if err != nil {
 		t.Errorf("Failed, got error: %v", err)
+		return
+	}
+
+	err = DB.AutoMigrate(&ShardingModel{})
+	if err != nil {
+		t.Errorf("Failed, got error: %v", err)
+		return
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	ch := make(chan error)
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				err := DB.Model(&ShardingModel{}).Where("uid = ?", 100).Find(&[]ShardingModel{}).Error
+				if err != nil {
+					ch<-err
+					return
+				}
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				err := DB.Model(&ShardingModel{}).Where("uid = ?", 100).Find(&[]ShardingModel{}).Error
+				if err != nil {
+					ch<-err
+					return
+				}
+			}
+		}
+	}()
+
+	select {
+	case <-time.After(time.Millisecond * 100):
+		cancel()
+	case err = <-ch:
+		cancel()
+		if err != nil {
+			t.Errorf("Failed, got error: %v", err)
+		}
 	}
 }
