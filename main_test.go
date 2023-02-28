97be5dfa8632904a1ec9292ace217186ed6e6610
Jeffry Luqman <jeffry.luqman@gmail.com>
2023-02-28 14:06:19 +0700

    fail to alter column from smallint to boolean in postgres

--- main_test.go
+++ main_test.go
@@ -18,3 +18,29 @@ func TestGORM(t *testing.T) {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
+
+func TestMigrateExistingSmallintBoolColumnPG(t *testing.T) {
+	if DB.Dialector.Name() != "postgres" {
+		return
+	}
+
+	type ColumnStruct struct {
+		Name     string
+		IsActive bool `gorm:"type:smallint"`
+	}
+
+	type ColumnStruct2 struct {
+		Name     string
+		IsActive bool // change existing boolean column from smallint or other to boolean
+	}
+
+	DB.Migrator().DropTable(&ColumnStruct{})
+
+	if err := DB.AutoMigrate(&ColumnStruct{}); err != nil {
+		t.Errorf("Failed to migrate, got %v", err)
+	}
+
+	if err := DB.Table("column_structs").AutoMigrate(&ColumnStruct2{}); err != nil {
+		t.Fatalf("no error should happened when auto migrate column, but got %v", err)
+	}
+}
