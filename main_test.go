a8162cab1d4d6dd1df11da1f747b68f726e69287
Jinzhu <wosmvp@gmail.com>
2023-03-07 19:20:34 +0800

    update

--- main_test.go
+++ main_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"testing"
+	"time"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -17,4 +20,43 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	DB.AutoMigrate(&EvalJob{})
+
+	DB.Create(&EvalJob{
+		Model: gorm.Model{
+			ID:        0,
+			CreatedAt: time.Now().Add(-24 * time.Hour),
+			UpdatedAt: time.Now().Add(-24 * time.Hour),
+		},
+		PSM:      "inf.lidar.api",
+		TaskID:   "taskid3",
+		Operator: "xiehengjian",
+		JobName:  "jobname",
+		// Status:      models.EvalFailed,
+		// RelatedData: map[RelatedDataType]interface{}{},
+		// ProfitTimes: make(map[string]*ProfitTime),
+	})
 }
+
+type EvalJob struct {
+	gorm.Model
+	PSM      string
+	TaskID   string // 任务ID
+	Operator string // 该job的发起人
+	JobName  string
+	// RespResult  ProfitDetail                    // 收益结果
+	// Status      EvalJobStatus                   // 状态，1 已提交，2 计算中，3 暂停，4 失败 5 成功
+	RelatedData map[RelatedDataType]interface{} `gorm:"serializer:json;type:text"`
+	Region      string
+	Internal    int // 1代表内部计算，0代表外部计算
+	// 新增字段
+	ProfitTimes map[string]*ProfitTime `gorm:"serializer:json"` // vregion和对应的计算时间
+	ReportData  Report                 `gorm:"serializer:json"` // 收益报告详细信息
+}
+
+type RelatedDataType int
+
+type ProfitTime struct{}
+
+type Report struct{}
