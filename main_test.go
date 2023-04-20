3fce34bccf2ada5d82feda3f69781011a8699220
耗子 <i@haozi.net>
2023-04-21 04:13:05 +0800

    fix: avoid data race

--- main_test.go
+++ main_test.go
@@ -13,19 +13,20 @@ import (
 var wg sync.WaitGroup
 
 func TestGORM(t *testing.T) {
-	// Point ID to 1
-	model := gorm.Model{
-		ID: 1,
-	}
 
-	user := User{
-		Model: model,
-	}
-
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
+
+			// Point ID to 1
+			model := gorm.Model{
+				ID: 1,
+			}
+
+			user := User{
+				Model: model,
+			}
 			if err := DB.FirstOrCreate(&user).Error; err != nil {
 				t.Errorf("Failed, got error: %v", err)
 			}
