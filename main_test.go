6a255b8bf29fc160b3caf4e6140a86181ca41cc6
耗子 <i@haozi.net>
2023-04-21 04:04:56 +0800

    feat: add race condition

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"gorm.io/gorm"
+	"sync"
 	"testing"
 )
 
@@ -8,13 +10,27 @@ import (
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+var wg sync.WaitGroup
+
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	// Point ID to 1
+	model := gorm.Model{
+		ID: 1,
 	}
+
+	user := User{
+		Model: model,
+	}
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := DB.FirstOrCreate(&user).Error; err != nil {
+				t.Errorf("Failed, got error: %v", err)
+			}
+		}()
+	}
+
+	wg.Wait()
 }
