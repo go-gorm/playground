c4f2baa566869fdc2edfb32fbe3411cb5b173976
Cinjon Resnick <cinjon.resnick@gmail.com>
2023-05-16 09:16:20 -0400

    Failing test.

--- main_test.go
+++ main_test.go
@@ -17,4 +17,37 @@ func TestGORM(t *testing.T) {
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	familiar := Familiar{Name: "fam"}
+	familiarIDBefore := familiar.Id
+	DB.Create(&familiar)
+	familiarIDAfter := familiar.Id
+	if familiarIDBefore == familiarIDAfter {
+		t.Errorf("Familiar ID should be created")
+	}
+
+	meeting := Meeting{IsLive: true}
+	DB.Create(&meeting)
+	familiars := []Familiar{familiar}
+	meetingFamiliars := meeting.Familiars
+	if len(meetingFamiliars) > 0 {
+		t.Errorf("Meeting should not have any familiars yet")
+	}
+
+	DB.Model(&meeting).Association("Familiars").Append(familiars)
+	meetingFamiliars = meeting.Familiars
+	if len(meetingFamiliars) != 1 {
+		t.Errorf("Meeting should have a familiar now")
+	}
+
+	familiarInMeetingID := meetingFamiliars[0].Id
+
+	familiarIDAfterMeetingAdd := familiar.Id
+	if familiarIDAfterMeetingAdd != familiarIDAfter {
+		t.Errorf("Familiar ID should not be changed")
+	}
+
+	if familiarInMeetingID != familiarIDAfter {
+		t.Errorf("Familiar ID should be the same. Meeting familiar ID: %v, familiar ID: %v", familiarInMeetingID, familiarIDAfter)
+	}
 }
--- models.go
+++ models.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -57,4 +58,34 @@ type Company struct {
 type Language struct {
 	Code string `gorm:"primarykey"`
 	Name string
+}
+
+type Meeting struct {
+	gorm.Model
+	Id          string     `gorm:"type:uuid;primaryKey" json:"id"`
+	IsLive      bool       `json:"isLive"`
+	CompletedAt time.Time  `json:"completedAt"`
+	Familiars   []Familiar `gorm:"many2many:familiar_meetings;"`
+}
+
+func (m *Meeting) BeforeCreate(tx *gorm.DB) (err error) {
+	// UUID version 4
+	m.Id = uuid.NewString()
+	return
+}
+
+// Represents a Familiar. A Familiar is a user's avatar that can talk to other Familiars. Note that it has a Location in
+// addition to its owner having a Location. They can be separate if its left at home, hence the WithUser field, which
+// defaults to true.
+type Familiar struct {
+	gorm.Model
+	Id       string    `gorm:"type:uuid;primaryKey" json:"id"`
+	Meetings []Meeting `gorm:"many2many:familiar_meetings;"`
+	Name     string    `json:"name"`
+}
+
+func (f *Familiar) BeforeCreate(tx *gorm.DB) (err error) {
+	// UUID version 4
+	f.Id = uuid.NewString()
+	return
 }
