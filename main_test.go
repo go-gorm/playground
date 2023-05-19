78d9626f7f59587562a1247b7b1cb0a2478cb123
Sam Cohen <samc1213@gmail.com>
2023-05-19 10:56:34 -0600

    Add Test for Empty String

--- main_test.go
+++ main_test.go
@@ -8,13 +8,20 @@ import (
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
+type NullableStringType struct {
+	Value string `gorm:"default:null"`
+}
+
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	DB.AutoMigrate(&NullableStringType{})
+	value := NullableStringType{Value: "jinzhu"}
+	value2 := NullableStringType{}
+	values := []NullableStringType{value, value2}
 
-	DB.Create(&user)
-
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	tx := DB.Create(&values)
+	if tx.Error != nil {
+		// Fails for sqlite - near "DEFAULT": syntax error
+		// [0.210ms] [rows:0] INSERT INTO `nullable_string_types` (`value`) VALUES ("jinzhu"),(DEFAULT) RETURNING `value`
+		t.Fail()
 	}
 }
