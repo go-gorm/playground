1ccc563d3b0fc99eb20937ed45dae173f2f72fe0
yudidi <972656027@qq.com>
2023-06-08 17:35:32 +0800

    done

--- main_test.go
+++ main_test.go
@@ -6,15 +6,24 @@ import (
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: mysql
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	// user := User{Name: "jinzhu"}
+	// 使用many2many创建关联表后,又再次AutoMigrate创建关联表,因为之前创建了联合索引(primary key (user_id, friend_id)),导致关系表无法增加新的逐渐索引.
+	err := DB.AutoMigrate(&User{}, &UserFriend{})
+	failOnError(t, err)
+}
 
-	DB.Create(&user)
+type UserFriend struct {
+	ID       uint64 `gorm:"primary_key"`
+	UserID   uint64 `gorm:"not null"`
+	FriendID uint64 `gorm:"not null"`
+}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+func failOnError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal(err)
 	}
 }
