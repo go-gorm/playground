d805a4ba3b9ba7961c823f1b947f5f6c311c6255
Ronan <crnoogle@gmail.com>
2023-07-16 00:01:29 +0800

    Update models and test (Problem about belongs to)

--- main_test.go
+++ main_test.go
@@ -10,11 +10,22 @@ import (
 
 func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
-
 	DB.Create(&user)
 
 	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	if err := DB.First(&result, user.Id).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+
+	product := Product{Name: "product", MyModel: MyModel{CreatedBy: user.Id}, OwnerId: user.Id}
+	store := Store{Name: "store", Products: []Product{product}}
+	DB.Create(&store)
+
+	var result2 []Store
+	if err := DB.Preload("Products.Owner").Find(&result2).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	if result2[0].Products[0].Owner.Id != user.Id && result2[0].Products[0].Owner.Id != 0 {
+		t.Errorf("Failed, expected Owner ID to be %v, got %v", user.Id, result2[0].Products[0].Owner.Id)
 	}
 }
--- models.go
+++ models.go
@@ -1,60 +1,26 @@
 package main
 
-import (
-	"database/sql"
-	"time"
+type MyModel struct {
+	Id        uint `gorm:"primaryKey"`
+	CreatedBy uint
+}
 
-	"gorm.io/gorm"
-)
-
-// User has one `Account` (has one), many `Pets` (has many) and `Toys` (has many - polymorphic)
-// He works in a Company (belongs to), he has a Manager (belongs to - single-table), and also managed a Team (has many - single-table)
-// He speaks many languages (many to many) and has many friends (many to many - single-table)
-// His pet also has one Toy (has one - polymorphic)
 type User struct {
-	gorm.Model
-	Name      string
-	Age       uint
-	Birthday  *time.Time
-	Account   Account
-	Pets      []*Pet
-	Toys      []Toy `gorm:"polymorphic:Owner"`
-	CompanyID *int
-	Company   Company
-	ManagerID *uint
-	Manager   *User
-	Team      []User     `gorm:"foreignkey:ManagerID"`
-	Languages []Language `gorm:"many2many:UserSpeak"`
-	Friends   []*User    `gorm:"many2many:user_friends"`
-	Active    bool
-}
-
-type Account struct {
-	gorm.Model
-	UserID sql.NullInt64
-	Number string
-}
-
-type Pet struct {
-	gorm.Model
-	UserID *uint
-	Name   string
-	Toy    Toy `gorm:"polymorphic:Owner;"`
-}
-
-type Toy struct {
-	gorm.Model
-	Name      string
-	OwnerID   string
-	OwnerType string
-}
-
-type Company struct {
-	ID   int
+	MyModel
 	Name string
 }
 
-type Language struct {
-	Code string `gorm:"primarykey"`
-	Name string
+type Store struct {
+	MyModel
+	Name     string
+	Products []Product `gorm:"foreignKey:StoreId"`
+}
+
+type Product struct {
+	MyModel
+	StoreId uint
+	Name    string
+	OwnerId uint
+	// Owner   User `gorm:"foreignKey:OwnerId;references:Id"` // this works
+	Owner User `gorm:"foreignKey:CreatedBy;references:Id"` // this not work, CreatedBy here belongs to Product, not User
 }
