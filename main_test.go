4bdf3c9e36938c14ef8f1ee25b020825c25c9d3c
ahuigo <1781999+ahuigo@users.noreply.github.com>
2023-07-26 23:58:30 +0800

    case: many2many does not support non primaryKey

--- main_test.go
+++ main_test.go
@@ -8,13 +8,32 @@ import (
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
-func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+// doc: https://gorm.io/docs/many_to_many.html#Override-Foreign-Key
+type Companny struct {
+	ID   int    `gorm:"primarykey"`
+	Name string `gorm:"index:,unique"`
+	Stus []Stu  `gorm:"many2many:stu_compannys;foreignKey:Name;joinForeignKey:CompannyName;References:Stuname;joinReferences:Stuname;"`
+}
+type Stu struct {
+	ID        int       `gorm:"primarykey"`
+	Stuname   string    `gorm:"index:,unique"`
+	Compannies []Companny `gorm:"many2many:stu_compannys;"`
+}
 
-	DB.Create(&user)
+func TestMany2ManyUniqueKey(t *testing.T) {
+    db := DB
+    db.Debug().Migrator().DropTable(&Companny{}, &Stu{}, "stu_compannys")
+	db.Debug().AutoMigrate(&Stu{})
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	err := db.Debug().Create(&Stu{
+		Stuname: "Alex3",
+		ID:      3,
+		Compannies: []Companny{
+			{Name: "PKU1"}, {Name: "TSU2"},
+		},
+	})
+	if err!=nil{
+		t.Fatal(err)
 	}
+
 }
