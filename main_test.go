86abc9654405551e857db8bd25426b112372df5d
Satyam Krishna <satyamkrishna92@gmail.com>
2023-08-07 21:36:28 +0530

    failure of conflict clause on 6th iteration

--- main_test.go
+++ main_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +13,43 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	err := DB.AutoMigrate(&DynamoDB{})
+	if err != nil {
+		panic("failed to auto-migrate table")
+	}
 
-	DB.Create(&user)
+	indexes := []string{
+		"DROP INDEX IF EXISTS idx_unique;",
+		"CREATE UNIQUE INDEX idx_unique ON dynamodb (hash_key, sort_key) WHERE NOT tombstone;",
+	}
+	for _, index := range indexes {
+		err = DB.Exec(index).Error
+		if err != nil {
+			panic(fmt.Errorf("failed to apply index: %w", err))
+		}
+	}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	for i := 1; i <= 6; i++ {
+		createMetadataVault(DB)
+		fmt.Println("ran successfully for iteration: ", i)
+	}
+}
+
+func createMetadataVault(db *gorm.DB) {
+	var metadataModel = DynamoDB{
+		HashKey: "hash",
+		SortKey: "sort",
+		Value:   "value",
+	}
+	result := db.
+		Clauses(clause.OnConflict{
+			Columns:     []clause.Column{{Name: "hash_key"}, {Name: "sort_key"}},
+			TargetWhere: clause.Where{Exprs: []clause.Expression{clause.Eq{Column: "tombstone", Value: false}}},
+			DoUpdates:   clause.AssignmentColumns([]string{"value"}),
+		}).
+		Create(&metadataModel)
+
+	if result.Error != nil {
+		panic("failed to create metadata vault")
 	}
 }
