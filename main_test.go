5540f0f50444372ffe680c2ee0ce712b2f526298
Chonghan Chen <chonghan.chen@lacework.net>
2023-08-09 15:09:13 -0700

    add a failed test case

--- main_test.go
+++ main_test.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"gorm.io/gorm"
 	"testing"
 )
 
-// GORM_REPO: https://github.com/go-gorm/gorm.git
-// GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// Exact Example from https://gorm.io/docs/has_many.html#Declare
+
+type MyUser struct {
+	gorm.Model
+	MyCreditCards []MyCreditCard
+}
+
+type MyCreditCard struct {
+	gorm.Model
+	Number   string
+	MyUserID uint
+}
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
 
-	DB.Create(&user)
+	// This passes
+	//if err := DB.AutoMigrate(&MyCreditCard{}, &MyUser{}); err != nil {
+	//	t.Errorf("Failed, got error: %v", err)
+	//}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	/* This fails. Error message:
+	[0.489ms] [rows:0] ALTER TABLE `my_credit_cards` ADD CONSTRAINT `fk_my_users_my_credit_cards` FOREIGN KEY (`my_user_id`) REFERENCES `my_users`(`id`)
+	--- FAIL: TestGORM (0.01s)
+	    main_test.go:30: Failed, got error: Error 1824 (HY000): Failed to open the referenced table 'my_users'
+	FAIL
+	exit status 1
+	FAIL    gorm.io/playground      0.364s
+	*/
+	if err := DB.AutoMigrate(&MyCreditCard{}, &MyUser{}); err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
