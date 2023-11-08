92765d2e57119aea386c663e2cbe5bc2d8e9e29c
Lasse Hyldahl Jensen <lhj@lunar.app>
2023-11-08 20:58:41 +0100

    Simplify test

--- main_test.go
+++ main_test.go
@@ -9,15 +9,22 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	if err := DB.Migrator().DropTable(&TestTable{}); err != nil {
-		t.Fatalf("failed to drop table: %v", err)
-	}
-	for i := 0; i < 10; i++ {
-		t.Run("test", func(t *testing.T) {
-			t.Parallel()
+	for a := 0; a < 2; a++ {
+		t.Run("drop and migrate", func(t *testing.T) {
+			t.Run("drop", func(t *testing.T) {
+				if err := DB.Migrator().DropTable(&TestTable{}); err != nil {
+					t.Fatalf("failed to drop table: %v", err)
+				}
+			})
 
-			if err := DB.AutoMigrate(&TestTable{}); err != nil {
-				t.Fatalf("failed to migrate: %v", err)
+			for i := 0; i < 2; i++ {
+				t.Run("migrate", func(t *testing.T) {
+					t.Parallel()
+
+					if err := DB.AutoMigrate(&TestTable{}); err != nil {
+						t.Fatalf("failed to migrate: %v", err)
+					}
+				})
 			}
 		})
 	}
