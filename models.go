5a43f96b87cde81ddd6614083f4cd3d9708012d2
Dariusz Kwiatkowski <dariusz.kwiatkowski@hpe.com>
2023-12-11 16:02:46 +0100

    Minimal reproduction

--- models.go
+++ models.go
@@ -13,21 +13,24 @@ import (
 // His pet also has one Toy (has one - polymorphic)
 type User struct {
 	gorm.Model
-	Name      string
-	Age       uint
-	Birthday  *time.Time
-	Account   Account
-	Pets      []*Pet
-	Toys      []Toy `gorm:"polymorphic:Owner"`
-	CompanyID *int
-	Company   Company
-	ManagerID *uint
-	Manager   *User
-	Team      []User     `gorm:"foreignkey:ManagerID"`
-	Languages []Language `gorm:"many2many:UserSpeak"`
-	Friends   []*User    `gorm:"many2many:user_friends"`
-	Active    bool
+	Name             string
+	Age              uint
+	Birthday         *time.Time
+	Account          Account
+	Pets             []*Pet
+	Toys             []Toy `gorm:"polymorphic:Owner"`
+	CompanyID        *int
+	Company          Company
+	ManagerID        *uint
+	Manager          *User
+	Team             []User     `gorm:"foreignkey:ManagerID"`
+	Languages        []Language `gorm:"many2many:UserSpeak"`
+	Friends          []*User    `gorm:"many2many:user_friends"`
+	Active           bool
+	SomeCustomString CustomString
 }
+
+type CustomString string
 
 type Account struct {
 	gorm.Model
