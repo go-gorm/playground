99f8560c8b23832b61d06d7e5e39315067073cfa
Daniel Schreij <dschreij@gmail.com>
2024-01-10 17:14:35 +0100

    #6790: Count() erases Select of preloads

--- main_test.go
+++ main_test.go
@@ -6,15 +6,89 @@ import (
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: postgres
 
 func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
 
-	DB.Create(&user)
+	tx := DB.Begin()
+	tx.Create(&user)
 
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+	tx.Rollback()
+}
+
+func TestJoinsWithoutCount(t *testing.T) {
+	users := []User{
+		{
+			Name: "John Doe",
+			Company: Company{
+				Name: "Company A",
+			},
+		},
+		{
+			Name: "Jane Doe",
+			Company: Company{
+				Name: "Company B",
+			},
+		},
+	}
+	tx := DB.Begin()
+	tx.Create(&users)
+
+	tx.Model(&User{}).Joins("Company").Order("\"Company\".\"name\" ASC")
+	var result []User
+	if err := tx.Find(&result).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	if len(result) != 2 {
+		t.Errorf("Failed, expected 2 companies, got %d", len(result))
+	}
+	if result[0].Company.Name != "Company A" {
+		t.Errorf("Failed, expected Company A, got %s", result[0].Company.Name)
+	}
+	tx.Rollback()
+}
+
+func TestJoinsWithCount(t *testing.T) {
+	users := []User{
+		{
+			Name: "John Doe",
+			Company: Company{
+				Name: "Company A",
+			},
+		},
+		{
+			Name: "Jane Doe",
+			Company: Company{
+				Name: "Company B",
+			},
+		},
+	}
+	tx := DB.Begin()
+	tx.Create(&users)
+	tx.Model(&User{}).Joins("Company").
+		Order("\"Company\".\"name\" ASC") // Order still works!
+
+	// --- Critical change
+	var count int64
+	if err := tx.Count(&count).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	// --- End critical change
+
+	var result []User
+	if err := tx.Find(&result).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	if len(result) != 2 {
+		t.Errorf("Failed, expected 2 companies, got %d", len(result))
+	}
+	if result[0].Company.Name != "Company A" {
+		t.Errorf("Failed, expected Company A, got %s", result[0].Company.Name)
+	}
+	tx.Rollback()
 }
