a9d7d8aa94f12a1a522c127c60df95d64fd35322
Gabriel Cojocaru <cojocarugabi31415@gmail.com>
2024-01-17 21:55:40 +0200

    COUNT does not work with multicolumn distinct

--- main_test.go
+++ main_test.go
@@ -9,12 +9,33 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+    l1 := Language{Code:"English",Name:"English"}
+    l2 := Language{Code:"French",Name:"French"}
+    user1 := User{Name: "jinzhu", Age: 1, Languages:[]Language{l1, l2}}
+    user2 := User{Name: "jinzhu", Age: 2, Languages:[]Language{l1, l2}}
 
-	DB.Create(&user)
+	DB.Create(&l1)
+	DB.Create(&l2)
+	DB.Create(&user1)
+	DB.Create(&user2)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+    count := int64(0)
+
+	var result []User
+	if err := DB.Model(&User{}).
+        Joins("JOIN user_speaks ON user_speaks.user_id = users.id").
+        Joins("JOIN languages ON user_speaks.language_code = languages.code").
+        Distinct("users.name, users.age").
+        Count(&count).
+        Find(&result).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+    if len(result) != 2 {
+		t.Errorf("Failed, expected only two users, got %d", len(result))
+    }
+
+    if count != 2 {
+		t.Errorf("Failed, expected only two users in count, got %d", count)
+    }
 }
