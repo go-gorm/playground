c82f9f0a878b0daaac6394e9f2df2b777ede0421
Jannis Rieger <omniskopus@gmail.com>
2024-01-20 01:03:51 +0100

    endless loop

--- main_test.go
+++ main_test.go
@@ -13,8 +13,8 @@ func TestGORM(t *testing.T) {
 
 	DB.Create(&user)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	var intf interface{} = user
+	if err := DB.Save(&intf).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
