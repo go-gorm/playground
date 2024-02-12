a576685d09128f7c0bd9e7ba65d56f1c696740e8
Christopher Biel <christopher.biel@jungheinrich.de>
2024-02-12 13:06:46 +0100

    Add example for broken nested where

--- models.go
+++ models.go
@@ -27,6 +27,7 @@ type User struct {
 	Languages []Language `gorm:"many2many:UserSpeak"`
 	Friends   []*User    `gorm:"many2many:user_friends"`
 	Active    bool
+	Something string
 }
 
 type Account struct {
@@ -50,8 +51,9 @@ type Toy struct {
 }
 
 type Company struct {
-	ID   int
-	Name string
+	ID      int
+	Name    string
+	Country string
 }
 
 type Language struct {
