a02cdcac62fa33782ee965b39179aa0013f4b2f2
Tsubasa Munekata <tsubasa.munekata@mixi.co.jp>
2024-02-22 19:05:27 +0900

    Test db.Not

--- main_test.go
+++ main_test.go
@@ -5,16 +5,24 @@ import (
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
-// GORM_BRANCH: master
+// GORM_BRANCH: v1.25.5
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	users := []User{
+		{Name: "a", Age: 1},
+		{Name: "a", Age: 2},
+		{Name: "b", Age: 1},
+		{Name: "b", Age: 2},
+	}
 
-	DB.Create(&user)
+	DB.Create(&users)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	var result int64
+	if err := DB.Model(&User{}).Not(DB.Where("name = 'a'").Where("age = 1")).Count(&result).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+	if result != 3 {
+		t.Errorf("Failed, expect: 3, got: %v", result)
 	}
 }
