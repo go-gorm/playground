d2e67635bf24d58d2615bde3986d69f68beb766f
Stijn De Haes <stijndehaes@gmail.com>
2024-04-10 18:20:55 +0200

    Failing embedded struct

--- main_test.go
+++ main_test.go
@@ -9,12 +9,45 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	type BasePost struct {
+		Id    int64
+		Title string
+		URL   string
+	}
 
-	DB.Create(&user)
+	type Address string
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	type Author struct {
+		ID      string
+		Name    string
+		Email   string
+		Age     int
+		Address Address
+	}
+
+	type HNPost struct {
+		*BasePost
+		Upvotes int32
+		*Author `gorm:"EmbeddedPrefix:user_"` // Embedded struct
+	}
+
+	DB.Migrator().DropTable(&HNPost{})
+	if err := DB.Migrator().AutoMigrate(&HNPost{}); err != nil {
+		t.Fatalf("failed to auto migrate, got error: %v", err)
+	}
+
+	DB.Create(&HNPost{BasePost: &BasePost{Title: "embedded_pointer_type"}})
+
+	var hnPost HNPost
+	if err := DB.First(&hnPost, "title = ?", "embedded_pointer_type").Error; err != nil {
+		t.Errorf("No error should happen when find embedded pointer type, but got %v", err)
+	}
+
+	if hnPost.Title != "embedded_pointer_type" {
+		t.Errorf("Should find correct value for embedded pointer type")
+	}
+
+	if hnPost.Author != nil {
+		t.Errorf("Expected to get back a nil Author but got: %v", hnPost.Author)
 	}
 }
