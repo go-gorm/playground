ed0127e122c7118323445f54977d36b5b85648ec
iTanken <23544702+iTanken@users.noreply.github.com>
2024-04-30 08:41:31 +0000

    test: re-migrate existing table

--- main_test.go
+++ main_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"errors"
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: sqlserver
 
 func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
@@ -15,6 +18,21 @@ func TestGORM(t *testing.T) {
 
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			t.Errorf("Failed, got error: %v", err)
+		}
+	}
+}
+
+func TestReMigrate(t *testing.T) {
+	// re-migrate existing table
+	u := &User{}
+	if !DB.Migrator().HasTable(u) {
+		if err := DB.AutoMigrate(u); err != nil {
+			t.Fatalf("Failed to auto migrate, but got error %v\n", err)
+		}
+	}
+	if err := DB.AutoMigrate(u); err != nil {
+		t.Fatalf("Failed to auto migrate, but got error %v\n", err)
 	}
 }
