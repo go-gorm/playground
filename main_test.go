b8c30e876c4131a0e7ca60796da4f2212983484b
Emilien Kofman <emilien.kofman@miimosa.com>
2024-05-07 17:50:10 +0200

    test: a testcase that causes panic

--- main_test.go
+++ main_test.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
-
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
+	user := User{
+		Name: "jinzhu",
+		Account: Account{
+			Number: "123456",
+			Companies: []Company{
+				{Name: "Corp1"}, {Name: "Corp2"},
+			},
+			Pet: Pet{
+				Name: "Pet1",
+			},
+		},
+	}
 	DB.Create(&user)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	for i := 0; i < 20; i++ {
+		DB.Create(&User{Name: fmt.Sprintf("User%d", i)})
 	}
+
+	var entries []User
+	assert.NotPanics(t, func() {
+		_ = DB.WithContext(context.Background()).
+			Joins("Account.Pet").
+			Preload("Account.Companies").
+			Find(&entries).Error
+	})
+
 }
