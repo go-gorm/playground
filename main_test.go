2796b1bd14ceca19192275ab2f2951f34371ba37
sgsv <->
2024-05-09 16:12:11 +0200

    Panic on associations replace

--- main_test.go
+++ main_test.go
@@ -9,12 +9,16 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	user := User{Name: "jinzhu", Languages: []Language{{Name: "EN"}}}
 
 	DB.Create(&user)
 
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+
+	if err := DB.Model(&user).Association("Languages").Replace(Language{Name: "DE"}, Language{Name: "FR"}); err == nil {
+		t.Error("Failed, error should be present")
 	}
 }
