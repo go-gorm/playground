655d00f86cb3f7464b340c88ec30f637813cb6df
Thomas Modeneis <thomas.modeneis@gmail.com>
2024-05-31 14:27:07 +0200

    add working test with uint and go-sqlmock tests

--- mock_test.go
+++ mock_test.go
@@ -157,6 +157,73 @@ func TestGORMV2_Working(t *testing.T) {
 	}
 }
 
+func TestGORMV2_ID_Working(t *testing.T) {
+
+	type Student struct {
+		gorm.Model
+		Name string `gorm:"type:varchar(50);not null"`
+	}
+	type v2Suite struct {
+		db      *gorm.DB
+		mock    sqlmock.Sqlmock
+		student Student
+	}
+
+	s := &v2Suite{}
+	var (
+		db  *sql.DB
+		err error
+	)
+
+	db, s.mock, err = sqlmock.New()
+	if err != nil {
+		t.Errorf("Failed to open mock sql db, got error: %v", err)
+	}
+
+	if db == nil {
+		t.Error("mock db is null")
+	}
+
+	if s.mock == nil {
+		t.Error("sqlmock is null")
+	}
+
+	s.db, err = gorm.Open(postgres.New(
+		postgres.Config{
+			Conn:       db,
+			DriverName: "postgres",
+		},
+	), &gorm.Config{})
+	if err != nil {
+		panic(err) // Error here
+	}
+
+	defer db.Close()
+
+	s.student = Student{
+		Name: "Test 1",
+	}
+
+	defer db.Close()
+	addRow := sqlmock.NewRows([]string{"id"}).AddRow("1")
+
+	s.mock.ExpectBegin()
+
+	s.mock.ExpectQuery(`INSERT INTO "students" (.+) VALUES (.+) RETURNING "id"`).WillReturnRows(addRow)
+
+	s.mock.ExpectCommit()
+
+	if err = s.db.Create(&s.student).Error; err != nil {
+		t.Errorf("Failed to insert to gorm db, got error: %v", err)
+		t.FailNow()
+	}
+
+	err = s.mock.ExpectationsWereMet()
+	if err != nil {
+		t.Errorf("Failed to meet expectations, got error: %v", err)
+	}
+}
+
 func TestMain(m *testing.M) {
 
 	code := m.Run()
