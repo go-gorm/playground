5ec90237267502476b9b9bbbd2401ca47409591a
Chad Baker <me@chadbaker.us>
2024-08-01 14:24:52 -0400

    test

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gorm.io/playground
 go 1.20
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	gorm.io/driver/mysql v1.5.2
 	gorm.io/driver/postgres v1.5.2
 	gorm.io/driver/sqlite v1.5.3
@@ -25,7 +26,7 @@ require (
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.15.0 // indirect
 	gorm.io/datatypes v1.1.1-0.20230130040222-c43177d3cf8c // indirect
 	gorm.io/hints v1.1.0 // indirect
--- main_test.go
+++ main_test.go
@@ -1,20 +1,48 @@
 package main
 
 import (
+	"regexp"
 	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: postgres
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
 
-	DB.Create(&user)
+	gdb, err := gorm.Open(postgres.New(postgres.Config{
+		Conn:       db,
+		DriverName: "postgres",
+	}), &gorm.Config{})
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	const id = 1
+	const name = "b"
+	var out *User
+	gdb.Model(&User{}).Where("active", true).Scopes(func(d *gorm.DB) *gorm.DB {
+		return d.Where(
+			d.Where("id", id).Or("name", name),
+		)
+	}).First(&out)
+
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "users" WHERE "active" = $1 AND ("id" = $2 or "name" = $3) AND "users"."deleted_at" IS NULL ORDER BY "users"."id" LIMIT 1`)).
+		WithArgs(true, id, name).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).
+			AddRow(id))
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatal(err)
 	}
 }
