b158b827c54d19501a4d45a02f760b1f5c1da44c
Leo Sjöberg <leo.sjoberg@gmail.com>
2024-08-28 12:10:30 +0100

    New test

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"gorm.io/gorm"
@@ -12,19 +13,26 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	// Create a wrapping transaction
+	var user User
+	err := DB.First(&user, "name = ?", "jinzhu").Error
+	// ensure we start with no user
+	if err == nil {
+		t.Errorf("User %d already exists", user.ID)
+	}
 
-	err := Transaction(context.Background(), DB, func(ctx context.Context, tx *gorm.DB) error {
+	// Create a wrapping transaction
+	err = Transaction(context.Background(), DB, func(ctx context.Context, tx *gorm.DB) error {
 		// Within this, create a transaction that retrieves a user, and then
 		// creates another transaction that retrieves the account, but errors,
 		// and bubble that error up.
-		_ = Transaction(ctx, tx, func(ctx context.Context, tx *gorm.DB) error {
+		err := Transaction(ctx, tx, func(ctx context.Context, tx *gorm.DB) error {
 			user := User{Name: "jinzhu"}
 			var account Account
 			err := DB.Create(&user).Error
 			if err != nil {
 				return err
 			}
+			fmt.Printf("User created: %d", user.ID)
 
 			// Since we propagate the error, we'll now also try to
 			// rollback this nested transaction, using the same
@@ -42,6 +50,9 @@ func TestGORM(t *testing.T) {
 				return nil
 			})
 		})
+		if err == nil {
+			t.Errorf("Expected an error, got none")
+		}
 		// We discard the inner transaction error, which allows us to commit this outer
 		// transaction (which does nothing), even if the inner fails.
 		return nil
@@ -49,9 +60,8 @@ func TestGORM(t *testing.T) {
 
 	// Since we have rolled back the inner transaction, we expect that
 	// no user was created, since we should have rolled that back.
-	var user User
 	err = DB.First(&user, "name = ?", "jinzhu").Error
 	if err == nil {
-		t.Errorf("User was created, despite erroring inside the transaction")
+		t.Errorf("User %d was created, despite erroring inside the transaction", user.ID)
 	}
 }
