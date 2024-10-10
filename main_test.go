dfb930a70d1972153d2cec1d76ffbdb661ed63e5
Mark Hildreth <mark.hildreth@gravityclimate.com>
2024-10-10 11:48:44 -0400

    Added failing test case

--- main_test.go
+++ main_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +12,26 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	user := User{Name: "jinzhu", Age: 30}
 
 	DB.Create(&user)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	type result struct {
+		Name      string
+		IsExample bool
+		Age       int
 	}
+	var results []result
+	err := DB.Table("users").Select("name, name = 'test example.com' as is_example", "age").Scan(&results).Error
+	require.NoError(t, err)
+	assert.False(t, results[0].IsExample)
+	assert.Equal(t, results[0].Age, 30)
+
+	// This time, an ampersand character is used in the select string
+	err = DB.Table("users").Select("name, name = 'test@example.com' as is_example", "age").Scan(&results).Error
+	assert.False(t, results[0].IsExample)
+	// Fails, because the @ causes the Select statement to ignore
+	assert.Equal(t, results[0].Age, 30)
+
+	require.NoError(t, err)
 }
