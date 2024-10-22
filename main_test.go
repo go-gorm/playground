26cdd18051984dee715c87ced7b093e461764906
Ezequiel Muns <ezeq.au@gmail.com>
2024-10-22 13:01:25 +0200

    clause.Not and clause.And result in erroneous simplification

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"gorm.io/gorm/clause"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +11,69 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	DB.Create(&User{Name: "jinzhu", Active: true})
+	DB.Create(&User{Name: "ezequiel", Active: true})
+	DB.Create(&User{Name: "jinzhu", Active: false})
+	DB.Create(&User{Name: "ezequiel", Active: false})
 
-	DB.Create(&user)
+	var results []User
+	conds := clause.Not(clause.And(
+		clause.Eq{
+			Column: clause.Column{Name: "name"},
+			Value:  "jinzhu",
+		},
+		clause.Eq{
+			Column: clause.Column{Name: "active"},
+			Value:  true,
+		},
+	))
+	// Query with `NOT(name = 'jinzhu' AND active = true)` should return last three records
+	// By De Morgan's Law this is equivalent to `name != 'jinzhu' OR active != true`
+	// but current version (since 1.25.6) transforms this to
+	// `name != 'jinzhu' AND active != true` which returns only the last record
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	err := DB.
+		Model(&User{}).
+		Clauses(clause.Where{Exprs: []clause.Expression{conds}}).
+		Find(&results).
+		Error
+	if err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+	if len(results) != 3 {
+		t.Errorf("Failed, expected 3 records, got %v", len(results))
+	}
+	for _, r := range results {
+		if r.Name == "jinzhu" && r.Active {
+			t.Errorf("Failed, unexpected record: %v", r)
+		}
+	}
+
+	conds = clause.Not(clause.Or(
+		clause.Eq{
+			Column: clause.Column{Name: "name"},
+			Value:  "jinzhu",
+		},
+		clause.Eq{
+			Column: clause.Column{Name: "active"},
+			Value:  true,
+		},
+	))
+	// Query with `NOT(name = 'jinzhu' OR active = true)` should return last record
+	err = DB.
+		Model(&User{}).
+		Clauses(clause.Where{Exprs: []clause.Expression{conds}}).
+		Find(&results).
+		Error
+	if err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	if len(results) != 1 {
+		t.Errorf("Failed, expected 1 records, got %v", len(results))
+	}
+	for _, r := range results {
+		if r.Name == "jinzhu" || r.Active {
+			t.Errorf("Failed, unexpected record: %v", r)
+		}
 	}
 }
