b14f6586740e4e8d48342d99bd6c648baadc4bd0
Keller <j.keller@bendergruppe.com>
2024-11-14 11:13:05 +0100

    Use Custom UpdatedAt time type

--- main_test.go
+++ main_test.go
@@ -11,10 +11,26 @@ import (
 func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
 
-	DB.Create(&user)
+	if err := DB.Create(&user).Error; err != nil {
+		t.Fatal(err)
+	}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	var st2 User
+	if err := DB.First(&st2, 1).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	if st2.ID != 1 {
+		t.Fatalf("expected ID 1, got %d", st2.ID)
+	}
+
+	st2.Name = "new User"
+	if err := DB.Save(&st2).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	var st3 User
+	if err := DB.First(&st3, 1).Error; err != nil {
+		t.Fatal(err)
 	}
 }
