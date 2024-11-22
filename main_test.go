9e1a274f75ee7cfd66142747cfd1db76e2d5c187
Mathieu Lemay <acidrain1@gmail.com>
2024-11-22 13:59:00 -0500

    Add test for issue

--- main_test.go
+++ main_test.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"os"
+	"strings"
 	"testing"
+
+	"ariga.io/atlas-provider-gorm/gormschema"
+
+	"gorm.io/gorm"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -16,5 +22,25 @@ func TestGORM(t *testing.T) {
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+}
+
+func TestDBMigrationGeneration(t *testing.T) {
+	type Product struct {
+		gorm.Model
+
+		Code  string
+		Price uint
+	}
+
+	dialect := os.Getenv("GORM_DIALECT")
+	stmts, err := gormschema.New(dialect).Load(&Product{})
+
+	if err != nil {
+		t.Errorf("error loading gorm schema: %v", err)
+	}
+
+	if !strings.Contains(stmts, "idx_products_deleted_at") {
+		t.Errorf("index not found in generated statements: %s", stmts)
 	}
 }
