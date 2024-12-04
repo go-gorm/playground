18c4daef452159c5eab8ece01a47b57b536d9457
Heran Yang <heran55@126.com>
2024-12-04 19:22:44 +0800

    Test idle time

--- main_test.go
+++ main_test.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"testing"
+	"time"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
 // GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
+// TEST_DRIVERS: mysql
 
 func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
+	DB.Raw("SET @@SESSION.wait_timeout=2")
+	time.Sleep(3 * time.Second)
 
 	DB.Create(&user)
 
