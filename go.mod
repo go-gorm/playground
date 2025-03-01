dfe63a0ef005a1cc6d9bd6aeeb0203b224c6c819
Keith Martin <keith.martin@outlook.com.au>
2025-03-01 15:26:43 +1000

    Add potential workaround/fix example test

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.0
 toolchain go1.23.3
 
 require (
+	github.com/jackc/pgx/v5 v5.7.2
 	github.com/stretchr/testify v1.10.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/driver/postgres v1.5.11
@@ -23,7 +24,6 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.7.2 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
--- main_test.go
+++ main_test.go
@@ -5,6 +5,15 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -44,4 +53,68 @@ func TestTimeZone(t *testing.T) {
 	birthdayResult = *userResult.Birthday
 	assert.Equal(t, birthday, birthdayResult)
 
+}
+
+func TestPGXPooltoSqlDB(t *testing.T) {
+	if DB.Dialector.Name() != "postgres" {
+		t.Skip("test only runs in postgres")
+	}
+
+	// The following test shows how to open a pgxpool with the forced return of timezone as UTC.
+	ctx := context.Background()
+
+	uri := "user=gorm password=gorm host=localhost dbname=gorm port=9920 sslmode=disable TimeZone=UTC"
+
+	config, err := pgxpool.ParseConfig(uri)
+	if err != nil {
+		t.Errorf("Failed, got error: %v", err)
+		t.FailNow()
+	}
+
+	config.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
+		conn.TypeMap().RegisterType(&pgtype.Type{
+			Name:  "timestamptz",
+			OID:   pgtype.TimestamptzOID,
+			Codec: &pgtype.TimestamptzCodec{ScanLocation: time.UTC}, // It would be better to parse the connection string for TimeZone and use that here.  Or have some other way of specifying it.
+		})
+
+		return nil
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		t.Errorf("Failed, got error: %v", err)
+		t.FailNow()
+	}
+
+	// test connection to ensure all is well
+	if err = pool.Ping(ctx); err != nil {
+		pool.Close()
+		t.Errorf("Failed, got error: %v", err)
+		t.FailNow()
+	}
+
+	nativeDB := stdlib.OpenDBFromPool(pool)
+	db, err := gorm.Open(postgres.New(postgres.Config{Conn: nativeDB}))
+	if err != nil {
+		t.Errorf("Failed, got error: %v", err)
+		t.FailNow()
+	}
+
+	birthday := time.Date(2023, 11, 12, 9, 7, 18, 0, time.UTC)
+	userResult := User{}
+
+	if err := db.Where("name = ?", "TimeZone UTC").First(&userResult).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+		t.FailNow()
+	}
+
+	birthdayResult := *userResult.Birthday
+	assert.Equal(t, birthday, birthdayResult)
+
+	// Need some way to allow the closing of the connection pool after the gorm connection has closed.
+	if sqlDB, _ := db.DB(); sqlDB != nil {
+		sqlDB.Close()
+		pool.Close()
+	}
 }
