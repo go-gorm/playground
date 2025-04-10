2ac0696f4c73c4f9884314c69c47e8bcf6355eeb
cruvie <cruvie@foxmail.com>
2025-04-10 22:52:53 +0800

    testOmit

--- main_test.go
+++ main_test.go
@@ -13,8 +13,14 @@ func TestGORM(t *testing.T) {
 
 	DB.Create(&user)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	err := DB.Debug().Select("*").Omit("id").Omit("created_at").Omit("name").
+		Where("name = ?", "jinzhu").
+		Updates(user).Error
+	if err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+	/*
+	   [0.883ms] [rows:1] UPDATE `users` SET `id`=1,`created_at`="2025-04-10 22:50:54.487",`updated_at`="2025-04-10 22:50:54.488",`deleted_at`=NULL,`age`=0,`birthday`=NULL,`company_id`=NULL,`manager_id`=NULL,`active`=false WHERE name = "jinzhu" AND `users`.`deleted_at` IS NULL AND `id` = 1
+	   --- PASS: TestGORM (0.00s)
+	*/
 }
