9274c9de5d83d089fe6c53f0df146586143a1768
Kunin <mosceo@gmail.com>
2025-07-28 14:42:44 +1000

    Add a test that fails to update time.Time when saving a list of models

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"time"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -16,5 +17,29 @@ func TestGORM(t *testing.T) {
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
+	}
+}
+
+func TestUpdateTime(t *testing.T) {
+	prices := []Price{
+		{ProductId: 1, Price: 100, SomeTime: time.Now()},
+		{ProductId: 2, Price: 150, SomeTime: time.Now()},
+	}
+
+	DB.Save(prices)
+
+	// Change time for the second model, and persist models
+	tim := time.Now().Add(-10000 * time.Hour)
+	prices[1].SomeTime = tim
+
+	DB.Save(prices)
+
+	var p Price
+	if err := DB.First(&p, 2).Error; err != nil {
+		t.Errorf("Failed to get model: %v", err)
+	}
+
+	if !p.SomeTime.Equal(tim) {
+		t.Errorf("Time was not updated: %v != %v", p.SomeTime, tim)
 	}
 }
